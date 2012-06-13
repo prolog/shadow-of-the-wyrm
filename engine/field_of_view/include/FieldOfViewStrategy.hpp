cdf11c2d3fe7e0845bf87b1c1dd25e3464cf249e
Julian Day <jcd748@mail.usask.ca>
2012-06-12 20:35:26 -0500

    Fixed a bug where all LOS was setting the viewed flag on the Tile, instead of just the player's LOS (unseen areas appearing).

--- engine/field_of_view/include/FieldOfViewStrategy.hpp
+++ engine/field_of_view/include/FieldOfViewStrategy.hpp
@@ -5,10 +5,14 @@
 class FieldOfViewStrategy
 {
   public:
+    FieldOfViewStrategy(const bool set_view_property);
+    
     virtual MapPtr calculate(MapPtr view_map, const Coordinate& centre_coordinate, const int los_length) = 0;
     
   protected:
     void add_point_to_map(const Coordinate& c, MapPtr view_map, MapPtr fov_map);
+    
+    bool set_fov_tile_view_flag;
 };
 
 typedef boost::shared_ptr<FieldOfViewStrategy> FieldOfViewStrategyPtr;
