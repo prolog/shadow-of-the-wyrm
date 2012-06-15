97001770e488a6c563b6633a00e214ad37b3de9d
Julian Day <jcd748@mail.usask.ca>
2012-06-15 17:34:13 -0500

    Change a few methods to use ActionCostValue before The Big Conversion.  Rename a few appropriate "uint"s to "ActionCostValue"s.

--- engine/actions/include/CharacterDumpManager.hpp
+++ engine/actions/include/CharacterDumpManager.hpp
@@ -5,11 +5,11 @@
 class CharacterDumpManager : public IActionManager
 {
   public:
-    void dump_character(CreaturePtr creature);
+    ActionCostValue dump_character(CreaturePtr creature);
 
   protected:
     friend class ActionManager;
     CharacterDumpManager();
     
-    uint get_action_cost_value() const;
+    ActionCostValue get_action_cost_value() const;
 };
