cb234068b09c18f97e74520936e0af59bf0d6a91
Julian Day <jcd748@mail.usask.ca>
2012-06-17 22:00:09 -0500

    Addition of a Calendar to the World - this will eventually keep track of all the game's date/time info.

--- world/include/World.hpp
+++ world/include/World.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <boost/shared_ptr.hpp>
+#include "Calendar.hpp"
 #include "Map.hpp"
 
 class World
@@ -10,9 +11,12 @@ class World
 
     void set_world(MapPtr new_world_map);
     MapPtr get_world() const;
+    
+    Calendar& get_calendar();
 
   protected:
     MapPtr world_map;
+    Calendar calendar;
 };
 
 typedef boost::shared_ptr<World> WorldPtr;
