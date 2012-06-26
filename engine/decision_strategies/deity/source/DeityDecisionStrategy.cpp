8da5dfd5c98f0fdd9aa66a0f150aae5d3773f253
Julian Day <jcd748@mail.usask.ca>
2012-06-26 07:36:39 -0500

    Allow healing prayers only when piety is non-negative.

--- engine/decision_strategies/deity/source/DeityDecisionStrategy.cpp
+++ engine/decision_strategies/deity/source/DeityDecisionStrategy.cpp
@@ -8,12 +8,17 @@ DeityDecisionType DeityDecisionStrategy::get_decision(CreaturePtr creature)
 
   if (creature)
   {
-    Statistic hp = creature->get_hit_points();
+    Religion& religion = creature->get_religion_ref();
+    int piety          = religion.get_deity_status(religion.get_active_deity_id()).get_piety();
+    Statistic hp       = creature->get_hit_points();
     
     // Only heal the creature if the creature is hurting a little bit.
-    if (hp.get_current() < (hp.get_base() * DeityDecisionConstants::HEAL_HP_MAX_PCT))
+    if (piety >= 0)
     {
-      decision_type = DEITY_DECISION_FULL_HP;
+      if (hp.get_current() < (hp.get_base() * DeityDecisionConstants::HEAL_HP_MAX_PCT))
+      {
+        decision_type = DEITY_DECISION_FULL_HP;
+      }
     }
   }
 
