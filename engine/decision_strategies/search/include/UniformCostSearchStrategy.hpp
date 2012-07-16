42ac331cb1e203f680baf1dc210e396f7363dd8b
Julian Day <jcd748@mail.usask.ca>
2012-07-16 09:08:40 -0500

    Adding the DFS/UCS classes.

---
+++ engine/decision_strategies/search/include/UniformCostSearchStrategy.hpp
@@ -0,0 +1,12 @@
+#include "Search.hpp"
+
+class UniformCostSearchStrategy : public Search
+{
+  protected:
+    // Queue so that the lowest cost (as measured by path cost) are at
+    // the front of the queue, and thus are expanded first.
+    virtual std::list<SearchNode> queueing_fn(const std::list<SearchNode>& explored_search_nodes, const std::list<SearchNode>& new_search_nodes);
+
+    // Sort in ascending order of path cost
+    static bool path_sort_comparison(const SearchNode& sn1, const SearchNode& sn2);
+};
