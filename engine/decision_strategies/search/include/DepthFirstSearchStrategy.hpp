ed9327ffd1e58b9a81f66364a4fdef7cca95b3f0
Julian Day <jcd748@mail.usask.ca>
2012-07-16 10:34:17 -0500

    NPCs now pathfind using A* search with Chebyshev heuristic.

--- engine/decision_strategies/search/include/DepthFirstSearchStrategy.hpp
+++
@@ -1,9 +0,0 @@
-#include "Search.hpp"
-
-class DepthFirstSearchStrategy : public Search
-{
-  public:
-  protected:
-    // Queue at start
-    virtual std::list<SearchNode> queueing_fn(const std::list<SearchNode>& explored_search_nodes, const std::list<SearchNode>& new_search_nodes);
-};
--- engine/decision_strategies/search/include/Search.hpp
+++ engine/decision_strategies/search/include/Search.hpp
@@ -17,14 +17,14 @@ class Search
     bool goal_test(const SearchNode& sn, const Coordinate& end_coord);
     
     bool is_empty(const std::list<SearchNode>& search_queue) const;
-    SearchNode remove_front(std::list<SearchNode>& search_queue);
+    virtual SearchNode remove_front(std::list<SearchNode>& search_queue);
     
     // Create a list of search nodes given a coordinate
-    std::list<SearchNode> make_search_nodes(MapPtr view_map, std::set<Coordinate>& visited, const Coordinate& c, const SearchNode* const parent = NULL);
+    std::list<SearchNode> make_search_nodes(MapPtr view_map, std::set<Coordinate>& visited, const Coordinate& c, const Coordinate& goal_coordinate, const SearchNode* const parent = NULL);
     
     // Create a list of search nodes given a search node - this will fully populate
     // depth and path cost details.
-    std::list<SearchNode> make_search_nodes(MapPtr view_map, std::set<Coordinate>& visited, const SearchNode& sn);
+    std::list<SearchNode> make_search_nodes(MapPtr view_map, std::set<Coordinate>& visited, const Coordinate& goal_coordinate, const SearchNode& sn);
     
     // Variations on this give different search types (BFS, DFS, etc).
     virtual std::list<SearchNode> queueing_fn(const std::list<SearchNode>& explored_search_nodes, const std::list<SearchNode>& new_search_nodes) = 0;
