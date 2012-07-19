9707fd279cf9340ab5ea55c4a6e10c041f83d584
Julian Day <jcd748@mail.usask.ca>
2012-07-19 13:24:44 -0500

    Fix a number of equipment wearing/removing bugs related to advancing the turn.

--- engine/actions/include/IActionManager.hpp
+++ engine/actions/include/IActionManager.hpp
@@ -7,5 +7,5 @@ class IActionManager
   protected:
     // Does not take into account creature speed: this value gets added to the creature speed if the value
     // is greater than zero.
-    virtual uint get_action_cost_value() const = 0;
+    virtual ActionCostValue get_action_cost_value() const = 0;
 };
