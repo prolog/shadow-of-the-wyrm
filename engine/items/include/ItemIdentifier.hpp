6592e346ba2d7ace5f1471047cdfcfc8c89b5f45
Julian Day <jcd748@mail.usask.ca>
2012-09-13 07:53:45 -0500

    Refactor eq/inv item identification into ItemIdentifier.

--- engine/items/include/ItemIdentifier.hpp
+++ engine/items/include/ItemIdentifier.hpp
@@ -1,11 +1,26 @@
 #pragma once
 #include <string>
+#include <vector>
+#include "Creature.hpp"
 #include "Item.hpp"
+#include "IItemDisplayFilter.hpp"
 
 // A class for working with logic based around an item's "identified" status.
 class ItemIdentifier
 {
   public:
+    // Set the creature's entire possessions (equipment and inventory) identified.  Filter as necessary.
+    void set_possessions_identified(CreaturePtr creature, const std::list<IItemDisplayFilterPtr> * const filter_v = NULL);
+
+    // Set the creature's equipment identified.
+    void set_equipment_identified(CreaturePtr creature, const std::list<IItemDisplayFilterPtr> * const filter_v = NULL);
+    
+    // Set the creature's inventory identified.
+    void set_inventory_identified(CreaturePtr creature, const std::list<IItemDisplayFilterPtr> * const filter_v = NULL);
+    
+    // Identify all possessions of a particular type.
+    void set_possessions_identified_by_type(CreaturePtr creature, const ItemType item_type);
+    
     // Functions for querying or modifying a particular item's
     // identification status.
     void set_item_identified(const std::string& base_item_id, const bool is_identified) const;
