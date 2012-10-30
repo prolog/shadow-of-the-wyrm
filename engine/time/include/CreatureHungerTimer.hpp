73e5386bdb13ecbee90188cc8eb6dd648aa75818
Julian Day <jcd748@mail.usask.ca>
2012-10-29 20:06:53 -0500

    Add hunger timer for player, and allow the player to starve to death.

---
+++ engine/time/include/CreatureHungerTimer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include "HungerDamageCalculator.hpp"
+#include "ICreatureRegeneration.hpp"
+
+class CreatureHungerTimer : public ICreatureRegeneration
+{
+  public:
+    void tick(CreaturePtr creature, const ulonglong minutes_this_tick, const ulonglong total_minutes_elapsed);
+
+  protected:
+    void apply_hunger_damage_if_appropriate(CreaturePtr creature, const int minutes_this_tick);
+
+    HungerDamageCalculator hdc;
+};
+
