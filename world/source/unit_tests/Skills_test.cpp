a2a2582581ead3bb6de6429744308912e5b9e502
Julian Day <jcd748@mail.usask.ca>
2012-11-02 08:02:00 -0500

    Fix build errors in unit tests after adding unit test define.

--- world/source/unit_tests/Skills_test.cpp
+++ world/source/unit_tests/Skills_test.cpp
@@ -1,10 +1,10 @@
 #include "gtest/gtest.h"
 
-// Can't instantiate Skill directly, so test the methods of a direct
+// Can't instantiate Skill directly, so test the methods of a
 // descendent.
 TEST(SL_World_Skills_GeneralSkill, value)
 {
-  GeneralSkill gs;
+  ArcherySkill gs;
   gs.set_value(5);
 
   EXPECT_EQ(5, gs.get_value());
@@ -16,7 +16,7 @@ TEST(SL_World_Skills_GeneralSkill, value)
 
 TEST(SL_World_Skills_GeneralSkill, marks)
 {
-  GeneralSkill gs;
+  ArcherySkill gs;
   gs.set_marks(7);
 
   EXPECT_EQ(7, gs.get_marks());
@@ -28,7 +28,7 @@ TEST(SL_World_Skills_GeneralSkill, marks)
 
 TEST(SL_World_Skills_GeneralSkill, threshold)
 {
-  GeneralSkill gs;
+  ArcherySkill gs;
   gs.set_threshold(100);
 
   EXPECT_EQ(100, gs.get_threshold());
@@ -36,36 +36,24 @@ TEST(SL_World_Skills_GeneralSkill, threshold)
 
 TEST(SL_World_Skills_GeneralSkill, category)
 {
-  GeneralSkill gs;
-  EXPECT_EQ(SKILL_CATEGORY_GENERAL, gs.get_category());
-
   ArcherySkill archery;
   EXPECT_EQ(SKILL_CATEGORY_GENERAL, archery.get_category());
 }
 
 TEST(SL_World_Skills_WeaponSkill, category)
 {
-  WeaponSkill ws;
-  EXPECT_EQ(SKILL_CATEGORY_MELEE, ws.get_category());
-
   AxesSkill axes;
   EXPECT_EQ(SKILL_CATEGORY_MELEE, axes.get_category());
 }
 
 TEST(SL_World_Skills_RangedWeaponSkill, category)
 {
-  RangedWeaponSkill rws;
-  EXPECT_EQ(SKILL_CATEGORY_RANGED, rws.get_category());
-
   BowsSkill bows;
   EXPECT_EQ(SKILL_CATEGORY_RANGED, bows.get_category());
 }
 
 TEST(SL_World_Skills_MagicSkill, category)
 {
-  MagicSkill ms;
-  EXPECT_EQ(SKILL_CATEGORY_MAGIC, ms.get_category());
-
   ArcaneMagicSkill arcane;
   EXPECT_EQ(SKILL_CATEGORY_MAGIC, arcane.get_category());
 }
