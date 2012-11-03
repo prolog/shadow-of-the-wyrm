cdd658dd7ab5255ade1aa2479a46933f9c27256e
Julian Day <jcd748@mail.usask.ca>
2012-11-03 08:45:45 -0500

    Hunger-related unit tests.

---
+++ engine/calculators/source/unit_tests/HungerDamageCalculator_test.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+TEST(SL_Engine_Calculators_HungerDamageCalculator, damage_per_tick)
+{
+  HungerDamageCalculator hdc;
+  CreaturePtr null_creature;
+
+  EXPECT_EQ(0, hdc.calculate_hunger_damage(null_creature, 0));
+  EXPECT_EQ(10, hdc.calculate_hunger_damage(null_creature, 1));
+  EXPECT_EQ(50, hdc.calculate_hunger_damage(null_creature, 5));
+  EXPECT_EQ(500, hdc.calculate_hunger_damage(null_creature, 50));
+  EXPECT_EQ(1000, hdc.calculate_hunger_damage(null_creature, 100));
+}
+
