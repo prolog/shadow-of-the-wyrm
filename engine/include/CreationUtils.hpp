dac5ff4e1f6242a5d4eb6ddd6b256b093f2f3c46
Julian Day <jcd748@mail.usask.ca>
2012-12-09 09:11:40 -0600

    Very crude multiple creature generation.

--- engine/include/CreationUtils.hpp
+++ engine/include/CreationUtils.hpp
@@ -1,9 +1,12 @@
 #pragma once
+#include "common.hpp"
 #include "RarityTypes.hpp"
 
 class CreationUtils
 {
   public:
+    static const uint MAX_UNSUCCESSFUL_CREATURE_ATTEMPTS;
+    static uint random_maximum_creatures(const int map_height, const int map_width);
     static Rarity generate_rarity();
 
   protected:
