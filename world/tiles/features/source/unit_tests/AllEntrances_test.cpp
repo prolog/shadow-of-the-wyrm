a1a51e7d970c190b1cc4ca2dd2aa540eb61353af
Julian Day <jcd748@mail.usask.ca>
2013-01-19 13:40:46 -0600

    Entrances weren't saving/loading properly - the base class wasn't called.
    Fixed the bug and added appropriate unit tests to check this automatically in future.

--- world/tiles/features/source/unit_tests/AllEntrances_test.cpp
+++ world/tiles/features/source/unit_tests/AllEntrances_test.cpp
@@ -9,6 +9,12 @@ TEST(SL_World_Tiles_Feature_Entrances, saveload)
 
   ostringstream ss, ss2;
 
+  door.get_state_ref().set_state(ENTRANCE_TYPE_OPEN);
+  door.set_maximum_size(CREATURE_SIZE_SMALL);
+
+  gate.get_state_ref().set_state(ENTRANCE_TYPE_CLOSED);
+  gate.set_maximum_size(CREATURE_SIZE_LARGE);
+
   door.serialize(ss);
   gate.serialize(ss2);
 
