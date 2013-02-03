6ea9443e24332363d1d396e051ade41eeabaec93
Julian Day <jcd748@mail.usask.ca>
2013-02-03 10:02:31 -0600

    Create a Lua scripting engine, and add an instance to the static Game object.

---
+++ engine/scripting/source/ScriptEngine.cpp
@@ -0,0 +1,15 @@
+#include "ScriptEngine.hpp"
+
+// Create a new Lua state object, and open the libraries.
+ScriptEngine::ScriptEngine()
+{
+   L = lua_open();
+   luaL_openlibs( L );
+}
+
+// Tear down the Lua state.
+ScriptEngine::~ScriptEngine()
+{
+   lua_close( L );
+}
+
