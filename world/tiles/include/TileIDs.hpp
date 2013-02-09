df3655b89ec1358f389fbe457b8c3c6ee9cb0d18
Julian Day <jcd748@mail.usask.ca>
2013-02-09 09:17:42 -0600

    Add the Barrows, and the associated creatures: thieves, graverobbers, hounds, and blackguards.  Add ivory pieces and the Sun Gem within the burial ship.

--- world/tiles/include/TileIDs.hpp
+++ world/tiles/include/TileIDs.hpp
@@ -6,6 +6,7 @@ class TileCustomMapIDs
   public:
     static const std::string CUSTOM_MAP_ID_ISEN_DUN;
     static const std::string CUSTOM_MAP_ID_LAST_ISLAND_LAIR;
+    static const std::string CUSTOM_MAP_ID_THE_BARROWS;
 
   protected:
     TileCustomMapIDs();
