3166a0571bd1c6c447f4c35712e652d18eae747a
Julian Day <jcd748@mail.usask.ca>
2013-02-17 10:58:03 -0600

    Additional quest updates for the Barrows quest.

--- scripts/hrimgar.lua
+++ scripts/hrimgar.lua
@@ -4,4 +4,8 @@ Quest = require('quest')
 bandit_quest = Quest.create("hrimgar_bandits", "HRIMGAR_BANDIT_QUEST_TITLE_SID", "HRIMGAR_SHORT_DESCRIPTION_SID", "HRIMGAR_BANDIT_DESCRIPTION_SID")
 
 add_new_quest(bandit_quest.quest_id, bandit_quest)
--- add_message("HRIMGAR_QUEST_START")
+add_message_with_pause("HRIMGAR_SPEECH_TEXT_SID")
+add_message_with_pause("HRIMGAR_BANDIT_QUEST_START_SID")
+add_message_with_pause("HRIMGAR_BANDIT_QUEST_START2_SID")
+add_message("HRIMGAR_BANDIT_QUEST_START3_SID")
+
