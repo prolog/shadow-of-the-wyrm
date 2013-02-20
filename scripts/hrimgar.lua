5849c62a4a811ac26d3f29b854b06aea5b0ce92f
Julian Day <jcd748@mail.usask.ca>
2013-02-19 20:17:08 -0600

    Refactor the Hrimgar quest to allow for completion if the condition is met, regardless of whether the quest has already been given.

--- scripts/hrimgar.lua
+++ scripts/hrimgar.lua
@@ -3,16 +3,25 @@ Quest = require('quest')
 -- Create the first quest: Bandits in the Barrow
 bandit_quest = Quest.create("hrimgar_bandits", "HRIMGAR_BANDIT_QUEST_TITLE_SID", "HRIMGAR_SHORT_DESCRIPTION_SID", "HRIMGAR_BANDIT_DESCRIPTION_SID")
 
+-- Quest is complete
 if is_quest_completed(bandit_quest.quest_id) then
   add_message("HRIMGAR_SPEECH_TEXT_SID")
-elseif is_on_quest(bandit_quest.quest_id) then
-  if get_num_creature_killed_global("garros") == 1 then
-    add_message("HRIMGAR_BANDIT_QUEST_COMPLETE_SID")
-    add_object_to_player_tile("boathouse_key")
-    mark_quest_completed(bandit_quest.quest_id)
-  else
-    add_message("HRIMGAR_BANDIT_QUEST_REMINDER_SID")
+-- Quest is not complete, but condition is met.
+elseif get_num_creature_killed_global("garros") == 1 then
+  add_message("HRIMGAR_BANDIT_QUEST_COMPLETE_SID")
+  add_object_to_player_tile("boathouse_key")
+
+  -- May not be on the quest.  Silently add the quest if not.
+  if is_on_quest(bandit_quest.quest_id) == false then
+    add_new_quest(bandit_quest.quest_id, bandit_quest)
   end
+
+  -- Guaranteed to have a quest at this point.  Mark it as complete.
+  mark_quest_completed(bandit_quest.quest_id)
+-- Quest is in progress, condition is not met.
+elseif is_on_quest(bandit_quest.quest_id) then
+  add_message("HRIMGAR_BANDIT_QUEST_REMINDER_SID")
+-- Quest is not in progress, condition is not met.
 else
   add_new_quest(bandit_quest.quest_id, bandit_quest)
   add_message_with_pause("HRIMGAR_SPEECH_TEXT_SID")
