2c5c1cccf0ee2e1c7db203f818218059140d8be7
Julian Day <jcd748@mail.usask.ca>
2013-02-23 10:43:23 -0600

    When a script error in the Lua files occurs, add a message to the UI.  Refactor the quest code so that it is more generic.  Add a new quest for Hrimgar to slay Amaurosis, the great wyrm.

--- engine/source/StringConstants.cpp
+++ engine/source/StringConstants.cpp
@@ -1675,3 +1675,12 @@ GameEnvTextKeys::~GameEnvTextKeys()
 }
 
 const string GameEnvTextKeys::GAME_ENV_LUA_STATE_CLEARED = "GAME_ENV_LUA_STATE_CLEARED";
+const string GameEnvTextKeys::GAME_ENV_LUA_ERROR = "GAME_ENV_LUA_ERROR";
+
+string GameEnvTextKeys::get_lua_error(const string& lua_error_msg)
+{
+  string error_msg = StringTable::get(GAME_ENV_LUA_ERROR);
+  boost::replace_first(error_msg, "%s", lua_error_msg);
+
+  return error_msg;
+}
