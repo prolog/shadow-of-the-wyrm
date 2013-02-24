5883620cda7110b4fa862ae32188e7004cb85b17
Julian Day <jcd748@mail.usask.ca>
2013-02-24 07:30:52 -0600

    Fix inventory and equipment display bugs.
    When leaving the inventory and equipment screens, sometimes wgetch() would
    be called on a menu, instead of getch() on STDSCR.  This was because the menus were not being cleared correctly (they remained on the stack), and some recent code I added checked the stack to determine which function to call.

--- engine/actions/source/EquipmentManager.cpp
+++ engine/actions/source/EquipmentManager.cpp
@@ -19,51 +19,60 @@ ActionCostValue EquipmentManager::manage_equipment()
 {
   ActionCostValue total_action_cost = 0;
   ActionCostValue action_cost = 0; // Looking at eq has no cost - adding or removing items will update this.
+  ulonglong menus_created = 0;
   
   bool manage_eq = true; // For looping
 
-  if (creature)
+  try
   {
-    CommandFactoryPtr command_factory    = boost::make_shared<EquipmentCommandFactory>();
-    KeyboardCommandMapPtr kb_command_map = boost::make_shared<EquipmentKeyboardCommandMap>();
-    
-    while (manage_eq)
+    if (creature)
     {
-      if (display && creature->get_is_player())
+      CommandFactoryPtr command_factory    = boost::make_shared<EquipmentCommandFactory>();
+      KeyboardCommandMapPtr kb_command_map = boost::make_shared<EquipmentKeyboardCommandMap>();
+    
+      while (manage_eq)
       {
-        DisplayEquipmentMap dem = EquipmentTranslator::create_display_equipment(creature);
-        display->display_equipment(dem);
-      }
-      
-      DecisionStrategyPtr decision_strategy = creature->get_decision_strategy();
-      
-      if (decision_strategy)
-      {
-        CommandPtr equipment_command = decision_strategy->get_decision(creature->get_id(), command_factory, kb_command_map);
-        action_cost = EquipmentCommandProcessor::process(creature, equipment_command);
-        
-        if ((action_cost > 0) && (total_action_cost == 0))
+        if (display && creature->get_is_player())
         {
-          total_action_cost = action_cost;
+          DisplayEquipmentMap dem = EquipmentTranslator::create_display_equipment(creature);
+          display->display_equipment(dem);
+          menus_created++;
         }
-      }
-      else
-      {
-        manage_eq = 0;
-      }
       
-      // -1 is a special value for the equipment command.  "0" means "don't advance the turn,
-      // but keep the equipment screen up."  "-1" means "exit was selected."
-      if (action_cost == -1)
-      {
-        manage_eq = 0;
-      }
+        DecisionStrategyPtr decision_strategy = creature->get_decision_strategy();
+      
+        if (decision_strategy)
+        {
+          CommandPtr equipment_command = decision_strategy->get_decision(creature->get_id(), command_factory, kb_command_map);
+          action_cost = EquipmentCommandProcessor::process(creature, equipment_command);
+        
+          if ((action_cost > 0) && (total_action_cost == 0))
+          {
+            total_action_cost = action_cost;
+            display->clear_menu();
+          }
+        }
+        else
+        {
+          manage_eq = 0;
+        }
+      
+        // -1 is a special value for the equipment command.  "0" means "don't advance the turn,
+        // but keep the equipment screen up."  "-1" means "exit was selected."
+        if (action_cost == -1)
+        {
+          manage_eq = 0;
+        }
+      }    
     }
-    
-    if (display && creature->get_is_player()) // Clean up the menu
-    {
-      display->clear_menu();
-    }
+  }
+  catch(...)
+  {
+  }
+
+  for (ulonglong i = 0; i < menus_created; i++)
+  {
+    display->clear_menu();
   }
 
   return total_action_cost;
