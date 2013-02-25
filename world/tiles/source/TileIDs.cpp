c0859afe9c486faf38543775faf05db11d12cf7b
Julian Day <jcd748@mail.usask.ca>
2013-02-25 07:55:16 -0600

    Initial, empty implementation of Wintersea Keep.

--- world/tiles/source/TileIDs.cpp
+++ world/tiles/source/TileIDs.cpp
@@ -13,4 +13,5 @@ TileCustomMapIDs::~TileCustomMapIDs()
 const string TileCustomMapIDs::CUSTOM_MAP_ID_ISEN_DUN = "isen_dun";
 const string TileCustomMapIDs::CUSTOM_MAP_ID_LAST_ISLAND_LAIR = "last_island_lair";
 const string TileCustomMapIDs::CUSTOM_MAP_ID_THE_BARROWS = "the_barrows";
+const string TileCustomMapIDs::CUSTOM_MAP_ID_WINTERSEA_KEEP = "wintersea_keep";
 
