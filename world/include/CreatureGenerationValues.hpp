9fa335220a5d14b01ca213a9cd019140ceaf5d03
Julian Day <jcd748@mail.usask.ca>
2013-02-26 19:20:59 -0600

    Refactored CreatureGenerationValues so that it subclasses a more generic "GenerationValues" class.  GenerationValues will be used to track the number of items.

--- world/include/CreatureGenerationValues.hpp
+++ world/include/CreatureGenerationValues.hpp
@@ -2,29 +2,19 @@
 #include <map>
 #include <set>
 #include <string>
-#include "common.hpp"
 #include "Dice.hpp"
-#include "ISerializable.hpp"
-#include "RarityTypes.hpp"
+#include "GenerationValues.hpp"
 #include "tiles.hpp"
 
 // Values that are used to generate a creature, without being a necessary
 // part of the creature itself.  Prevents values that only need a single
 // instance from being duplicated on every creature.
-class CreatureGenerationValues : public ISerializable
+class CreatureGenerationValues : public GenerationValues
 {
   public:
     CreatureGenerationValues();
     ~CreatureGenerationValues();
     bool operator==(const CreatureGenerationValues& cgv);
-
-    void set_current(const int new_current);
-    int incr_current();
-    int get_current() const;
-
-    void set_maximum(const int new_maximum);
-    int get_maximum() const;
-    bool is_maximum_reached() const;
 
     void add_allowable_terrain_type(const TileType additional_terrain_type);
     void clear_allowable_terrain_types();
@@ -35,12 +25,6 @@ class CreatureGenerationValues : public ISerializable
     void set_friendly(const bool new_friendly);
     bool get_friendly() const;
 
-    void set_danger_level(const uint new_danger_level);
-    uint get_danger_level() const;
-    
-    void set_rarity(const Rarity new_rarity);
-    Rarity get_rarity() const;
-    
     void set_initial_hit_points(const Dice& new_initial_hit_points);
     Dice get_initial_hit_points() const;
     
@@ -51,12 +35,6 @@ class CreatureGenerationValues : public ISerializable
     bool deserialize(std::istream& stream);
 
   protected:
-    // The current number that has been generated.
-    int current;
-
-    // The maximum number of creatures, if non-negative.  If negative, ignored.
-    int maximum;
-
     // The terrain types in which the creature can be generated
     std::set<TileType> allowable_terrain_types;
 
@@ -65,13 +43,6 @@ class CreatureGenerationValues : public ISerializable
     // hostile towards the player.
     bool friendly;
 
-    // The "danger level" at which the creature may start appearing,
-    // if it is not out of depth/danger-depth.
-    uint danger_level;
-
-    // The more rare a creature is, the less chance of it getting generated in a given spawn.
-    Rarity rarity;
-    
     // A range used to randomly generate a creature's hit points every time an instance
     // is created from the template.
     Dice initial_hit_points;
---
+++ world/source/GenerationValues.cpp
@@ -0,0 +1,105 @@
+#include "GenerationValues.hpp"
+#include "Serialize.hpp"
+
+using namespace std;
+
+GenerationValues::GenerationValues()
+: current(0),
+maximum(-1),
+rarity(RARITY_COMMON), 
+danger_level(0)
+{
+}
+
+GenerationValues::~GenerationValues()
+{
+}
+
+bool GenerationValues::operator==(const GenerationValues& gv)
+{
+  bool result = true;
+
+  result = result && (current == gv.current);
+  result = result && (maximum == gv.maximum);
+  result = result && (rarity == gv.rarity);
+  result = result && (danger_level == gv.danger_level);
+
+  return result;
+}
+
+void GenerationValues::set_current(const int new_current)
+{
+  current = new_current;
+}
+
+int GenerationValues::incr_current()
+{
+  return ++current;
+}
+
+int GenerationValues::get_current() const
+{
+  return current;
+}
+
+void GenerationValues::set_maximum(const int new_maximum)
+{
+  maximum = new_maximum;
+}
+
+int GenerationValues::get_maximum() const
+{
+  return maximum;
+}
+
+// Check to see if the maximum number of generatable creatures has been
+// reached.
+bool GenerationValues::is_maximum_reached() const
+{
+  return (current == maximum);
+}
+
+void GenerationValues::set_danger_level(const uint new_danger_level)
+{
+  danger_level = new_danger_level;
+}
+
+uint GenerationValues::get_danger_level() const
+{
+  return danger_level;
+}
+
+void GenerationValues::set_rarity(const Rarity new_rarity)
+{
+  rarity = new_rarity;
+}
+
+Rarity GenerationValues::get_rarity() const
+{
+  return rarity;
+}
+
+bool GenerationValues::serialize(ostream& stream)
+{
+  Serialize::write_int(stream, current);
+  Serialize::write_int(stream, maximum);
+  Serialize::write_enum(stream, rarity);
+  Serialize::write_uint(stream, danger_level);
+
+  return true;
+}
+
+bool GenerationValues::deserialize(istream& stream)
+{
+  Serialize::read_int(stream, current);
+  Serialize::read_int(stream, maximum);
+  Serialize::read_enum(stream, rarity);
+  Serialize::read_enum(stream, danger_level);
+
+  return true;
+}
+
+ClassIdentifier GenerationValues::internal_class_identifier() const
+{
+  return CLASS_ID_GENERATION_VALUES;
+}
