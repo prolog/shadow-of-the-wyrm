6df6891d99f9ca9577dc09b2797e1799b1f7ca21
Julian Day <jcd748@mail.usask.ca>
2013-04-07 08:22:41 -0500

    Add messages when the player has no magical knowledge, and when the player has magical knowledge but is trying to cast on the world map.

---
+++ engine/strings/include/SpellcastingTextKeys.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+class SpellcastingTextKeys
+{
+  public:
+    static const std::string SPELLCASTING_NO_MAGICAL_KNOWLEDGE;
+    static const std::string SPELLCASTING_UNAVAILABLE_ON_WORLD_MAP;
+
+  protected:
+    SpellcastingTextKeys();
+};
---
+++ engine/strings/source/SpellcastingTextKeys.cpp
@@ -0,0 +1,11 @@
+#include "SpellcastingTextKeys.hpp"
+
+using namespace std;
+
+SpellcastingTextKeys::SpellcastingTextKeys()
+{
+}
+
+const string SpellcastingTextKeys::SPELLCASTING_NO_MAGICAL_KNOWLEDGE = "SPELLCASTING_NO_MAGICAL_KNOWLEDGE";
+const string SpellcastingTextKeys::SPELLCASTING_UNAVAILABLE_ON_WORLD_MAP = "SPELLCASTING_UNAVAILABLE_ON_WORLD_MAP";
+
