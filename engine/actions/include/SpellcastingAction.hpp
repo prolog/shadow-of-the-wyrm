3aa7f76968de04a7ee06117a21048d05500b9629
Julian Day <jcd748@mail.usask.ca>
2013-04-27 08:59:11 -0500

    Refactored the two main processing functions of SpellcastingAction into a number of new, smaller functions.

--- engine/actions/include/SpellcastingAction.hpp
+++ engine/actions/include/SpellcastingAction.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "IActionManager.hpp"
 #include "Creature.hpp"
+#include "SpellShapeProcessor.hpp"
 
 class SpellcastingAction : public IActionManager
 {
@@ -18,6 +19,22 @@ class SpellcastingAction : public IActionManager
     void add_no_magical_knowledge_message() const;
     void add_invalid_spellcasting_location_message() const;
     void add_insufficient_power_message() const;
+
+    std::pair<std::string, ActionCostValue> cast_spell_on_valid_map_type(CreaturePtr creature) const;
+    std::pair<bool, std::pair<std::string, ActionCostValue>> process_spellcasting_selection(CreaturePtr creature) const;
+
+    // Helper functions for the main cast_spell function
+    //
+    // Reduce the caster's AP appropriately.
+    void reduce_caster_ap_by_spell_cost(CreaturePtr caster, const Spell& spell) const;
+    // Handle reducing/removing castings after a successful cast.
+    void reduce_castings_or_remove_spell(CreaturePtr caster, const Spell& spell) const;
+    // When a spell requires a direction (beams, etc), get the direction from
+    // the creature.
+    std::pair<bool, Direction> get_spell_direction_from_creature(CreaturePtr caster, const Direction spell_direction) const;
+    // Get and process the spell's animation, followed by the spell damage and
+    // effect.
+    void process_animation_and_spell(SpellShapeProcessorPtr spell_processor, CreaturePtr caster, MapPtr map, const Coordinate& caster_coord, const Direction spell_direction, const Spell& spell) const;
     
     ActionCostValue get_action_cost_value() const;
 };
