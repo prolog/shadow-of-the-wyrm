9d735792b7039eb407f214f390bf6f9c55d62644
Julian Day <jcd748@mail.usask.ca>
2013-06-11 22:34:46 -0500

    Included the player's world map location as an additional property on the generator after creation, so that this information can be used as necessary to generate latitude/longitude-based features.

--- world/include/MapProperties.hpp
+++ world/include/MapProperties.hpp
@@ -7,6 +7,10 @@ class MapProperties
     // Can be used to override a generator's default permanence value.
     static const std::string MAP_PROPERTIES_PERMANENCE;
 
+    // Can be used to determine how to generate certain map features (such as
+    // proportion of evergreen vs. broadleaf trees).
+    static const std::string MAP_PROPERTIES_WORLD_MAP_LOCATION;
+
   protected:
     MapProperties();
     ~MapProperties();
