a429b719a8a28259b41f9d6e7adccf07773cfd6f
Julian Day <jcd748@mail.usask.ca>
2013-06-19 18:09:52 -0500

    Differentiation of trees by adding various species that are assigned when the tile is created.

--- engine/generators/tile_config/include/DefaultTileConfiguration.hpp
+++ engine/generators/tile_config/include/DefaultTileConfiguration.hpp
@@ -5,6 +5,5 @@ class DefaultTileConfiguration : public ITileConfiguration
 {
   public:
     virtual void configure(TilePtr tile) const;
-    virtual void update_for_season(TilePtr tile, ISeasonPtr season) const;
 };
 
