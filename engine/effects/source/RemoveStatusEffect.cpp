562dd19a762b07b800decc45946ae7a6924ee96b
Julian Day <jcd748@mail.usask.ca>
2013-07-09 19:24:52 -0500

    Updated the item randomization process for potions, scrolls, etc., to also randomize colour as part of the process - the descriptions and colours need to be randomized as a unit.  Prior to this change, only the descriptions were randomized, so each potion would keep its original colour, which was incorrect.

---
+++ engine/effects/source/RemoveStatusEffect.cpp
@@ -0,0 +1,54 @@
+#include <boost/make_shared.hpp>
+#include "AdditionalEffectMessages.hpp"
+#include "Creature.hpp"
+#include "EffectTextKeys.hpp"
+#include "MessageManager.hpp"
+#include "RemoveStatusEffect.hpp"
+#include "StatusEffectFactory.hpp"
+
+using namespace std;
+
+RemoveStatusEffect::RemoveStatusEffect(const string& status_ident)
+: status_id(status_ident)
+{
+}
+
+string RemoveStatusEffect::get_effect_identification_message(CreaturePtr creature) const
+{
+  string no_effect_msg;
+  return no_effect_msg;
+}
+
+Effect* RemoveStatusEffect::clone()
+{
+  return new RemoveStatusEffect(*this);
+}
+
+bool RemoveStatusEffect::effect_blessed(CreaturePtr creature, ActionManager * const am)
+{
+  return remove_status(creature);
+}
+
+bool RemoveStatusEffect::effect_uncursed(CreaturePtr creature, ActionManager * const am)
+{
+  return remove_status(creature);
+}
+
+bool RemoveStatusEffect::effect_cursed(CreaturePtr creature, ActionManager * am)
+{
+  return remove_status(creature);
+}
+
+bool RemoveStatusEffect::remove_status(CreaturePtr creature)
+{
+  bool removed_status = false;
+
+  if (creature && creature->has_status(status_id))
+  {
+    StatusEffectPtr status_effect = StatusEffectFactory::create_status_effect(status_id);
+    status_effect->undo_change(creature);
+    removed_status = true;
+  }
+
+  return removed_status;
+}
