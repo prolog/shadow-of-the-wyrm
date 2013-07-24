36c8aef71079a46470deb9a79de1cab2278ecea7
Julian Day <jcd748@mail.usask.ca>
2013-07-24 07:06:04 -0500

    Kill the affected creature when stoning is complete and uncured.

--- engine/creatures/source/StoneStatusEffect.cpp
+++ engine/creatures/source/StoneStatusEffect.cpp
@@ -1,4 +1,5 @@
 #include <boost/make_shared.hpp>
+#include "CombatManager.hpp"
 #include "StoneCalculator.hpp"
 #include "StoneStatusEffect.hpp"
 #include "StatusAilmentTextKeys.hpp"
@@ -9,6 +10,15 @@ using namespace std;
 StoneStatusEffect::StoneStatusEffect()
 {
   status_calc = boost::make_shared<StoneCalculator>();
+}
+
+void StoneStatusEffect::finalize(CreaturePtr creature) const
+{
+  CombatManager cm;
+  CreaturePtr no_creature;
+  string message_sid = StatusAilmentTextKeys::STATUS_MESSAGE_PLAYER_STONE_FINALIZE;
+
+  cm.deal_damage(no_creature, creature, creature->get_hit_points().get_base(), message_sid);
 }
 
 string StoneStatusEffect::get_player_application_message() const
