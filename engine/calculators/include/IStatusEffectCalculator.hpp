aa1dbbf8b2612d4d1e83fdc21a66340e5df68aae
Julian Day <jcd748@mail.usask.ca>
2013-08-04 09:59:44 -0500

    Added a new creature (apprentice), and added a currently-unfriendly example to Isen Dun for testing.  Updated CurrentCreatureAbilities to allow querying for the ability to move, and added a virtual destructor to IStatusEffectCalculator to avoid happy fun debugging time later on.

--- engine/calculators/include/IStatusEffectCalculator.hpp
+++ engine/calculators/include/IStatusEffectCalculator.hpp
@@ -5,6 +5,8 @@
 class IStatusEffectCalculator
 {
   public:
+    virtual ~IStatusEffectCalculator() {};
+
     virtual int calculate_pct_chance_effect(CreaturePtr creature) const = 0;
     virtual int calculate_duration_in_minutes(CreaturePtr creature) const = 0;
 };
--- engine/creatures/include/CurrentCreatureAbilities.hpp
+++ engine/creatures/include/CurrentCreatureAbilities.hpp
@@ -7,6 +7,7 @@ class CurrentCreatureAbilities
 {
   public:
     bool can_speak(CreaturePtr creature, const bool add_message_if_player_and_cannot_speak = false) const;
+    bool can_move(CreaturePtr creature, const bool add_message_if_player_and_cannot_move = false) const;
 
     // can_act is very general - it asks if the creature can perform any action
     // at all.  The only case when a creature can't should be when the creature
