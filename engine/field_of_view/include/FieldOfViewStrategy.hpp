41f572c93e9c782498fa194e5f8f819d35455b36
Julian Day <jcd748@mail.usask.ca>
2013-08-08 08:47:17 -0500

    Updated the FOV strategies to handle blindness.  This doesn't work very well, but provides an initial checkpoint from which to work.

--- engine/field_of_view/include/FieldOfViewStrategy.hpp
+++ engine/field_of_view/include/FieldOfViewStrategy.hpp
@@ -7,11 +7,14 @@ class FieldOfViewStrategy
   public:
     FieldOfViewStrategy(const bool set_view_property);
     
-    virtual MapPtr calculate(MapPtr view_map, const Coordinate& centre_coordinate, const int los_length) = 0;
+    virtual MapPtr calculate(CreaturePtr fov_creaure, MapPtr view_map, const Coordinate& centre_coordinate, const int los_length) = 0;
     
   protected:
-    void add_point_to_map(const Coordinate& c, MapPtr view_map, MapPtr fov_map);
+    void add_point_to_map(CreaturePtr fov_creature, const Coordinate& c, MapPtr view_map, MapPtr fov_map);
     
+    // The "view" flag is the flag that causes the tile to be displayed when
+    // redrawing the screen.  This should only be set for the player, or else
+    // the screen will be updated as creatures move about.
     bool set_fov_tile_view_flag;
 };
 
--- engine/field_of_view/source/FieldOfViewStrategy.cpp
+++ engine/field_of_view/source/FieldOfViewStrategy.cpp
@@ -1,3 +1,4 @@
+#include "CurrentCreatureAbilities.hpp"
 #include "FieldOfViewStrategy.hpp"
 
 FieldOfViewStrategy::FieldOfViewStrategy(const bool set_fov_flag)
@@ -5,18 +6,29 @@ FieldOfViewStrategy::FieldOfViewStrategy(const bool set_fov_flag)
 {  
 }
 
-void FieldOfViewStrategy::add_point_to_map(const Coordinate& c, MapPtr view_map, MapPtr fov_map)
+void FieldOfViewStrategy::add_point_to_map(CreaturePtr fov_creature, const Coordinate& c, MapPtr view_map, MapPtr fov_map)
 {
   TilePtr tile = view_map->at(c);
+
+  CurrentCreatureAbilities cca;
+  bool creature_blinded = !cca.can_see(fov_creature);
   
   if (tile)
   {
-    if (set_fov_tile_view_flag)
+    if (set_fov_tile_view_flag && !creature_blinded)
     {
       tile->set_viewed(true);      
     }
 
-    fov_map->insert(c.first, c.second, tile);
+    if (!creature_blinded || 
+         (creature_blinded && 
+          fov_creature && 
+          tile && 
+          tile->get_creature() && 
+          tile->get_creature()->get_id() == fov_creature->get_id()))
+    {
+      fov_map->insert(c.first, c.second, tile);
+    }
   }
 }
 
