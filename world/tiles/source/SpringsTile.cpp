66b42c6f2b721961debeab2757da3562569288c0
Julian Day <jcd748@mail.usask.ca>
2013-08-28 17:45:12 -0500

    Updated Springs to take a temperature, and return a different description SID based on the temperature.  This allows differentiation of springs vs. hot springs, which is of absolutely earth-shaking importance.

--- world/tiles/source/SpringsTile.cpp
+++ world/tiles/source/SpringsTile.cpp
@@ -1,4 +1,21 @@
 #include "SpringsTile.hpp"
+#include "Serialize.hpp"
+
+using namespace std;
+
+SpringsTile::SpringsTile()
+: temperature(SPRINGS_TEMPERATURE_COOL)
+{
+}
+
+bool SpringsTile::operator==(const SpringsTile& sp) const
+{
+  bool result = Tile::operator==(sp);
+
+  result = result && (temperature == sp.temperature);
+
+  return result;
+}
 
 TileType SpringsTile::get_tile_type() const
 {
@@ -12,12 +29,45 @@ TileSuperType SpringsTile::get_tile_super_type() const
 
 std::string SpringsTile::get_tile_description_sid() const
 {
-  return TileTextKeys::TILE_DESC_SPRINGS;
+  if (temperature >= SPRINGS_TEMPERATURE_HOT)
+  {
+    return TileTextKeys::TILE_DESC_HOT_SPRINGS;
+  }
+  else
+  {
+    return TileTextKeys::TILE_DESC_SPRINGS;
+  }
+}
+
+void SpringsTile::set_temperature(const int new_temperature)
+{
+  temperature = new_temperature;
+}
+
+int SpringsTile::get_temperature() const
+{
+  return temperature;
 }
 
 Tile* SpringsTile::clone()
 {
   return new SpringsTile(*this);
+}
+
+bool SpringsTile::serialize(ostream& stream)
+{
+  Tile::serialize(stream);
+  Serialize::write_int(stream, temperature);
+
+  return true;
+}
+
+bool SpringsTile::deserialize(istream& stream)
+{
+  Tile::deserialize(stream);
+  Serialize::read_int(stream, temperature);
+
+  return true;
 }
 
 ClassIdentifier SpringsTile::internal_class_identifier() const
