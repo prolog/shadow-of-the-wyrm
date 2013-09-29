434cea94e6a8cfd3221f93e2fe6f3fc68e7a065c
Julian Day <jcd748@mail.usask.ca>
2013-09-29 12:21:28 -0500

    Update unit test variable name.

--- world/tiles/source/unit_tests/WellTile_test.cpp
+++ world/tiles/source/unit_tests/WellTile_test.cpp
@@ -2,15 +2,15 @@
 
 TEST(SL_World_Tiles_WellTile, type_is_TILE_TYPE_WELL)
 {
-  WellTile pier_tile;
+  WellTile well_tile;
 
-  EXPECT_EQ(TILE_TYPE_WELL, pier_tile.get_tile_type());
+  EXPECT_EQ(TILE_TYPE_WELL, well_tile.get_tile_type());
 }
 
 TEST(SL_World_Tiles_WellTile, serialization_id)
 {
-  WellTile pier_tile;
+  WellTile well_tile;
 
-  EXPECT_EQ(CLASS_ID_WELL_TILE, pier_tile.get_class_identifier());
+  EXPECT_EQ(CLASS_ID_WELL_TILE, well_tile.get_class_identifier());
 }
 
