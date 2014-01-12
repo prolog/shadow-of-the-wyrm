f0f2414994915c925d4f8cddc97f208a1a846a48
Julian Day <jcd748@mail.usask.ca>
2014-01-12 09:18:02 -0600

    Added Stonewall (currently empty) northwest of Atua-Elar.

--- world/tiles/source/TileIDs.cpp
+++ world/tiles/source/TileIDs.cpp
@@ -19,3 +19,6 @@ const string TileCustomMapIDs::CUSTOM_MAP_ID_FAR_SHORE = "far_shore";
 const string TileCustomMapIDs::CUSTOM_MAP_ID_GNORDVAR = "gnordvar";
 const string TileCustomMapIDs::CUSTOM_MAP_ID_FOREST_OF_YEW = "forest_of_yew";
 const string TileCustomMapIDs::CUSTOM_MAP_ID_ATUA_ELAR = "atuaelar";
+const string TileCustomMapIDs::CUSTOM_MAP_ID_STONEWALL = "stonewall";
+
+
