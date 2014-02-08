f09bc7ebd910a7f88628be97c4fbb10b7fb58dc3
Julian Day <jcd748@mail.usask.ca>
2014-02-08 08:01:46 -0600

    Added random placements of creatures/items, and added random spellbooks to the library.

--- engine/XML/custom_maps/include/XMLMapReaders.hpp
+++ engine/XML/custom_maps/include/XMLMapReaders.hpp
@@ -11,6 +11,7 @@ class XMLMapReader
     MapPtr get_custom_map(const XMLNode& custom_map_node);
 
   protected:
+    std::string parse_id(const XMLNode& parent_node);
     Dimensions parse_dimensions(const XMLNode& dimensions_node);
     void parse_initial_placements(const XMLNode& initial_placements_node, MapPtr map);
     void parse_initial_creature_placements(const XMLNode& creatures_node, MapPtr map);
