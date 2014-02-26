23557fcba6aca419276883757d76e03af2498fa4
Julian Day <jcd748@mail.usask.ca>
2014-02-25 21:15:25 -0600

    Refactored the tile mapper out of a shared header.  Created a subclass that redefined its map(char)->TilePtr method, which will be used for mapping world map tiles.  Created the basic XML reader support for the world map areas.

---
+++ engine/XML/custom_maps/include/XMLWorldMapTileMapper.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "XMLTileMapper.hpp"
+
+// Does the tile mappings required for WorldMapAreas.xml - these will be
+// world map tiles (forest, field, sea, etc) rather than overworld/
+// underworld specific mappings.
+//
+// Basically, this allows re-using symbols because I'd prefer not to use
+// weird symbols (extended ascii/unicord/etc) if I don't have to.
+class XMLWorldMapTileMapper : public XMLTileMapper
+{
+  public:
+    virtual TilePtr create_tile(const char xml_tile);
+};
--- engine/XML/custom_maps/source/XMLMapTilesReader.cpp
+++ engine/XML/custom_maps/source/XMLMapTilesReader.cpp
@@ -3,6 +3,7 @@
 #include "TileGenerator.hpp"
 #include "SpringsTile.hpp"
 #include "XMLMapTilesReader.hpp"
+#include "XMLTileMapper.hpp"
 
 using namespace std;
 
@@ -53,104 +54,4 @@ TilesContainer XMLMapTilesReader::parse_tiles(const XMLNode& tiles_node, const i
   return tiles;
 }
 
-// Maps the XML tile (as a character) into a new TilePtr.
-TilePtr XMLTileMapper::create_tile(const char xml_tile)
-{
-  TilePtr tile;
-
-  if (xml_tile == '.')
-  {
-    tile = tg.generate(TILE_TYPE_DUNGEON);
-  }
-  else if (xml_tile == 'D')
-  {
-    tile = tg.generate(TILE_TYPE_DAIS);
-  }
-  else if (xml_tile == '<')
-  {
-    tile = tg.generate_staircase(STAIRCASE_UP);
-  }
-  else if (xml_tile == '>')
-  {
-    tile = tg.generate_staircase(STAIRCASE_DOWN);
-  }
-  else if (xml_tile == '#')
-  {
-    tile = tg.generate(TILE_TYPE_ROCK);
-  }
-  else if (xml_tile == '^')
-  {
-    tile = tg.generate(TILE_TYPE_MOUNTAINS);
-  }
-  else if (xml_tile == 'P')
-  {
-    tile = tg.generate(TILE_TYPE_PIER);
-  }
-  else if (xml_tile == 'T')
-  {
-    tile = tg.generate(TILE_TYPE_TREE);
-  }
-  else if (xml_tile == 'F')
-  {
-    tile = tg.generate(TILE_TYPE_FRUIT_TREE);
-  }
-  else if (xml_tile == ',')
-  {
-    tile = tg.generate(TILE_TYPE_FIELD);
-  }
-  else if (xml_tile == '*')
-  {
-    tile = tg.generate(TILE_TYPE_BUSH);
-  }
-  else if (xml_tile == '~')
-  {
-    tile = tg.generate(TILE_TYPE_RIVER);
-  }
-  else if (xml_tile == 'R')
-  {
-    tile = tg.generate(TILE_TYPE_ROCKY_EARTH);
-  }
-  else if (xml_tile == 'w')
-  {
-    tile = tg.generate(TILE_TYPE_WELL);
-  }
-  else if (xml_tile == 'r')
-  {
-    tile = tg.generate(TILE_TYPE_ROAD);
-  }
-  else if (xml_tile == 'S')
-  {
-    tile = tg.generate(TILE_TYPE_SEA);
-  }
-  else if (xml_tile == 's')
-  {
-    tile = tg.generate(TILE_TYPE_SHOALS);
-  }
-  else if (xml_tile == 'C')
-  {
-    tile = tg.generate(TILE_TYPE_CAIRN);
-  }
-  else if (xml_tile == '0')
-  {
-    tile = tg.generate(TILE_TYPE_BARROW);
-  }
-  else if ((xml_tile == '\'') || (xml_tile == '`'))
-  {
-    // Single quote indicates a regular freshwater spring.
-    tile = tg.generate(TILE_TYPE_SPRINGS);
-
-    // Backtick indicates hot springs.
-    if (xml_tile == '`')
-    {
-      std::shared_ptr<SpringsTile> springs_tile = std::dynamic_pointer_cast<SpringsTile>(tile);
-
-      if (springs_tile)
-      {
-        springs_tile->set_temperature(SPRINGS_TEMPERATURE_HOT);
-      }
-    }
-  }
-
-  return tile;
-}
 
