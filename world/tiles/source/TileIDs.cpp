169bc4c140e444b9d5fe092d270f78b195657508
Julian Day <jcd748@mail.usask.ca>
2014-02-26 13:13:37 -0600

    Removed custom map IDs and extra description SIDs from the cpp code, which were previously needed by the WorldGenerator - since Little Island is now handled by a Lua script, explicit cpp strings are no longer needd.

--- world/tiles/source/TileIDs.cpp
+++ world/tiles/source/TileIDs.cpp
@@ -10,10 +10,7 @@ TileCustomMapIDs::~TileCustomMapIDs()
 {
 }
 
-const string TileCustomMapIDs::CUSTOM_MAP_ID_ISEN_DUN = "isen_dun";
 const string TileCustomMapIDs::CUSTOM_MAP_ID_LAST_ISLAND_LAIR = "last_island_lair";
-const string TileCustomMapIDs::CUSTOM_MAP_ID_THE_BARROWS = "the_barrows";
-const string TileCustomMapIDs::CUSTOM_MAP_ID_WINTERSEA_KEEP = "wintersea_keep";
 const string TileCustomMapIDs::CUSTOM_MAP_ID_LAST_ROCK = "last_rock";
 const string TileCustomMapIDs::CUSTOM_MAP_ID_FAR_SHORE = "far_shore";
 const string TileCustomMapIDs::CUSTOM_MAP_ID_GNORDVAR = "gnordvar";
