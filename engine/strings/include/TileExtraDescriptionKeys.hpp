51d604b44a7a118f2263383db6fe3af56b592d3c
Julian Day <jcd748@mail.usask.ca>
2014-02-26 13:13:37 -0600

    Removed custom map IDs and extra description SIDs from the cpp code, which were previously needed by the WorldGenerator - since Little Island is now handled by a Lua script, explicit cpp strings are no longer needd.

--- engine/strings/include/TileExtraDescriptionKeys.hpp
+++ engine/strings/include/TileExtraDescriptionKeys.hpp
@@ -4,11 +4,7 @@
 class TileExtraDescriptionKeys
 {
   public:
-    static const std::string TILE_EXTRA_DESCRIPTION_ISEN_DUN;
-    static const std::string TILE_EXTRA_DESCRIPTION_ISEN_DUN_GRAVEYARD;
-    static const std::string TILE_EXTRA_DESCRIPTION_ISEN_DUN_DUNGEON;
     static const std::string TILE_EXTRA_DESCRIPTION_LAST_ISLAND_LAIR;
-    static const std::string TILE_EXTRA_DESCRIPTION_WINTERSEA_KEEP;
     static const std::string TILE_EXTRA_DESCRIPTION_INFINITE_DUNGEON;
     static const std::string TILE_EXTRA_DESCRIPTION_LAST_ROCK;
     static const std::string TILE_EXTRA_DESCRIPTION_FAR_SHORE;
--- world/tiles/include/TileIDs.hpp
+++ world/tiles/include/TileIDs.hpp
@@ -4,10 +4,7 @@
 class TileCustomMapIDs
 {
   public:
-    static const std::string CUSTOM_MAP_ID_ISEN_DUN;
     static const std::string CUSTOM_MAP_ID_LAST_ISLAND_LAIR;
-    static const std::string CUSTOM_MAP_ID_THE_BARROWS;
-    static const std::string CUSTOM_MAP_ID_WINTERSEA_KEEP;
     static const std::string CUSTOM_MAP_ID_LAST_ROCK;
     static const std::string CUSTOM_MAP_ID_FAR_SHORE;
     static const std::string CUSTOM_MAP_ID_GNORDVAR;
