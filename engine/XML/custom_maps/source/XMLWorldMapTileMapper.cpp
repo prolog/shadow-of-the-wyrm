633b37c9e111a67bb52e2a0f5fb06f92bb4969a1
Julian Day <jcd748@mail.usask.ca>
2014-02-27 18:22:11 -0600

    Moved Gnordvar into XML/Lua.

--- engine/XML/custom_maps/source/XMLWorldMapTileMapper.cpp
+++ engine/XML/custom_maps/source/XMLWorldMapTileMapper.cpp
@@ -51,6 +51,10 @@ TilePtr XMLWorldMapTileMapper::create_tile(const char xml_tile)
   {
     tile = tg.generate(TILE_TYPE_ROCKY_EARTH);
   }
+  else if (xml_tile == 'M')
+  { 
+    tile = tg.generate(TILE_TYPE_MINE);
+  }
   else
   {
     // So, the user's specified a tile that doesn't exist (I'm going to assume
