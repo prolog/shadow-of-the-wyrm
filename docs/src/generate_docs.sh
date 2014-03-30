57819c0e0585708aa94200d9397cee92d4e0f30d
Julian Day <jcd748@mail.usask.ca>
2014-03-30 09:48:57 -0500

    Added a command reference to the guide.  Broke out all the other sections into their own TeX files for easy inclusion/editing.  Moved around the commands in the basic command mapping to match the sections from the guide.

--- docs/src/generate_docs.sh
+++ docs/src/generate_docs.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 # Called twice due to the two passes needed for the table of contents.
+echo 'Generating the Guidebook.'
 pdflatex Guide.tex
 pdflatex Guide.tex
 mv Guide.pdf ..
+
+echo 'Generating the Command Reference.'
+pdflatex Commands.tex
+mv Commands.pdf ..
 
