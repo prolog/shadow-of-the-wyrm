9bbac8a601ac6f9c6e0fd6fd302f9d6ff4f0f75c
Julian Day <jcd748@mail.usask.ca>
2014-04-04 20:52:58 -0500

    Mild refactoring of ScoreCalculator.

--- engine/calculators/source/ScoreCalculator.cpp
+++ engine/calculators/source/ScoreCalculator.cpp
@@ -7,24 +7,42 @@ ulonglong ScoreCalculator::calculate_score(CreaturePtr creature)
 
   if (creature != nullptr)
   {
-    Inventory& inv = creature->get_inventory();
-
-    // Did the player win?
-    Mortuary& mortuary = creature->get_mortuary_ref();
-    if (mortuary.get_num_creature_killed("end_boss") > 0)
-    {
-      score += ScoreConstants::END_BOSS_BONUS;
-    }
-
-    // Spending ivory can help in game, but saving it helps after.
-    ItemPtr item = inv.get_from_base_id(ItemIdKeys::ITEM_ID_CURRENCY);
-    if (item != nullptr)
-    {
-      score += item->get_quantity();
-    }
+    update_score_end_boss(creature, score);
+    update_score_currency(creature, score);
+    update_score_experience(creature, score);
   }
 
   return score;
+}
+
+// Update the score based on whether the end boss was killed.
+void ScoreCalculator::update_score_end_boss(CreaturePtr creature, ulonglong& score)
+{
+  Mortuary& mortuary = creature->get_mortuary_ref();
+  if (mortuary.get_num_creature_killed("end_boss") > 0)
+  {
+    score += ScoreConstants::END_BOSS_BONUS;
+  }
+}
+
+// Update the score based on the number of ivory pieces accumulated.
+void ScoreCalculator::update_score_currency(CreaturePtr creature, ulonglong& score)
+{
+  Inventory& inv = creature->get_inventory();
+
+  // Spending ivory can help in game, but saving it helps after.
+  ItemPtr item = inv.get_from_base_id(ItemIdKeys::ITEM_ID_CURRENCY);
+  if (item != nullptr)
+  {
+    score += item->get_quantity();
+  }
+}
+
+void ScoreCalculator::update_score_experience(CreaturePtr creature, ulonglong& score)
+{
+  uint exp = creature->get_experience_points();
+
+  // ...
 }
 
 #ifdef UNIT_TESTS
