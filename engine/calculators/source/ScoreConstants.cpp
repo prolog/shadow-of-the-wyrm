bbd5aebf25f989425de0e9023e525faca83306a0
Julian Day <jcd748@mail.usask.ca>
2014-04-06 10:01:10 -0500

    Added an experience component to the score calculator, with appropriate unit tests.

--- engine/calculators/source/ScoreConstants.cpp
+++ engine/calculators/source/ScoreConstants.cpp
@@ -1,5 +1,7 @@
 #include "ScoreConstants.hpp"
 
+using namespace std;
+
 // Various constants used to help calculate the player's score when leaving
 // the game.
 
@@ -8,4 +10,7 @@ const uint ScoreConstants::END_BOSS_BONUS = 500000;
 
 // How much is each level worth?
 const uint ScoreConstants::LEVEL_MULTIPLIER = 100;
+
+// How much is each experience point after a certain amount worth?
+const vector<pair<uint, double>> ScoreConstants::EXPERIENCE_TIER_MULTIPLIERS{ {1000000, 0.05}, {100000, 0.1}, {10000, 0.2}, {0, 1.0} };
 
--- engine/calculators/source/unit_tests/ScoreCalculator_test.cpp
+++ engine/calculators/source/unit_tests/ScoreCalculator_test.cpp
@@ -31,6 +31,22 @@ TEST(SL_Engine_Calculators_ScoreCalculator, currency_component)
   EXPECT_EQ(12345, sc.calculate_score(cp));
 }
 
+TEST(SL_Engine_Calculators_ScoreCalculator, experience_component)
+{
+  Creature c;
+  CreaturePtr cp(new Creature(c));
+
+  cp->set_experience_points(400000);
+
+  ScoreCalculator sc;
+
+  EXPECT_EQ(58000, sc.calculate_score(cp));
+
+  cp->set_experience_points(1234567);
+
+  EXPECT_EQ(129728, sc.calculate_score(cp));
+}
+
 TEST(SL_Engine_Calculators_ScoreCalculator, level_component)
 {
   Creature c;
@@ -42,4 +58,35 @@ TEST(SL_Engine_Calculators_ScoreCalculator, level_component)
   ScoreCalculator sc;
 
   EXPECT_EQ(3600, sc.calculate_score(cp));
+}
+
+TEST(SL_Engine_Calculators_ScoreCalculator, total_score)
+{
+  Creature c;
+  CreaturePtr cp(new Creature(c));
+
+  // exp
+  cp->set_experience_points(400000);
+
+  // currency
+  CurrencyPtr currency = std::make_shared<Currency>();
+  currency->set_base_id(ItemIdKeys::ITEM_ID_CURRENCY);
+  currency->set_quantity(12345);
+
+  Inventory& inv = cp->get_inventory();
+  inv.add(currency);
+
+  // end boss
+  Mortuary& m = cp->get_mortuary_ref();
+  m.add_creature_kill("end_boss");
+
+  // level
+  Statistic level(36);
+  cp->set_level(level);
+
+  ScoreCalculator sc;
+
+  int expected_score = ScoreConstants::END_BOSS_BONUS + 58000 + 12345 + 3600;
+
+  EXPECT_EQ(expected_score, sc.calculate_score(cp));
 }
