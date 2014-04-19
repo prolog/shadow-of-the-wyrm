07a9dd4fda73a4c65f1522a8484b72705e0820a3
Julian Day <jcd748@mail.usask.ca>
2014-04-19 09:55:10 -0500

    Generate the main room and staircase for crypts.

--- engine/generators/include/CryptGenerator.hpp
+++ engine/generators/include/CryptGenerator.hpp
@@ -12,6 +12,11 @@ class CryptGenerator : public SL::Generator
   protected:
     virtual bool get_permanence_default() const override;
 
+    // Creates the central crypt with the up-staircase.  The two coordinates refer
+    // to the top-left and lower-right bounds, and the direction is the side of
+    // the crypt on which the up-staircase has been placed.
+    std::tuple<CardinalDirection, Coordinate, Coordinate> generate_central_crypt(MapPtr map);
+
     virtual TilePtr generate_tile(MapPtr current_map, const int row, const int col);
 
     TileGenerator tg;
