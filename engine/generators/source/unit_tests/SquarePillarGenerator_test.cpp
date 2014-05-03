79383f62f668ad44a2198bc2eb3532ddcfa83db2
Julian Day <jcd748@mail.usask.ca>
2014-05-03 12:56:55 -0500

    Fix a bug with square pillar height/width.

--- engine/generators/source/unit_tests/SquarePillarGenerator_test.cpp
+++ engine/generators/source/unit_tests/SquarePillarGenerator_test.cpp
@@ -11,13 +11,20 @@ TEST(SL_World_Generators_SquarePillarGenerator, expected_shape)
   SquarePillarGenerator spg;
   spg.generate(map, 0, 0);
 
-  for (int i = 0; i <= spg.get_height(); i++)
+  for (int i = 0; i <= spg.get_height() - 1; i++)
   {
-    for (int j = 0; j <= spg.get_width(); j++)
+    for (int j = 0; j <= spg.get_width() - 1; j++)
     {
       TilePtr tile = map->at(i, j);
       EXPECT_TRUE(tile->get_tile_type() == TILE_TYPE_ROCK);
     }
   }
+
+  // Check the edges to make sure the number of tiles is correct.
+  TilePtr tile = map->at(0, 2);
+  EXPECT_FALSE(tile->get_tile_type() == TILE_TYPE_ROCK);
+
+  tile = map->at(2, 0);
+  EXPECT_FALSE(tile->get_tile_type() == TILE_TYPE_ROCK);
 }
 
