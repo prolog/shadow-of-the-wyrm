e1f919453af1b682a81757f85ca2d8a9ba38f0bf
Julian Day <jcd748@mail.usask.ca>
2014-05-04 10:01:05 -0500

    Additional refactoring to help with code separation.

--- engine/generators/crypts/include/VaultCryptLayoutStrategy.hpp
+++ engine/generators/crypts/include/VaultCryptLayoutStrategy.hpp
@@ -10,8 +10,8 @@ class VaultCryptLayoutStrategy : public ICryptLayoutStrategy
     virtual std::pair<Coordinate, Coordinate> generate_vault(MapPtr map, const std::tuple<Coordinate, Coordinate, Coordinate>& stair_loc_and_room_boundary);
     virtual Coordinate get_door_location(const Direction d, const int mid_height, const int mid_width, const int vh, const int vw);
     virtual void generate_vault_entrance(MapPtr map, const int mid_height, const int mid_width, const int vault_height, const int vault_width, const Coordinate& stair_coord);
-    virtual void populate_vault(MapPtr map, const Coordinate& vault_topleft, const Coordinate& vault_bottomright);
-    virtual void populate_vault_creatures(MapPtr map, const std::set<Coordinate>& vault_coords);
+    virtual void populate_vault(MapPtr map, const TileType tile_type, const Coordinate& vault_topleft, const Coordinate& vault_bottomright);
+    virtual void populate_vault_creatures(MapPtr map, const TileType tile_type, const std::set<Coordinate>& vault_coords);
     virtual void populate_vault_items(MapPtr map, const std::set<Coordinate>& vault_coords);
 
   private:
