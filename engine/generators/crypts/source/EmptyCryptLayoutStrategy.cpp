90191ad37c87e8fffdb00e338dbee9c721398ad4
Julian Day <jcd748@mail.usask.ca>
2014-05-07 18:45:43 -0500

    Add grave generation over empty crypt layouts, sometimes.

--- engine/generators/crypts/source/EmptyCryptLayoutStrategy.cpp
+++ engine/generators/crypts/source/EmptyCryptLayoutStrategy.cpp
@@ -1,8 +1,26 @@
+#include "CoordUtils.hpp"
 #include "EmptyCryptLayoutStrategy.hpp"
+#include "OrderedGraveyardGenerator.hpp"
+#include "RNG.hpp"
 
 using namespace std;
 
 void EmptyCryptLayoutStrategy::create_layout(MapPtr map, const tuple<Coordinate, Coordinate, Coordinate>& stair_loc_and_room_boundary)
 {
+  // 50% chance of graves within the crypt
+  if (RNG::percent_chance(50))
+  { 
+    Coordinate top_left = std::get<1>(stair_loc_and_room_boundary);
+    Coordinate bottom_right = std::get<2>(stair_loc_and_room_boundary);
+
+    // Adjust top-left and bottom-right for the graves:
+    top_left.first +=1;
+    top_left.second +=1;
+    bottom_right.first -= 1;
+    bottom_right.second -= 1;
+
+    OrderedGraveyardGenerator ogg(map->get_map_id());
+    ogg.add_graves(map, top_left.first, bottom_right.first, top_left.second, bottom_right.second);
+  }
 }
 
--- engine/generators/source/ScatteredGraveyardGenerator.cpp
+++ engine/generators/source/ScatteredGraveyardGenerator.cpp
@@ -14,15 +14,22 @@ void ScatteredGraveyardGenerator::add_graves(MapPtr map)
   Dimensions dim = map->size();
   int rows = dim.get_y();
   int cols = dim.get_x();
-  int rand;
 
-  for (int row = 2; row < rows-2; row++)
+  int start_row = 2;
+  int end_row = rows - 3;
+  int start_col = 2;
+  int end_col = cols - 3;
+
+  add_graves(map, start_row, end_row, start_col, end_col);
+}
+
+void ScatteredGraveyardGenerator::add_graves(MapPtr map, const int start_row, const int end_row, const int start_col, const int end_col)
+{
+  for (int row = start_row; row <= end_row; row++)
   {
-    for (int col = 2; col < cols-2; col++)
+    for (int col = start_col; col <= end_col; col++)
     {
-      rand = RNG::range(1, 100);
-
-      if (rand < 10)
+      if (RNG::percent_chance(10))
       {
         TilePtr grave = tg.generate(TILE_TYPE_GRAVE);
         map->insert(row, col, grave);
@@ -30,4 +37,3 @@ void ScatteredGraveyardGenerator::add_graves(MapPtr map)
     }
   }
 }
-
