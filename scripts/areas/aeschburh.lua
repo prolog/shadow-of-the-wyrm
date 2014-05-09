ce3ec94844225102df18c5d8031518c3d4e01ea8
Julian Day <jcd748@mail.usask.ca>
2014-05-09 07:18:17 -0500

    Add (empty) Aeschburh map, with associated descriptions and Lua support.

---
+++ scripts/areas/aeschburh.lua
@@ -0,0 +1,7 @@
+require('areas')
+
+local aeschburh = Area:new(84, 84)
+aeschburh:set_custom_map_id("aeschburh")
+aeschburh:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_AESCHBURH")
+
+aeschburh:insert()
