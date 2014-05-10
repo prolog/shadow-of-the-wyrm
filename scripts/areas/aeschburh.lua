e0e78a6cf56d5444099ceac9f9ac0389d6559a77
Julian Day <jcd748@mail.usask.ca>
2014-05-10 09:28:13 -0500

    Added (empty) Aeschburh north-west of Isen Dun.

--- scripts/areas/aeschburh.lua
+++ scripts/areas/aeschburh.lua
@@ -1,6 +1,6 @@
 require('areas')
 
-local aeschburh = Area:new(84, 84)
+local aeschburh = Area:new(84, 83)
 aeschburh:set_custom_map_id("aeschburh")
 aeschburh:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_AESCHBURH")
 
