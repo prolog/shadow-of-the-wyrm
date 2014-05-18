a48aa51d683cecf7fa8d2babc1b468b58b8ebbde
Julian Day <jcd748@mail.usask.ca>
2014-05-18 10:01:49 -0500

    Created a second quest for Cynwise, to slay the servants of Sceadugenga at AAeschburh.

--- scripts/quests/cynwise.lua
+++ scripts/quests/cynwise.lua
@@ -37,7 +37,7 @@ local function cynwise_wintersea_completion_fn()
   return true
 end
 
-cynwise_quest = Quest:new("cynwise_wintersea",
+wintersea_quest = Quest:new("cynwise_wintersea",
                          "CYNWISE_WINTERSEA_QUEST_TITLE_SID",
                          "CYNWISE_DESCRIPTION_SID",
                          "CYNWISE_WINTERSEA_DESCRIPTION_SID",
@@ -48,6 +48,44 @@ cynwise_quest = Quest:new("cynwise_wintersea",
                          cynwise_wintersea_completion_condition_fn,
                          cynwise_wintersea_completion_fn)
 
-if cynwise_quest:execute() == false then
-  add_message("CYNWISE_SPEECH_TEXT_SID")
+-- Aeschburh quest: find the source of the blight in Aeschburh, and
+-- extinguish it.
+local function cynwise_burh_start_fn()
+  add_message_with_pause("CYNWISE_AESCHBURH_QUEST_START_SID")
+  add_message_with_pause("CYNWISE_AESCHBURH_QUEST_START2_SID")
+  add_message_with_pause("CYNWISE_AESCHBURH_QUEST_START3_SID")
+  clear_and_add_message("CYNWISE_AESCHBURH_QUEST_START4_SID")
 end
+
+local function cynwise_burh_completion_fn()
+  -- Reward is the Shroud of Cynehild
+  add_message_with_pause("CYNWISE_AESCHBURH_QUEST_COMPLETE_SID")
+  clear_and_add_message("CYNWISE_AESCHBURH_QUEST_COMPLETE2_SID")
+  add_object_to_player_tile("cynehild_shroud")
+  return true
+end
+
+local function cynwise_burh_completion_condition_fn()
+  return (get_num_creature_killed_global("pirrha") > 0) 
+end
+
+cynwise_aeschburh_quest = Quest:new("cynwise_aeschburh_quest",
+                                    "CYNWISE_AESCHBURH_QUEST_TITLE_SID",
+                                    "CYNWISE_SHORT_DESCRIPTION_SID",
+                                    "CYNWISE_AESCHBURH_DESCRIPTION_SID",
+                                    "CYNWISE_AESCHBURH_QUEST_COMPLETE_SID",
+                                    "CYNWISE_AESCHBURH_QUEST_REMINDER_SID",
+                                    truefn,
+                                    cynwise_burh_start_fn,
+                                    cynwise_burh_completion_condition_fn,
+                                    cynwise_burh_completion_fn)
+
+
+-- Handle quest ordering for Cynwise.
+-- The Wintersea quest comes first, followed by Aeschburh.
+if wintersea_quest:execute() == false then
+  if cynwise_aeschburh_quest:execute() == false then
+    add_message("CYNWISE_SPEECH_TEXT_SID")
+  end
+end
+
