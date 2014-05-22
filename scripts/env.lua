aa0fd5b49366e8c1dddb420f12b876d94158a9bb
Julian Day <jcd748@mail.usask.ca>
2014-05-22 07:16:01 -0500

    Updated the Aeschburh generation to include both set creatures, and a list of creatures to be generated randomly (no support for random generation on an already-existant map yet, though).

--- scripts/env.lua
+++ scripts/env.lua
@@ -1,5 +1,6 @@
 -- Update the environment to always check the "scripts" directory.
 package.path = package.path .. ";./scripts/?.lua"
+package.path = package.path .. ";./scripts/common/?.lua"
 package.path = package.path .. ";./scripts/quests/?.lua"
 package.path = package.path .. ";./scripts/level/?.lua"
 package.path = package.path .. ";./scripts/areas/?.lua"
