c895b030ce6899c3d87d168206e0294866d124b0
Julian Day <jcd748@mail.usask.ca>
2014-05-31 08:46:39 -0500

    Created new creatures: caravan guards, fae musicians, and fae travellers.  Added all of these to the Fae Caravan.

--- scripts/areas/fae_caravan.lua
+++ scripts/areas/fae_caravan.lua
@@ -1,10 +1,19 @@
 require('areas')
+local creature = require('creature')
 
 -- The caravan has no fixed location, appearing instead on one of the
 -- outer island tiles.
 local caravan_locs = {{6, 53}, {8, 53}, {7, 52}, {7, 54}}
 local coords = caravan_locs[RNG_range(1, #caravan_locs)]
 local creatures_csv = "caravan_master"
+
+local creature_list = {{"caravan_guard", 7, 10},
+                       {"fae_traveller", 6, 9},
+                       {"fae_musician", 5, 7},
+                       {"caravan_master", 1, 1}}
+
+local creatures_csv = creature.to_csv(creature_list)
+
 
 local fae_caravan = Area:new(coords[1], coords[2])
 fae_caravan:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_FAE_CARAVAN")
