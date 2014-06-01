441382ca79a17f176055f9831bd733ff3dc50ad6
Julian Day <jcd748@mail.usask.ca>
2014-06-01 09:21:33 -0500

    Added the summoners to be used in Cithriel.  Created a new artifact, Erandu, given by the caravan master on completion of the Cithriel quest.  Created the basic quest script for the caravan master and hooked it up, but the quest can't be completed yet - there is no Cithriel.

---
+++ scripts/quests/caravan_master.lua
@@ -0,0 +1,37 @@
+require('quest')
+
+-- Quest to clear the ancient underground fortress of Cithriel.
+local function caravan_master_cithriel_start_fn()
+  add_message_with_pause("CARAVAN_MASTER_CITHRIEL_QUEST_START_SID")
+  add_message_with_pause("CARAVAN_MASTER_CITHRIEL_QUEST_START2_SID")
+  add_message_with_pause("CARAVAN_MASTER_CITHRIEL_QUEST_START3_SID")
+  add_message_with_pause("CARAVAN_MASTER_CITHRIEL_QUEST_START4_SID")
+  clear_and_add_message("CARAVAN_MASTER_CITHRIEL_QUEST_START5_SID")
+end
+
+local function caravan_master_cithriel_completion_condition_fn()
+  return (get_num_creature_killed_global("cithriel_summoner") == 5)
+end
+
+local function caravan_mater_cithriel_completion_fn()
+  add_message_with_pause("CARAVAN_MASTER_CITHRIEL_QUEST_COMPLETE_SID")
+  clear_and_add_message("CARAVAN_MASTER_CITHRIEL_QUEST_COMPLETE2_SID")
+  add_object_to_player_tile("erandu")
+  return true
+end
+
+cithriel_quest = Quest:new("cithriel_quest",
+                           "CARAVAN_MASTER_CITHRIEL_QUEST_TITLE_SID",
+                           "CARAVAN_MASTER_DESCRIPTION_SID",
+                           "CARAVAN_MASTER_CITHRIEL_DESCRIPTION_SID",
+                           "CARAVAN_MASTER_CITHRIEL_QUEST_COMPLETE_SID",
+                           "CARAVAN_MASTER_CITHRIEL_QUEST_REMINDER_SID",
+                           truefn,
+                           caravan_master_cithriel_start_fn,
+                           caravan_master_cithriel_completion_condition_fn,
+                           caravan_master_cithriel_completion_fn)
+
+if cithriel_quest:execute() == false then
+  add_message("CARAVAN_MASTER_SPEECH_TEXT_SID")
+end
+
