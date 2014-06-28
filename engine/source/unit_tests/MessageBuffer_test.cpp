44c3d045dbcefb519c23795971658dd4a2d1169a
Julian Day <jcd748@mail.usask.ca>
2014-06-27 22:28:15 -0500

    Updated MessageBuffer so that new messages are added to the front, not the back; and old messages are removed from the back, not the front.

--- engine/source/unit_tests/MessageBuffer_test.cpp
+++ engine/source/unit_tests/MessageBuffer_test.cpp
@@ -8,15 +8,15 @@ TEST(SL_Engine_MessageBuffer, insertion)
   mb.add_message("b");
   mb.add_message("c");
 
-  EXPECT_EQ("c", mb.get_message(2));
+  EXPECT_EQ("a", mb.get_message(2));
 
   mb.add_message("d");
   mb.add_message("e");
 
   // "a" and "b" should have been removed:
-  EXPECT_EQ("c", mb.get_message(0));
+  EXPECT_EQ("e", mb.get_message(0));
   EXPECT_EQ("d", mb.get_message(1));
-  EXPECT_EQ("e", mb.get_message(2));
+  EXPECT_EQ("c", mb.get_message(2));
 
   EXPECT_EQ(3, mb.capacity());
   EXPECT_EQ(3, mb.size());
