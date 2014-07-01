17bb781cb4f53bd8272703767849854345f39f90
Julian Day <jcd748@mail.usask.ca>
2014-07-01 10:41:30 -0500

    Created the basic map for the base level of Zaeda's Tower.  Placed it at the southern tip of the Snake Lands, and created the key (a rune stone), which will be guarded within the snakeling settlement.

--- scripts/areas/snake_lands.lua
+++ scripts/areas/snake_lands.lua
@@ -1,1 +1,17 @@
+require('areas')
+
+local snake_lands = {}
+
+-- Zaeda's Tower, recently emerging from its shroud of illusion, sits on
+-- the southern tip of the Snake Lands, the only dry region of the
+-- island.
+local zaedas_tower = Area:new(5, 95)
+zaedas_tower:set_custom_map_id("zaedas_tower")
+zaedas_tower:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_ZAEDAS_TOWER")
+
+table.insert(snake_lands, zaedas_tower)
+
+for k,v in pairs(snake_lands) do
+  v:insert()
+end
 
