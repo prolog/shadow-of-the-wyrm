1b800d1aca7f221dac7e1b469e763bd3226090a4
Julian Day <jcd748@mail.usask.ca>
2014-07-13 18:54:51 -0500

    Minor textual updates.  Created Altus, added him to the base of Zaeda's Tower, and gave him a quest to open the tower and slay Zaeda - the reward is a powerful longbow.

---
+++ scripts/quests/altus.lua
@@ -0,0 +1,39 @@
+require('quest')
+
+-- Quest to retrieve the rune stone and defeat the sorceror Zaeda.
+local function altus_quest_start_fn()
+  add_message_with_pause("ALTUS_QUEST_START_SID")
+  add_message_with_pause("ALTUS_QUEST_START2_SID")
+  clear_and_add_message("ALTUS_QUEST_START3_SID")
+end
+
+local function altus_quest_completion_condition_fn()
+  -- JCD FIXME: At some point, it would be nice to have the victory
+  -- condition require that Zaeda be defeated within some sort of time
+  -- frame - maybe a day or two - before Altus succumbs to the poison.
+  return (get_num_creature_killed_global("zaeda") > 0)
+end
+
+local function altus_quest_completion_fn()
+  add_message_with_pause("ALTUS_QUEST_COMPLETE_SID")
+  clear_and_add_message("ALTUS_QUEST_COMPLETE2_SID")
+
+  add_object_to_player_tile("lora")
+  return true
+end
+
+christof_mine_quest = Quest:new("altus_quest",
+                                "ALTUS_QUEST_TITLE_SID",
+                                "ALTUS_DESCRIPTION_SID",
+                                "ALTUS_QUEST_DESCRIPTION_SID",
+                                "ALTUS_QUEST_COMPLETE_SID",
+                                "ALTUS_QUEST_REMINDER_SID",
+                                truefn,
+                                altus_quest_start_fn,
+                                altus_quest_completion_condition_fn,
+                                altus_quest_completion_fn)
+
+if christof_mine_quest:execute() == false then
+  add_message("CHRISTOF_SPEECH_TEXT_SID")
+end
+
