e98269ffd07de0333dee4e73edfd4ea00864ccd7
Julian Day <jcd748@mail.usask.ca>
2014-07-26 08:42:21 -0500

    Creatures can now be poisoned when eating poisoned food or quaffing a poisoned potion.

--- world/include/ConsumableConstants.hpp
+++ world/include/ConsumableConstants.hpp
@@ -5,6 +5,7 @@ class ConsumableConstants
 {
   public:
     static const std::string CORPSE_DESCRIPTION_SID;
+    static const int FOOD_POISON_APPLICATION_BONUS;
 
   protected:
     ConsumableConstants();
--- world/source/ConsumableConstants.cpp
+++ world/source/ConsumableConstants.cpp
@@ -14,3 +14,7 @@ ConsumableConstants::~ConsumableConstants()
 // indicate that the item is actually a corpse.
 const string ConsumableConstants::CORPSE_DESCRIPTION_SID = "CORPSE_DESCRIPTION_SID";
 
+// Food poisoning is more of a sure thing than is getting hit by a 
+// poisoned blade.
+const int ConsumableConstants::FOOD_POISON_APPLICATION_BONUS = 30;
+
