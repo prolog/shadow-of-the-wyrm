5c53e0e19d14b63ccac5f0b11c34758573038a1d
Julian Day <jcd748@mail.usask.ca>
2014-08-22 07:15:39 -0500

    TileUtils class with a function to return the "opposite" tile type.  This returns the opposite staircase tile type when the input is a staircase tile, and returns the input otherwise.

---
+++ engine/maps/source/unit_tests/TileUtils_test.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+TEST(SL_Engine_Maps_TileUtils, get_opposite_tile_type)
+{
+  EXPECT_EQ(TILE_TYPE_UP_STAIRCASE, TileUtils::get_opposite_tile_type(TILE_TYPE_DOWN_STAIRCASE));
+
+  EXPECT_EQ(TILE_TYPE_DOWN_STAIRCASE, TileUtils::get_opposite_tile_type(TILE_TYPE_UP_STAIRCASE));
+
+  EXPECT_EQ(TILE_TYPE_CASTLE, TileUtils::get_opposite_tile_type(TILE_TYPE_CASTLE));
+
+  EXPECT_EQ(TILE_TYPE_FIELD, TileUtils::get_opposite_tile_type(TILE_TYPE_FIELD));
+}
+
