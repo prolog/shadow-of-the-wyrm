0cdce15855a2d74f860c2e7f421597c5138614cb
Julian Day <jcd748@mail.usask.ca>
2014-08-23 08:10:02 -0500

    Frogot to add TileUtils last time.  Refactored a bit of code so that map exits are only being added via the appropriate MapUtils function, rather than directly.

---
+++ engine/maps/source/TileUtils.cpp
@@ -0,0 +1,32 @@
+#include "TileUtils.hpp"
+
+TileUtils::TileUtils()
+{
+}
+
+TileUtils::~TileUtils()
+{
+}
+
+// The "opposite" tile type really only makes sense for staircase tiles,
+// where the opposite of an up staircase is a down staircase, and vice
+// versa.  For every other tile type, the opposite is the tile type
+// provided as a parameter.
+TileType TileUtils::get_opposite_tile_type(const TileType tt)
+{
+  if (tt == TILE_TYPE_UP_STAIRCASE)
+  {
+    return TILE_TYPE_DOWN_STAIRCASE;
+  }
+  else if (tt == TILE_TYPE_DOWN_STAIRCASE)
+  {
+    return TILE_TYPE_UP_STAIRCASE;
+  }
+
+  return tt;
+}
+
+#ifdef UNIT_TESTS
+#include "unit_tests/TileUtils_test.cpp"
+#endif
+
