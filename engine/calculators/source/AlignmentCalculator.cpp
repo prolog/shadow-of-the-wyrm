002b2a60087824fc5dcbcedf7ef679546b1f0ed4
Julian Day <jcd748@mail.usask.ca>
2014-10-16 07:22:46 -0500

    Calculation for alignment loss when sacrificing on a cross-aligned altar, with unit tests.

--- engine/calculators/source/AlignmentCalculator.cpp
+++ engine/calculators/source/AlignmentCalculator.cpp
@@ -1,6 +1,12 @@
 #include "AlignmentCalculator.hpp"
 #include "Alignment.hpp"
 
+const int AlignmentCalculator::BASE_ALIGNMENT_DELTA_COALIGNED = 10;
+const int AlignmentCalculator::BASE_ALIGNMENT_DELTA_CROSSALIGNED = 250;
+const float AlignmentCalculator::BASE_ALIGNMENT_MULTIPLIER_CROSSALIGNED = 1.0;
+
+// Sacrificing on a co-aligned altar adjusts a creature's alignment slightly,
+// drawing the alignment closer to the absolute value for the range.
 int AlignmentCalculator::calculate_alignment_for_sacrifice_on_coaligned_altar(int current_alignment, const AlignmentRange altar_alignment_range)
 {
   int new_alignment = current_alignment;
@@ -9,25 +15,71 @@ int AlignmentCalculator::calculate_alignment_for_sacrifice_on_coaligned_altar(in
   switch (altar_alignment_range)
   {
     case ALIGNMENT_RANGE_EVIL:
-      new_alignment -= 10;
+      new_alignment -= BASE_ALIGNMENT_DELTA_COALIGNED;
       break;
     case ALIGNMENT_RANGE_GOOD:
-      new_alignment += 10;
+      new_alignment += BASE_ALIGNMENT_DELTA_COALIGNED;
       break;
     case ALIGNMENT_RANGE_NEUTRAL:
       if (new_alignment > true_neutral)
       {
-        new_alignment -= 10;
+        new_alignment -= BASE_ALIGNMENT_DELTA_COALIGNED;
       }
       else if (new_alignment < true_neutral)
       {
-        new_alignment += 10;
+        new_alignment += BASE_ALIGNMENT_DELTA_COALIGNED;
       }
       break;
     default:
       break;
   }
   
+  return new_alignment;
+}
+
+// Sacrificing on a cross-aligned altar is risky, because it changes the
+// creature's alignment; this can cause the creature to cross an alignment
+// barrier, causing them to forsake their deity, become a fallen champion
+// (if applicable), etc.
+int AlignmentCalculator::calculate_alignment_for_sacrifice_on_crossaligned_altar(int current_alignment, const AlignmentRange current_alignment_range, const AlignmentRange altar_alignment_range)
+{
+  int new_alignment = current_alignment;
+  float multiplier = BASE_ALIGNMENT_MULTIPLIER_CROSSALIGNED;
+
+  if (current_alignment_range == altar_alignment_range)
+  {
+    return current_alignment;
+  }
+
+  // Crossing two alignment barriers causes a greater drift.
+  if ((current_alignment_range == ALIGNMENT_RANGE_GOOD && altar_alignment_range == ALIGNMENT_RANGE_EVIL) ||
+      (current_alignment_range == ALIGNMENT_RANGE_EVIL && altar_alignment_range == ALIGNMENT_RANGE_GOOD))
+  {
+    multiplier *= 1.5;
+  }
+
+  switch (current_alignment_range)
+  {
+    case ALIGNMENT_RANGE_GOOD:
+      new_alignment -= static_cast<int>(BASE_ALIGNMENT_DELTA_CROSSALIGNED * multiplier);
+      break;
+    case ALIGNMENT_RANGE_EVIL:
+      new_alignment += static_cast<int>(BASE_ALIGNMENT_DELTA_CROSSALIGNED * multiplier);
+      break;
+    case ALIGNMENT_RANGE_NEUTRAL:
+      if (altar_alignment_range == ALIGNMENT_RANGE_GOOD)
+      {
+        new_alignment += static_cast<int>(BASE_ALIGNMENT_DELTA_CROSSALIGNED * multiplier);
+      }
+      else
+      {
+        new_alignment -= static_cast<int>(BASE_ALIGNMENT_DELTA_CROSSALIGNED * multiplier);
+      }
+      break;
+    default:
+      break;
+  }
+
   return new_alignment;
 }
 
--- engine/calculators/source/unit_tests/AlignmentCalculator_test.cpp
+++ engine/calculators/source/unit_tests/AlignmentCalculator_test.cpp
@@ -20,3 +20,52 @@ TEST(SL_Engine_Calculators_AlignmentCalculator, calculate_alignment_for_sacrific
 
   EXPECT_EQ(-1276, ac.calculate_alignment_for_sacrifice_on_coaligned_altar(alignment, ALIGNMENT_RANGE_EVIL));
 }
+
+TEST(SL_Engine_Calculators_AlignmentCalculator, calculate_alignment_for_sacrifice_on_crossaligned_altar)
+{
+  AlignmentCalculator ac;
+
+  // This should never happen, but:
+  // Cross-sacrificing as the same alignment should not change the current
+  // value.
+  EXPECT_EQ(1000, ac.calculate_alignment_for_sacrifice_on_crossaligned_altar(1000, ALIGNMENT_RANGE_GOOD, ALIGNMENT_RANGE_GOOD));
+  EXPECT_EQ(0, ac.calculate_alignment_for_sacrifice_on_crossaligned_altar(0, ALIGNMENT_RANGE_NEUTRAL, ALIGNMENT_RANGE_NEUTRAL));
+  EXPECT_EQ(-1000, ac.calculate_alignment_for_sacrifice_on_crossaligned_altar(-1000, ALIGNMENT_RANGE_EVIL, ALIGNMENT_RANGE_EVIL));
+
+  // Cross-sacrificing as good.
+  int alignment = 1000;
+  AlignmentRange cur_range = ALIGNMENT_RANGE_GOOD;
+  AlignmentRange alt_range = ALIGNMENT_RANGE_NEUTRAL;
+
+  EXPECT_EQ(750, ac.calculate_alignment_for_sacrifice_on_crossaligned_altar(alignment, cur_range, alt_range));
+
+  alt_range = ALIGNMENT_RANGE_EVIL;
+  int exp_value = (1000 - static_cast<int>(250 * 1.5));
+  EXPECT_EQ(exp_value, ac.calculate_alignment_for_sacrifice_on_crossaligned_altar(alignment, cur_range, alt_range));
+
+  // Cross-sacrificing as neutral.
+  alignment = 200;
+  cur_range = ALIGNMENT_RANGE_NEUTRAL;
+  alt_range = ALIGNMENT_RANGE_GOOD;
+  exp_value = 450;
+
+  EXPECT_EQ(exp_value, ac.calculate_alignment_for_sacrifice_on_crossaligned_altar(alignment, cur_range, alt_range));
+
+  alt_range = ALIGNMENT_RANGE_EVIL;
+  exp_value = -50;
+
+  EXPECT_EQ(exp_value, ac.calculate_alignment_for_sacrifice_on_crossaligned_altar(alignment, cur_range, alt_range));
+
+  // Cross-sacrificing as evil.
+  alignment = -1000;
+  cur_range = ALIGNMENT_RANGE_EVIL;
+  alt_range = ALIGNMENT_RANGE_NEUTRAL;
+  exp_value = -750;
+
+  EXPECT_EQ(exp_value, ac.calculate_alignment_for_sacrifice_on_crossaligned_altar(alignment, cur_range, alt_range));
+
+  alt_range = ALIGNMENT_RANGE_GOOD;
+  exp_value = (-1000 + static_cast<int>(250 * 1.5));
+
+  EXPECT_EQ(exp_value, ac.calculate_alignment_for_sacrifice_on_crossaligned_altar(alignment, cur_range, alt_range));
+}
