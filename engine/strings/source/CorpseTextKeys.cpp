70271747799c7fb1c4cc5c72b9d964469bc9476e
Julian Day <jcd748@mail.usask.ca>
2014-11-17 14:24:11 -0600

    Show the type of skin in the inventory (imp skin, rat skin, etc) instead of just "a skin".

--- engine/strings/source/CorpseTextKeys.cpp
+++ engine/strings/source/CorpseTextKeys.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 const string CorpseTextKeys::CORPSE_MESSAGE = "CORPSE_MESSAGE";
+const string CorpseTextKeys::SKIN_MESSAGE = "SKIN_MESSAGE";
 
 CorpseTextKeys::CorpseTextKeys()
 {
@@ -16,7 +17,7 @@ CorpseTextKeys::~CorpseTextKeys()
 
 // Create the corpse description.  This will be something like 
 // "A goblin corpse".
-string CorpseTextKeys::get_corpse_description(const std::string& creature_desc)
+string CorpseTextKeys::get_corpse_description(const string& creature_desc)
 {
   string corpse_desc;
 
@@ -26,3 +27,12 @@ string CorpseTextKeys::get_corpse_description(const std::string& creature_desc)
   return corpse_desc;
 }
 
+string CorpseTextKeys::get_skin_description(const string& creature_desc)
+{
+  string corpse_desc;
+
+  corpse_desc = StringTable::get(SKIN_MESSAGE);
+  boost::replace_first(corpse_desc, "%s", creature_desc);
+
+  return corpse_desc;
+}
