363094f0f5a1fbd1f8c6186900399efd216ce381
Julian Day <jcd748@mail.usask.ca>
2014-11-18 07:13:38 -0600

    When the player applies a tannery, first prompt to select a skin, and then an equipment slot.

--- engine/features/source/TanneryManipulator.cpp
+++ engine/features/source/TanneryManipulator.cpp
@@ -6,6 +6,7 @@
 #include "MessageManagerFactory.hpp"
 #include "RNG.hpp"
 #include "SkinningConstants.hpp"
+#include "SkinSelectionScreen.hpp"
 
 using namespace std;
 
@@ -24,12 +25,20 @@ void TanneryManipulator::kick(CreaturePtr creature, MapPtr current_map, TilePtr 
   }
 }
 
+// Manipulating a tannery allows turning skins into armour.
+// The player can select a skin, then an armour slot, and
+// an appropriate piece of armour is created.  The resistances
+// on the skin are added to the armour based on the creature's
+// Tanning skill.
 bool TanneryManipulator::handle(TilePtr tile, CreaturePtr creature)
 {
   bool handled = false;
 
   if (creature && tile)
   {
+    Game& game = Game::instance();
+    ActionManager& am = game.get_action_manager_ref();
+
     // Check for skins
     if (!creature->get_inventory()->has_item_with_property(SkinningConstants::SKIN_DESCRIPTION_SID))
     {
@@ -37,7 +46,18 @@ bool TanneryManipulator::handle(TilePtr tile, CreaturePtr creature)
     }
     else
     {
-      // ...
+      vector<pair<string, string>> item_property_filter = {make_pair(SkinningConstants::SKIN_DESCRIPTION_SID, "")};
+      list<IItemFilterPtr> display_filter_list = ItemFilterFactory::create_item_property_type_filter(item_property_filter);
+
+      ItemPtr selected_skin = am.inventory(creature, creature->get_inventory(), display_filter_list, false);
+
+      if (selected_skin)
+      {
+        SkinSelectionScreen sss(game.get_display());
+        string slot_selection = sss.display();
+
+        int x = 1;
+      }
     }
   }
 
