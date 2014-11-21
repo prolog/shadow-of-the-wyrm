ea084940c1e870e59bacb2bad0a1ed3f489d6e17
Julian Day <jcd748@mail.usask.ca>
2014-11-20 21:01:59 -0600

    Updated corpse descriptions so that they differentiated between regular and usage descriptions ("fox's corpse" vs. "a fox's corpse"

--- world/include/ConsumableConstants.hpp
+++ world/include/ConsumableConstants.hpp
@@ -5,6 +5,7 @@ class ConsumableConstants
 {
   public:
     static const std::string CORPSE_DESCRIPTION_SID;
+	static const std::string CORPSE_SHORT_DESCRIPTION_SID;
     static const std::string CORPSE_RACE_ID;
     static const int FOOD_POISON_APPLICATION_BONUS;
 
