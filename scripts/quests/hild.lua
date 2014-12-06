a415da3e3f8a91a81f3f5cbe8fb9e629d2d06615
Julian Day <jcd748@mail.usask.ca>
2014-12-06 08:45:59 -0600

    Renamed Hildred to Hild.

---
+++ scripts/quests/hild.lua
@@ -0,0 +1,5 @@
+-- Fill in quest later.
+
+-- if hild_quest:execute() == false then
+add_message("HILD_SPEECH_TEXT_SID")
+-- end
--- scripts/quests/hildred.lua
+++
@@ -1,5 +0,0 @@
--- Fill in quest later.
-
--- if hildred_quest:execute() == false then
-add_message("HILDRED_SPEECH_TEXT_SID")
--- end
