052f2e3ec798f7dead58f331f2447fd9e4a2a04d
Julian Day <jcd748@mail.usask.ca>
2014-12-07 10:34:50 -0600

    When the player has completed Hild's quest, remove the whiteflower from the inventory.

--- world/source/NullInventory.cpp
+++ world/source/NullInventory.cpp
@@ -38,7 +38,7 @@ bool NullInventory::remove(const string& id)
   return false;
 }
 
-bool NullInventory::remove_by_base_id(const string& base_id)
+bool NullInventory::remove_by_base_id(const string& base_id, const int quantity)
 {
   return false;
 }
