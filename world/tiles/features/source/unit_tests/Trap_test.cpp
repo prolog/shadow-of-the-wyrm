a178cd17eb3a28c475d92b1e20671a48ded601b1
Julian Day <jcd748@mail.usask.ca>
2014-12-23 09:21:23 -0600

    Added damage and description SIDs to traps.

--- world/tiles/features/source/unit_tests/Trap_test.cpp
+++ world/tiles/features/source/unit_tests/Trap_test.cpp
@@ -7,3 +7,21 @@ TEST(SL_World_Tiles_Features_Trap, serialization_id)
   EXPECT_EQ(CLASS_ID_TRAP, trap.get_class_identifier());
 }
 
+TEST(SL_World_Tiles_Features_Trap, saveload)
+{
+  Trap t1, t2;
+  Damage d1(5, 5, 7, DAMAGE_TYPE_COLD, false, false, 0, {});
+
+  t1.set_damage(d1);
+  t1.set_description_sid("abc2465");
+
+  EXPECT_FALSE(t1 == t2);
+
+  ostringstream oss;
+  t1.serialize(oss);
+
+  istringstream iss(oss.str());
+  t2.deserialize(iss);
+
+  EXPECT_TRUE(t1 == t2);
+}
