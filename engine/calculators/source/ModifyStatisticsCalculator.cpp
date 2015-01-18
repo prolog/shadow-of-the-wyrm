840f6f52df675fe0dd10395f611c45d613d4d397
Julian Day <jcd748@mail.usask.ca>
2015-01-18 10:52:19 -0600

    Modify the mean length of the Modify Statistics effect to 20 minutes.

--- engine/calculators/source/ModifyStatisticsCalculator.cpp
+++ engine/calculators/source/ModifyStatisticsCalculator.cpp
@@ -1,12 +1,12 @@
 #include "ModifyStatisticsCalculator.hpp"
 #include "Random.hpp"
 
-const int ModifyStatisticsCalculator::BASE_MODIFY_STATISTICS_DURATION_MEAN = 60;
+const int ModifyStatisticsCalculator::BASE_MODIFY_STATISTICS_DURATION_MEAN = 20;
 
 // Calculate the duration in minutes.
 int ModifyStatisticsCalculator::calculate_duration() const
 {
-  // Statistics modification lasts around an hour or so.
+  // Statistics modification lasts around twenty minutes.
   PoissonDistribution p(BASE_MODIFY_STATISTICS_DURATION_MEAN);
   int duration = p.next();
 
