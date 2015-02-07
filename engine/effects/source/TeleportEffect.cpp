da287b94eb5fd2bd92960b007004480c23c4f139
Julian Day <jcd748@mail.usask.ca>
2015-02-07 10:45:32 -0600

    Added "Blink" to Cantrips.  Updated the teleport effect so that blinking could be specified - blinking will only move within the creature's view map, instead of the entire current map.

--- engine/effects/source/TeleportEffect.cpp
+++ engine/effects/source/TeleportEffect.cpp
@@ -1,15 +1,22 @@
+#include <boost/range/adaptors.hpp>
+#include <boost/range/algorithm.hpp>
 #include "AdditionalEffectMessages.hpp"
+#include "Conversion.hpp"
 #include "Creature.hpp"
 #include "EffectTextKeys.hpp"
 #include "Game.hpp"
+#include "Log.hpp"
 #include "MapUtils.hpp"
 #include "MessageManager.hpp"
 #include "RNG.hpp"
 #include "TeleportEffect.hpp"
 
-using std::string;
+using namespace std;
+
+const string TeleportEffect::TELEPORT_BLINK = "TELEPORT_BLINK";
 
 TeleportEffect::TeleportEffect()
+: blink_effect(false)
 {
 }
 
@@ -49,33 +56,105 @@ bool TeleportEffect::teleport(CreaturePtr creature)
 {
   Game& game = Game::instance();
   MapPtr map = game.get_current_map();
-  Dimensions dim = map->size();
-  int rows = dim.get_y();
-  int cols = dim.get_x();
 
   // The creature's original tile.
   TilePtr old_tile = map->at(map->get_location(creature->get_id()));
 
   bool teleported = false;
 
-  // Make a number of attempts to teleport.
-  for (int i = 0; i < 100; i++)
+  if (blink_effect)
   {
-    int row = RNG::range(0, rows);
-    int col = RNG::range(0, cols);
+    // Blink just teleports in the field of view.
+    teleported = blink(creature, map, old_tile);
+  }
+  else
+  {
+    // Proper teleporting can go anywhere on the map.
+    teleported = teleport(creature, map, old_tile);
+  }
 
-    Coordinate c(row, col);
-    TilePtr tile = map->at(c);
+  return teleported;
+}
 
-    if (tile && !tile->get_creature() && !tile->get_is_blocking(creature))
+void TeleportEffect::read_properties(const map<string, string>& properties)
+{
+  auto p_it_blink = properties.find(TELEPORT_BLINK);
+
+  if (p_it_blink != properties.end())
+  {
+    try
     {
-      MapUtils::add_or_update_location(map, creature, c, old_tile);
-      teleported = true;
+      blink_effect = String::to_bool(p_it_blink->second);
+    }
+    catch (...)
+    {
+      Log::instance().error("Could not convert blink effect value!");
+    }
+  }
+}
 
-      // Originally, I didn't have the break statement below.
-      // End result?  Lots of player shared_ptrs all over the map!
-      // Hilarity ensues!
-      break;
+bool TeleportEffect::blink(CreaturePtr creature, MapPtr current_map, TilePtr old_tile)
+{
+  bool teleported = false;
+
+  if (creature && current_map && old_tile)
+  {
+    vector<string> keys;
+    MapPtr fov_map = creature->get_decision_strategy()->get_fov_map();
+    TilesContainer tiles = fov_map->get_tiles();
+
+    for (const auto& pair : tiles)
+    {
+      keys.push_back(pair.first);
+    }
+
+    shuffle(keys.begin(), keys.end(), RNG::get_engine());
+
+    while (!keys.empty() && !teleported)
+    {
+      string k = keys.back();
+      Coordinate c = MapUtils::convert_map_key_to_coordinate(k);
+      TilePtr tile = tiles.find(k)->second;
+
+      if (tile && MapUtils::is_tile_available_for_creature(creature, tile))
+      {
+        MapUtils::add_or_update_location(current_map, creature, c, old_tile);
+        teleported = true;
+      }
+
+      keys.pop_back();
+    }
+  }
+
+  return teleported;
+}
+
+bool TeleportEffect::teleport(CreaturePtr creature, MapPtr map, TilePtr old_tile)
+{
+  bool teleported = false;
+
+  if (creature && map && old_tile)
+  {
+    Dimensions dim = map->size();
+    int rows = dim.get_y();
+    int cols = dim.get_x();
+
+    // Make a number of attempts to teleport.
+    for (int i = 0; i < 100; i++)
+    {
+      int row = RNG::range(0, rows);
+      int col = RNG::range(0, cols);
+
+      Coordinate c(row, col);
+      TilePtr tile = map->at(c);
+
+      if (tile && !tile->get_creature() && !tile->get_is_blocking(creature))
+      {
+        MapUtils::add_or_update_location(map, creature, c, old_tile);
+        teleported = true;
+
+        break;
+      }
     }
   }
 
