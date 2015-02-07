ff7dc3f8659063547e0d28a7e9402d597fcd3cb0
Julian Day <jcd748@mail.usask.ca>
2015-02-07 10:45:32 -0600

    Added "Blink" to Cantrips.  Updated the teleport effect so that blinking could be specified - blinking will only move within the creature's view map, instead of the entire current map.

--- engine/effects/include/TeleportEffect.hpp
+++ engine/effects/include/TeleportEffect.hpp
@@ -7,6 +7,8 @@ class TeleportEffect : public Effect
     TeleportEffect();
     
     virtual std::string get_effect_identification_message(std::shared_ptr<Creature> creature) const override;
+    virtual void read_properties(const std::map<std::string, std::string>& properties) override;
+
     virtual Effect* clone() override;
 
   protected:
@@ -15,5 +17,14 @@ class TeleportEffect : public Effect
     virtual bool effect_blessed(std::shared_ptr<Creature> creature, ActionManager * const am) override;
     virtual bool effect_uncursed(std::shared_ptr<Creature> creature, ActionManager * const am) override;
     virtual bool effect_cursed(std::shared_ptr<Creature> creature, ActionManager * const am) override;
+
+    // Teleport anywhere on the map
+    virtual bool teleport(std::shared_ptr<Creature> creature, std::shared_ptr<Map> current_map, TilePtr old_tile);
+
+    // Teleport within the creature's view map
+    virtual bool blink(std::shared_ptr<Creature> creature, std::shared_ptr<Map> current_map, TilePtr old_tile);
+
+    bool blink_effect;
+    static const std::string TELEPORT_BLINK;
 };
 
