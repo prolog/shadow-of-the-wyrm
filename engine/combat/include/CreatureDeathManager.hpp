9c89e77e7b63d09182ed0a543684dd1a5faf174b
Julian Day <jcd748@mail.usask.ca>
2015-02-19 07:15:19 -0600

    Refactored CreatureDeathManager::die() into a number of smaller functions.

--- engine/combat/include/CreatureDeathManager.hpp
+++ engine/combat/include/CreatureDeathManager.hpp
@@ -8,5 +8,12 @@ class CreatureDeathManager : public DeathManager
     ~CreatureDeathManager();
     
     void die() const override;
+
+  protected:
+    void add_creature_death_messages(CreaturePtr attacking_creature, CreaturePtr dead_creature) const;
+    void remove_creature_equipment_and_drop_inventory_on_tile(MapPtr map, CreaturePtr dead_creature, IInventoryPtr ground) const;
+    void potentially_generate_random_drop(CreaturePtr dead_creature, IInventoryPtr ground) const;
+    void potentially_generate_corpse(CreaturePtr attacking_creature, CreaturePtr dead_creature, IInventoryPtr ground) const;
+
 };
 
