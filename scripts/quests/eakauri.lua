7920895975d1978e5c15e7ff0375b446b879a304
Julian Day <jcd748@mail.usask.ca>
2015-03-14 08:26:31 -0500

    Added Eakauri, the undying.

---
+++ scripts/quests/eakauri.lua
@@ -0,0 +1,8 @@
+-- Monologue.
+add_message_with_pause("EKAURI_SPEECH_TEXT_SID")
+add_message_with_pause("EKAURI_SPEECH_TEXT2_SID")
+add_message("EKAURI_SPEECH_TEXT3_SID")
+
+-- Apparently, tens of thousands of years beneath the sea make an undying
+-- astral creature a little cranky.
+-- set_hostility("super_boss", "player")
