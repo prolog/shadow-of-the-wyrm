a2bb8bbd52badcc13a42bd54e13c5b4a3e323410
Julian Day <jcd748@mail.usask.ca>
2015-03-15 08:48:17 -0500

    Moved the item-reading functionality in XMLClassesReader and XMLInitialItemReader into XMLReader.  Made XMLClassesReader inherit from XMLReader, and deleted XMLInitialItemsReader.  This is in preparation for having creatures have XML-definable equipment/inventory, so that these can be read either from a Creature node or from a Class node.  Added an initial item to Eakauri, but this doesn't load, yet.

--- engine/XML/include/XMLReader.hpp
+++ engine/XML/include/XMLReader.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <map>
+#include "Class.hpp"
 #include "Damage.hpp"
 #include "XMLDataStructures.hpp"
 
@@ -12,4 +13,17 @@ class XMLReader
     void parse_damage(Damage& damage, const XMLNode& damage_node) const;
     void parse_status_ailments(StatusAilments& ailments, const XMLNode& status_ailments_node) const;
     void parse_properties(std::map<std::string, std::string>& properties_map, const XMLNode& properties_node) const;
+
+    void parse_initial_equipment_and_inventory(ClassPtr current_class, const XMLNode& initial_equipment_and_inventory_node);
+    void parse_initial_equipment(ClassPtr current_class, const XMLNode& initial_equipment_node);
+    void parse_initial_inventory(ClassPtr current_class, const XMLNode& initial_inventory_node);
+
+    InitialItem get_initial_item(const XMLNode& initial_item_node);
+
+  protected:
+    // Parse the schema's Quantity Amount/Random into a Dice object.
+    Dice parse_quantity(const XMLNode& quantity_node) const;
+
+    std::vector<std::pair<std::string, Dice>> get_random_item_ids(const XMLNode& initial_random_node) const;
+    std::map<std::string, std::pair<std::string, Dice>> get_racial_item_ids(const XMLNode& initial_racial_node) const;
 };
