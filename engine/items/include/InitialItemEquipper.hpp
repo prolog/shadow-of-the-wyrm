ce51f24bb2c5d13aff5afc09819e51ed978cc252
Julian Day <jcd748@mail.usask.ca>
2015-03-20 22:50:06 -0500

    Creatures now can have equipment/items at the Creature level, instead of just the Class level.

--- engine/items/include/InitialItemEquipper.hpp
+++ engine/items/include/InitialItemEquipper.hpp
@@ -8,4 +8,8 @@ class InitialItemEquipper
   public:
     void equip(CreaturePtr creature, const CreatureGenerationValues& cgv, ActionManager& am);
     void add_inventory_items(CreaturePtr creature, const CreatureGenerationValues& cgv, ActionManager& am);
+
+  protected:
+    void process_initial_equipment(CreaturePtr creature, const std::map<EquipmentWornLocation, InitialItem>& initial_eq, ActionManager& am);
+    void process_initial_inventory(CreaturePtr creature, const std::vector<InitialItem>& initial_inv, ActionManager& am);
 };
