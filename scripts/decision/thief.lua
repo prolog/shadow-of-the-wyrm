5f5d06dce2162f7589e4cac7dab9368f491de4a5
Julian Day <jcd748@mail.usask.ca>
2015-04-19 09:59:22 -0500

    Fix up logging statements.

--- scripts/decision/thief.lua
+++ scripts/decision/thief.lua
@@ -3,11 +3,7 @@ require('actions')
 
 -- Thieves scavenge money off the ground.
 local function thief_decision_fn(creature_id)
-  log(CLOG_ERROR, "Here")
   local take_currency_fn = actions.take_currency
-  log(CLOG_ERROR, "Here2")
-  local decision_fns = {take_currency_fn}
-  log(CLOG_ERROR, "Here3")
   return decision_fns
 end
 
