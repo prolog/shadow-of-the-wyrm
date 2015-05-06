1d3916e8b9be905fd4afa810f5c421de8073d749
Julian Day <jcd748@mail.usask.ca>
2015-05-06 11:28:07 -0500

    Finalize v0.3.5, start v0.3.6

--- engine/source/Metadata.cpp
+++ engine/source/Metadata.cpp
@@ -42,8 +42,8 @@ string Metadata::get_version() const
 
 string Metadata::get_code_name() const
 {
-  // 20th century Greek guitar composer.
-  return "\"Tzortzinakis\"";
+  // Renaissance vihuela composer, best known for his pavanes.
+  return "\"Milan\"";
 }
 
 string Metadata::get_game_version_synopsis() const
