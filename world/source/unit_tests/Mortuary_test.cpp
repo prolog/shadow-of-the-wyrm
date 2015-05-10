c0c5bb771fe7c09d9b2c8afd22b7428a9fdc49c8
Julian Day <jcd748@mail.usask.ca>
2015-05-10 09:54:41 -0500

    Updated Mortuary unit tests.

--- world/source/unit_tests/Mortuary_test.cpp
+++ world/source/unit_tests/Mortuary_test.cpp
@@ -7,6 +7,7 @@ void add_creatures_to_mortuary(Mortuary& m)
 {
   m.add_creature_kill("metal_slime");
   m.add_creature_kill("metal_slime");
+  m.add_creature_kill("urok", true);
 
   m.add_creature_kill("dragon_cat_of_doom");
 }
@@ -21,7 +22,16 @@ TEST(SL_World_Mortuary, death_toll)
   EXPECT_EQ(1, m.get_num_creature_killed("dragon_cat_of_doom"));
   EXPECT_EQ(0, m.get_num_creature_killed("snipe"));
 
-  EXPECT_EQ(3, m.get_num_creatures_killed());
+  EXPECT_EQ(4, m.get_num_creatures_killed());
+}
+
+TEST(SL_World_Mortuary, num_uniques_killed)
+{
+  Mortuary m;
+
+  add_creatures_to_mortuary(m);
+
+  EXPECT_EQ(1, m.get_num_uniques_killed());
 }
 
 TEST(SL_World_Mortuary, serialization_id)
@@ -46,5 +56,7 @@ TEST(SL_World_Mortuary, saveload)
   m2.deserialize(iss);
 
   EXPECT_TRUE(m == m2);
+
+  EXPECT_EQ(1, m2.get_num_uniques_killed());
 }
 
