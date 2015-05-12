f5553eb03b9d9bea20d680af901b0e09ff8b076c
Julian Day <jcd748@mail.usask.ca>
2015-05-12 07:12:54 -0500

    Refactor a big function into a slightly less big function.

--- engine/time/source/CreatureSkillIncrementer.cpp
+++ engine/time/source/CreatureSkillIncrementer.cpp
@@ -25,37 +25,55 @@ void CreatureSkillIncrementer::tick(CreaturePtr creature, const ulonglong minute
       Skills& skills = creature->get_skills();
       map<SkillType, SkillPtr>& raw_skills = skills.get_raw_skills();
       
+      IMessageManager& manager = MessageManagerFactory::instance(creature, creature && creature->get_is_player());
+
       // If the skill has been marked enough, increment the value of the skill, display a message
       // (when the creature is the player), and then reset the number of marks back to zero.
       for (map<SkillType, SkillPtr>::iterator sk_it = raw_skills.begin(); sk_it != raw_skills.end(); sk_it++)
       {
         SkillPtr skill = sk_it->second;
-        int skill_threshold = skill->get_threshold();
-        int skill_value = skill->get_value();
-        
-        // Skills aren't automatically learned when at 0 and enough success/failure occurs.  This is meant to be a model
-        // similar to ADOM and not DCSS, and is absolutely intentional.  Classes matter, and the initial skill set is
-        // important. 
-        if (skill->get_marks() > skill_threshold)
+        bool added_msg = update_skill_if_necessary(creature, skill, manager);
+
+        if (added_msg)
         {
-          if ((skill_value == 0 && (skill->can_train_from_unlearned())) || (skill_value > 0))
-          {
-            // Update the value, which also updates the threshold.
-            skill->set_value(skill_value + 1);
-            
-            if (creature->get_is_player())
-            {
-              IMessageManager& manager = MessageManagerFactory::instance(creature, creature && creature->get_is_player());
-  
-              string skill_increase_message = StringTable::get(skill->get_skill_increment_message_sid());
-              manager.add_new_message(skill_increase_message);
-              manager.send();
-            }
-            
-            skill->set_marks(0);
-          }
+          manager.send();
         }
       }
     }
   }
+}
+
+bool CreatureSkillIncrementer::update_skill_if_necessary(CreaturePtr creature, SkillPtr skill, IMessageManager& manager)
+{
+  bool trained_skill = false;
+
+  int skill_threshold = skill->get_threshold();
+  int skill_value = skill->get_value();
+  int skill_marks = skill->get_marks();
+
+  // Skills aren't automatically learned when at 0 and enough success/failure occurs.  This is meant to be a model
+  // similar to ADOM and not DCSS, and is absolutely intentional.  Classes matter, and the initial skill set is
+  // important. 
+  while (skill_marks > skill_threshold)
+  {
+    if ((skill_value == 0 && (skill->can_train_from_unlearned())) || (skill_value > 0))
+    {
+      // Update the value, which also updates the threshold.
+      skill->set_value(skill_value + 1);
+
+      if (creature->get_is_player())
+      {
+        string skill_increase_message = StringTable::get(skill->get_skill_increment_message_sid());
+        manager.add_new_message(skill_increase_message);
+        trained_skill = true;
+      }
+    }
+
+    // Must decrement by at least 1 (i.e., when the skill is initially
+    // at 0 points).
+    skill_marks -= std::min(skill_threshold, 1);
+  }
+
+  skill->set_marks(skill_marks);
+  return trained_skill;
 }
