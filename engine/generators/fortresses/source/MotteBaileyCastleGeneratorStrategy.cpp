d0f4bf7f6937a4c5286a4ede7f1ca5bf478f0191
Julian Day <jcd748@mail.usask.ca>
2015-05-19 07:09:24 -0500

    Fix castle generation bugs.

--- engine/generators/fortresses/source/MotteBaileyCastleGeneratorStrategy.cpp
+++ engine/generators/fortresses/source/MotteBaileyCastleGeneratorStrategy.cpp
@@ -21,11 +21,12 @@ void MotteBaileyCastleGeneratorStrategy::generate(MapPtr castle_map)
   generate_motte(castle_map, motte_height, motte_width);
 }
 
-// Generate the moat around the castle structure
+// Generate the moat around the castle structure, generated on the
+// centre of the map.
 void MotteBaileyCastleGeneratorStrategy::generate_moat(MapPtr castle_map, const int moat_radius)
 {
   Dimensions dim = castle_map->size();
-  GeneratorUtils::generate_circle(castle_map, dim.get_y(), dim.get_x(), moat_radius, TileType::TILE_TYPE_RIVER);
+  GeneratorUtils::generate_circle(castle_map, dim.get_y()/2, dim.get_x()/2, moat_radius, TileType::TILE_TYPE_RIVER);
 }
 
 // Generate the motte (the keep).
@@ -39,8 +40,8 @@ void MotteBaileyCastleGeneratorStrategy::generate_motte(MapPtr map, const int mo
 
   // Generate doors on each wall. (N, S, E, W)
   vector<Coordinate> door_coords = { {start_row, start_col + (motte_width/2) }, 
-                                     {start_row + motte_height, start_col + (motte_width/2)}, 
-                                     {start_row + (motte_height/2), start_col + motte_width}, 
+                                     {start_row + motte_height-1, start_col + (motte_width/2)}, 
+                                     {start_row + (motte_height/2), start_col + motte_width - 1}, 
                                      {start_row + (motte_height/2), start_col} };
   for (const Coordinate& c : door_coords)
   {
