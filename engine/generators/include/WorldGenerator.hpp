7b8dfba65ef459beabd3034b6fdbeb56294a0fef
Julian Day <jcd748@mail.usask.ca>
2015-05-26 07:28:47 -0500

    Added a few of the Isen Dun creatures as generic Village creatures.  Added guardsmen.

--- engine/generators/include/WorldGenerator.hpp
+++ engine/generators/include/WorldGenerator.hpp
@@ -37,6 +37,7 @@ class WorldGenerator : public SL::Generator
     void remove_village_coordinates_if_present(const Coordinate& c);
     void set_tile_properties(TilePtr tile, TileType tile_type, TileType tile_subtype, const int row, const int col);
     TilePtr generate_feature_or_default(const std::vector<std::pair<int, std::pair<TileType, TileType>>>& special_features, TileType default_tile_type, const int row, const int col);
+    void set_initial_creatures_for_village(TilePtr village_tile);
     
     std::set<Coordinate> village_coordinates;
     std::set<std::string> initial_race_ids;
