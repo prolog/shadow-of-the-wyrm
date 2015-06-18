87a23ac189ef704e327676d3edfc2c461b6b3acd
Julian Day <jcd748@mail.usask.ca>
2015-06-18 06:52:49 -0500

    Beds can now have their material set in the XML configuration.

--- engine/XML/custom_maps/features/source/XMLBedReader.cpp
+++ engine/XML/custom_maps/features/source/XMLBedReader.cpp
@@ -7,6 +7,12 @@ FeaturePtr XMLBedReader::create(const XMLNode& bed_node) const
 {
   FeaturePtr bed = FeatureGenerator::generate_bed();
 
+  if (!bed_node.is_null())
+  {
+    MaterialType material = static_cast<MaterialType>(XMLUtils::get_child_node_int_value(bed_node, "Material", static_cast<int>(MaterialType::MATERIAL_TYPE_WOOD)));
+    bed->set_material_type(material);
+  }
+
   return bed;
 }
 
