26a6c16f32f0361e43d202a1c4801d2f31e10241
Julian Day <jcd748@mail.usask.ca>
2015-06-27 08:59:31 -0500

    Added a CreatureUtils function to determine if the creature can pick up the proposed item.

---
+++ engine/creatures/source/unit_tests/CreatureUtils_test.cpp
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+#include "Amulet.hpp"
+
+TEST(SL_Engine_Creatures_CreatureUtils, can_pick_up)
+{
+  CreaturePtr c = std::make_shared<Creature>();
+  c->set_size(CreatureSize::CREATURE_SIZE_MEDIUM);
+  Statistic dex(10);
+  c->set_dexterity(dex);
+
+  ItemPtr amulet = std::make_shared<Amulet>();
+  amulet->set_quantity(90);
+
+  ItemPtr amulet2 = std::make_shared<Amulet>();
+  amulet2->set_quantity(3);
+
+  ItemPtr amulet3 = std::make_shared<Amulet>();
+  amulet3->set_quantity(30);
+
+  c->get_inventory()->add_front(amulet);
+
+  // Carrying capacity should be 100, so a proposed new total of 93
+  // is fine.
+  EXPECT_TRUE(CreatureUtils::can_pick_up(c, amulet2));
+
+  // Carrying capacity should be 100, so a proposed new total of 120
+  // should not be allowed.
+  EXPECT_FALSE(CreatureUtils::can_pick_up(c, amulet3));
+}
+
