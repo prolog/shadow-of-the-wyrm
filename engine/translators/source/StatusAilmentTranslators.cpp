5ea65f8d27c4f528e220c091c1f47fb8f0200b5b
Julian Day <jcd748@mail.usask.ca>
2015-06-28 08:32:13 -0500

    WeightStatusAilmentTranslator implementation

--- engine/translators/source/StatusAilmentTranslators.cpp
+++ engine/translators/source/StatusAilmentTranslators.cpp
@@ -79,6 +79,74 @@ Colour HungerStatusAilmentTranslator::get_status_ailment_colour(const CreaturePt
   return colour_map[HungerLevelConverter::to_hunger_level(creature->get_hunger_clock().get_hunger())];
 }
 
+
+// Weight
+map<BurdenLevel, string> WeightStatusAilmentTranslator::text_sid_map;
+map<BurdenLevel, Colour> WeightStatusAilmentTranslator::colour_map;
+
+void WeightStatusAilmentTranslator::initialize_text_sid_map()
+{
+  text_sid_map.clear();
+  text_sid_map = map<BurdenLevel, string>{{BurdenLevel::BURDEN_LEVEL_UNBURDENED, StatusAilmentTextKeys::STATUS_BURDEN_UNBURDENED},
+                                          {BurdenLevel::BURDEN_LEVEL_BURDENED, StatusAilmentTextKeys::STATUS_BURDEN_BURDENED},
+                                          {BurdenLevel::BURDEN_LEVEL_STRAINED, StatusAilmentTextKeys::STATUS_BURDEN_STRAINED},
+                                          {BurdenLevel::BURDEN_LEVEL_OVERBURDENED, StatusAilmentTextKeys::STATUS_BURDEN_OVERBURDENED}};
+}
+
+void WeightStatusAilmentTranslator::initialize_colour_map()
+{
+  colour_map.clear();
+  colour_map = map<BurdenLevel, Colour>{{BurdenLevel::BURDEN_LEVEL_UNBURDENED, Colour::COLOUR_WHITE},
+                                        {BurdenLevel::BURDEN_LEVEL_BURDENED, Colour::COLOUR_WHITE},
+                                        {BurdenLevel::BURDEN_LEVEL_STRAINED, Colour::COLOUR_WHITE},
+                                        {BurdenLevel::BURDEN_LEVEL_OVERBURDENED, Colour::COLOUR_BOLD_RED}};
+}
+
+// Check to see if the burden level is not unburdened.
+bool WeightStatusAilmentTranslator::has_ailment(const CreaturePtr& creature) const
+{
+  bool ailment = false;
+
+  if (creature)
+  {
+    BurdenLevel bl = BurdenLevelConverter::to_burden_level(creature);
+    ailment = (bl != BurdenLevel::BURDEN_LEVEL_UNBURDENED);
+  }
+
+  return ailment;
+}
+
+string WeightStatusAilmentTranslator::get_status_ailment_text(const CreaturePtr& creature) const
+{
+  string weight_text;
+
+  if (text_sid_map.empty())
+  {
+    initialize_text_sid_map();
+  }
+
+  if (has_ailment(creature))
+  {
+    weight_text = StringTable::get(text_sid_map[BurdenLevelConverter::to_burden_level(creature)]);
+  }
+
+  return weight_text;
+}
+
+Colour WeightStatusAilmentTranslator::get_status_ailment_colour(const CreaturePtr& creature) const
+{
+  Colour colour = Colour::COLOUR_WHITE;
+
+  if (colour_map.empty())
+  {
+    initialize_colour_map();
+  }
+
+  return colour_map[BurdenLevelConverter::to_burden_level(creature)];
+}
+
+// General status ailment.
+
 StatusAilmentTranslator::StatusAilmentTranslator(const string& id, const string& t_sid, const Colour c)
 : identifier(id), text_sid(t_sid), colour(c)
 {
