75d81d451aac50b067825e864c262f9aa51a527f
Julian Day <jcd748@mail.usask.ca>
2015-06-29 07:21:53 -0500

    Item unit test for matching.

--- world/source/unit_tests/Item_test.cpp
+++ world/source/unit_tests/Item_test.cpp
@@ -27,3 +27,18 @@ TEST(SL_World_Item, total_weight)
   EXPECT_EQ(48, amulet->get_total_weight().get_weight());
 }
 
+TEST(SL_World_Item, matches)
+{
+  ItemPtr amulet = std::make_shared<Amulet>();
+  amulet->set_description_sid("asdf");
+  amulet->set_usage_description_sid("aaa");
+  amulet->set_quantity(2);
+  Weight w(14);
+  amulet->set_weight(w);
+
+  ItemPtr amulet2 = std::make_shared<Amulet>();
+  *amulet2 = *amulet;
+
+  EXPECT_EQ(2, amulet2->get_quantity());
+  EXPECT_TRUE(amulet->matches(amulet2));
+}
