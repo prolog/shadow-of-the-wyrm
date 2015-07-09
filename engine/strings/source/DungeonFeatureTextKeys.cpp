2221b8317af40dc6edb363238709e52d55484f2c
Julian Day <jcd748@mail.usask.ca>
2015-07-08 23:22:56 -0500

    Resource strings for dungeon features, that will be displayed when the player enters a level with a particular feature.

---
+++ engine/strings/source/DungeonFeatureTextKeys.cpp
@@ -0,0 +1,18 @@
+#include "DungeonFeatureTextKeys.hpp"
+
+using namespace std;
+
+DungeonFeatureTextKeys::DungeonFeatureTextKeys()
+{
+}
+
+DungeonFeatureTextKeys::~DungeonFeatureTextKeys()
+{
+}
+
+const string DungeonFeatureTextKeys::DUNGEON_FEATURE_ALTAR = "DUNGEON_FEATURE_ALTAR";
+const string DungeonFeatureTextKeys::DUNGEON_FEATURE_TREASURE_ROOM = "DUNGEON_FEATURE_TREASURE_ROOM";
+const string DungeonFeatureTextKeys::DUNGEON_FEATURE_ZOO = "DUNGEON_FEATURE_ZOO";
+const string DungeonFeatureTextKeys::DUNGEON_FEATURE_REST_ROOM = "DUNGEON_FEATURE_REST_ROOM";
+const string DungeonFeatureTextKeys::DUNGEON_FEATURE_NODE = "DUNGEON_FEATURE_NODE";
+
