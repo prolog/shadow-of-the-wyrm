65136feb6d262a60da5364040d107b73ca173051
Julian Day <jcd748@mail.usask.ca>
2015-07-26 09:22:49 -0500

    lol :(

--- world/include/ConductTypes.hpp
+++ world/include/ConductTypes.hpp
@@ -7,7 +7,7 @@ enum struct ConductType
   CONDUCT_TYPE_AGNOSTIC = 2,
   CONDUCT_TYPE_ILLITERATE = 3,
   CONDUCT_TYPE_WEAPONLESS = 4, /* Never hits something with a weapon, rather than not wielding one. */
-  CONDCUT_TYPE_NO_GRAVEDIGGING = 5,
+  CONDUCT_TYPE_NO_GRAVEDIGGING = 5,
   CONDUCT_SIZE = 6
 };
 
