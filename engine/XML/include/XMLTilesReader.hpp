7c21912b45361e6cff245add2404dde010163690
Julian Day <jcd748@mail.usask.ca>
2015-07-30 06:53:57 -0500

    Made tile dig chances configurable in the XML game data.

--- engine/XML/include/XMLTilesReader.hpp
+++ engine/XML/include/XMLTilesReader.hpp
@@ -2,6 +2,7 @@
 #include "XMLDataStructures.hpp"
 #include <vector>
 
+class DigChances;
 class DisplayTile;
 
 class XMLTilesReader
@@ -11,4 +12,5 @@ class XMLTilesReader
 
   protected:
     void parse_tile_text_details(std::vector<DisplayTile>&, const XMLNode& tile_node);
+    void parse_dig_chances(DigChances& dc, const XMLNode& dig_chances_node);
 };
