3a1d669f6ba6e6492d7ee8141412930ba031d2e4
Julian Day <jcd748@mail.usask.ca>
2015-08-03 09:18:10 -0500

    TileDescriber now adds a message when examining a tile when that tile has been previously dug.

--- engine/description/source/TileDescriber.cpp
+++ engine/description/source/TileDescriber.cpp
@@ -1,5 +1,7 @@
 #include "TileDescriber.hpp"
+#include "Conversion.hpp"
 #include "StringTable.hpp"
+#include "TileTextKeys.hpp"
 
 using std::string;
 
@@ -15,6 +17,12 @@ string TileDescriber::describe() const
   if (tile)
   {
     description = StringTable::get(tile->get_tile_description_sid());
+
+    string prev_dug = tile->get_additional_property(TileProperties::TILE_PROPERTY_PREVIOUSLY_DUG);
+    if (!prev_dug.empty() && (String::to_bool(prev_dug) == true))
+    {
+      description = description + " " + StringTable::get(TileTextKeys::TILE_DESC_DUG);
+    }
 
     if (tile->has_extra_description())
     {
