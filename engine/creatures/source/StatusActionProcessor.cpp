28d5237f70fdb3068654ca4e71c9946adbfbcba6
Julian Day <jcd748@mail.usask.ca>
2015-08-16 09:34:45 -0500

    Timewalk wears off when taking a turn with an action cost.

---
+++ engine/creatures/source/StatusActionProcessor.cpp
@@ -0,0 +1,16 @@
+#include "StatusActionProcessor.hpp"
+#include "StatusEffectFactory.hpp"
+
+using namespace std;
+
+void StatusActionProcessor::process_action(CreaturePtr creature, CreatureStatusMap statuses_before_action, ActionCost action)
+{
+  bool is_timewalking = false;
+  auto s_it = statuses_before_action.find(StatusIdentifiers::STATUS_ID_TIMEWALK);
+
+  if (s_it != statuses_before_action.end() && s_it->second == true && action.get_cost() > 0)
+  {
+    StatusEffectPtr se = StatusEffectFactory::create_status_effect(StatusIdentifiers::STATUS_ID_TIMEWALK);    
+    se->undo_change(creature);
+  }
+}
