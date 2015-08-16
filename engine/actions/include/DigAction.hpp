a57db8096ac3ebf4c1603832c5992842c4de7e51
Julian Day <jcd748@mail.usask.ca>
2015-08-16 10:44:05 -0500

    Function support for digging through an adjacent tile (not implemented in this revision).

--- engine/actions/include/DigAction.hpp
+++ engine/actions/include/DigAction.hpp
@@ -4,11 +4,18 @@
 class DigAction : public IActionManager
 {
   public:
-    ActionCostValue dig(CreaturePtr creature, MapPtr map, TilePtr tile) const;
+    // Dig within an existing tile.  Eg, dig up a grave, a barrow, etc.
+    ActionCostValue dig_within(CreaturePtr creature, MapPtr map, TilePtr tile) const;
+
+    // Dig through an adjacent tile - break up the tile into another type.
+    // Eg, rock should become rocky earth, earth should become floor, etc.
+    ActionCostValue dig_through(CreaturePtr creature, MapPtr map, TilePtr adjacent_tile) const;
 
     ActionCostValue get_action_cost_value(CreaturePtr creature) const override;
 
   protected:
     friend class StairwayMovementAction;
+    friend class MovementAction;
+
     DigAction();
 };
