6b46ea6acac31c8a7411d66892de55bbe8c4a087
Julian Day <jcd748@mail.usask.ca>
2015-09-29 19:17:48 -0500

    Dropping on rock/earth tiles while incorporeal now destroys the item.

--- world/tiles/include/RockTile.hpp
+++ world/tiles/include/RockTile.hpp
@@ -4,10 +4,11 @@
 class RockTile : public Tile
 {
   public:
+    RockTile();
+
     TileType get_tile_type() const override;
     TileType get_decomposition_tile_type() const override;
     std::string get_decomposition_item_id() const override;
-    int get_hardness() const override;
     std::string get_tile_description_sid() const override;
     int get_movement_multiplier() const override;
 
