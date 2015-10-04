b14ad57658d99a541601aa7fac4b5f4d85c55f94
Julian Day <jcd748@mail.usask.ca>
2015-10-04 15:42:13 -0500

    More door kicking changes.

--- engine/calculators/include/DoorBreakageCalculator.hpp
+++ engine/calculators/include/DoorBreakageCalculator.hpp
@@ -10,10 +10,11 @@ class DoorBreakageCalculator
     int calculate_pct_chance_breakage(CreaturePtr creature, DoorPtr door);
 
   protected:
-    int get_base_strength_value(const MaterialType mt);
+    int get_base_strength_value(DoorPtr door);
 
     const std::map<MaterialType, int> material_base_strength_values;
     static const int DEFAULT_STR_BASE;
+    static const int DEFAULT_STR_LOCK;
     static const int STR_INCR_DIVISOR;
 };
 
--- engine/calculators/source/unit_tests/DoorBreakageCalculator_test.cpp
+++ engine/calculators/source/unit_tests/DoorBreakageCalculator_test.cpp
@@ -10,18 +10,18 @@ TEST(SL_Engine_Calculators_DoorBreakageCalculator, calculate_pct_chance_breakage
   DoorPtr wood_door = std::make_shared<Door>();
   wood_door->set_material_type(MaterialType::MATERIAL_TYPE_WOOD);
 
-  EXPECT_EQ(2, dbc.calculate_pct_chance_breakage(creature, wood_door));
+  EXPECT_EQ(17, dbc.calculate_pct_chance_breakage(creature, wood_door));
 
   DoorPtr bone_door = std::make_shared<Door>();
   bone_door->set_material_type(MaterialType::MATERIAL_TYPE_BONE);
 
-  EXPECT_EQ(2, dbc.calculate_pct_chance_breakage(creature, bone_door));
+  EXPECT_EQ(17, dbc.calculate_pct_chance_breakage(creature, bone_door));
 
   str.set_current(20);
   str.set_base(20);
   creature->set_strength(str);
 
-  EXPECT_EQ(3, dbc.calculate_pct_chance_breakage(creature, bone_door));
+  EXPECT_EQ(20, dbc.calculate_pct_chance_breakage(creature, bone_door));
 
   DoorPtr stone_door = std::make_shared<Door>();
   stone_door->set_material_type(MaterialType::MATERIAL_TYPE_STONE);
@@ -32,7 +32,7 @@ TEST(SL_Engine_Calculators_DoorBreakageCalculator, calculate_pct_chance_breakage
   str.set_base(60);
   creature->set_strength(str);
 
-  EXPECT_EQ(3, dbc.calculate_pct_chance_breakage(creature, stone_door));
+  EXPECT_EQ(10, dbc.calculate_pct_chance_breakage(creature, stone_door));
 
   DoorPtr iron_door = std::make_shared<Door>();
   iron_door->set_material_type(MaterialType::MATERIAL_TYPE_IRON);
@@ -43,17 +43,17 @@ TEST(SL_Engine_Calculators_DoorBreakageCalculator, calculate_pct_chance_breakage
   str.set_base(73);
   creature->set_strength(str);
 
-  EXPECT_EQ(4, dbc.calculate_pct_chance_breakage(creature, iron_door));
+  EXPECT_EQ(13, dbc.calculate_pct_chance_breakage(creature, iron_door));
 
   DoorPtr steel_door = std::make_shared<Door>();
   steel_door->set_material_type(MaterialType::MATERIAL_TYPE_STEEL);
 
-  EXPECT_EQ(1, dbc.calculate_pct_chance_breakage(creature, steel_door));
+  EXPECT_EQ(3, dbc.calculate_pct_chance_breakage(creature, steel_door));
 
   str.set_current(99);
   str.set_base(99);
   creature->set_strength(str);
 
-  EXPECT_EQ(9, dbc.calculate_pct_chance_breakage(creature, steel_door));
+  EXPECT_EQ(29, dbc.calculate_pct_chance_breakage(creature, steel_door));
 }
 
