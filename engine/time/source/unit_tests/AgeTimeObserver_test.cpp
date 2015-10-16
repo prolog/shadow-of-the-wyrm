6871c79ef93ca7af1bd6cfacaa4af78eacae759f
Julian Day <jcd748@mail.usask.ca>
2015-10-15 20:46:30 -0500

    Added an annual time observer for handling aging.  This currently does nothing, but will eventually iterate over all the maps and age all the creatures, culling as necessary.

---
+++ engine/time/source/unit_tests/AgeTimeObserver_test.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+
+TEST(SL_Engine_Time_AgeTimeObserver, serialization_id)
+{
+  AgeTimeObserver ato;
+
+  EXPECT_EQ(ClassIdentifier::CLASS_ID_AGE_TIME_OBSERVER, ato.get_class_identifier());
+}
+
