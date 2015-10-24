1ceb3c3f15b7007294a4f74f47cbf252881d448d
Julian Day <jcd748@mail.usask.ca>
2015-10-23 19:58:01 -0500

    Potentially run a death script when the player dies.

--- engine/combat/include/DeathManager.hpp
+++ engine/combat/include/DeathManager.hpp
@@ -12,6 +12,8 @@ class DeathManager
     virtual void die() const = 0;
     
   protected:
+    void run_death_event(CreaturePtr dead_creature, MapPtr map) const;
+
     CreaturePtr attacking_creature;
     CreaturePtr dead_creature;
     MapPtr map;
