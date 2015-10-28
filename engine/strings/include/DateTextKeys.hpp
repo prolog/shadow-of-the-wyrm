6231c5624392c06e1dcb13da79d1eb8205a1baf2
Julian Day <jcd748@mail.usask.ca>
2015-10-27 20:08:56 -0500

    Added phase of moon to the Time command.

--- engine/strings/include/DateTextKeys.hpp
+++ engine/strings/include/DateTextKeys.hpp
@@ -1,16 +1,22 @@
 #pragma once
 #include <string>
+#include "CalendarTypes.hpp"
 #include "Date.hpp"
 
 class DateTextKeys
 {
   public:
-    static std::string get_date_time_message(const Date& date, const std::string& season_sid);
+    static std::string get_date_time_message(const Date& date, const std::string& season_sid, const PhaseOfMoonType pom);
     static std::string get_time(const Date& date);
+    static std::string get_phase_of_moon_sid(const PhaseOfMoonType pom);
     
   protected:
     static const std::string DATE_TIME_MESSAGE;
-    
+    static const std::string PHASE_OF_MOON_NEW;
+    static const std::string PHASE_OF_MOON_WAXING;
+    static const std::string PHASE_OF_MOON_FULL;
+    static const std::string PHASE_OF_MOON_WANING;
+        
     DateTextKeys();
     ~DateTextKeys();
 };
