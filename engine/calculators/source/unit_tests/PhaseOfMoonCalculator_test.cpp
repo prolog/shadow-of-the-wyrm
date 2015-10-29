f42fbe2f9ec78e2b9fcf69e3b41fe9c4000bd75f
Julian Day <jcd748@mail.usask.ca>
2015-10-28 19:46:04 -0500

    Updated the POM unit tests.

--- engine/calculators/source/unit_tests/PhaseOfMoonCalculator_test.cpp
+++ engine/calculators/source/unit_tests/PhaseOfMoonCalculator_test.cpp
@@ -3,19 +3,36 @@
 TEST(SL_Engine_Calculators_PhaseOfMoonCalculator, calculate_phase)
 {
   PhaseOfMoonCalculator pomc;
-  double seconds = 12.0;
 
-  EXPECT_EQ(PhaseOfMoonType::PHASE_OF_MOON_NEW, pomc.calculate_phase_of_moon(seconds));
+  vector<PhaseOfMoonType> phase_v = {PhaseOfMoonType::PHASE_OF_MOON_NEW, 
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WAXING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_FULL,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING,
+                                     PhaseOfMoonType::PHASE_OF_MOON_WANING};
 
-  seconds = 60*60*24 + 10000;
-
-  EXPECT_EQ(PhaseOfMoonType::PHASE_OF_MOON_WAXING, pomc.calculate_phase_of_moon(seconds));
-
-  seconds = 60*60*24*14 + 10000;
-
-  EXPECT_EQ(PhaseOfMoonType::PHASE_OF_MOON_FULL, pomc.calculate_phase_of_moon(seconds));
-
-  seconds = 60*60*24*18 + 20000;
-
-  EXPECT_EQ(PhaseOfMoonType::PHASE_OF_MOON_WANING, pomc.calculate_phase_of_moon(seconds));
+  for (size_t i = 0; i < phase_v.size(); i++)
+  {
+    EXPECT_EQ(phase_v.at(i), pomc.calculate_phase_of_moon(60*60*24*i));
+  }
 }
