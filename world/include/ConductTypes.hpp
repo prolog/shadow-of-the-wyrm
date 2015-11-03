962887d5afe81ae408a32e8cc434137e96a629e1
Julian Day <jcd748@mail.usask.ca>
2015-11-02 19:47:19 -0600

    New conduct: questless.

--- world/include/ConductTypes.hpp
+++ world/include/ConductTypes.hpp
@@ -8,6 +8,7 @@ enum struct ConductType
   CONDUCT_TYPE_ILLITERATE = 3,
   CONDUCT_TYPE_WEAPONLESS = 4, /* Never hits something with a weapon, rather than not wielding one. */
   CONDUCT_TYPE_NO_GRAVEDIGGING = 5,
-  CONDUCT_SIZE = 6
+  CONDUCT_TYPE_QUESTLESS = 6,
+  CONDUCT_SIZE = 7
 };
 
