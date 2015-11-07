c3eec05d1afc2033f275a7042816a2e20267d933
Julian Day <jcd748@mail.usask.ca>
2015-11-07 08:11:50 -0600

    Added seeds to fruit.

--- world/include/ItemProperties.hpp
+++ world/include/ItemProperties.hpp
@@ -6,6 +6,7 @@ class ItemProperties
   public:
     static const std::string ITEM_PROPERTIES_DIG_HARDNESS;
     static const std::string ITEM_PROPERTIES_DIG_BREAKAGE_PCT_CHANCE;
+    static const std::string ITEM_PROPERTIES_SEED_ITEM_ID;
 
   protected:
     ItemProperties();
--- world/source/ItemProperties.cpp
+++ world/source/ItemProperties.cpp
@@ -12,3 +12,4 @@ ItemProperties::~ItemProperties()
 
 const string ItemProperties::ITEM_PROPERTIES_DIG_HARDNESS = "dig_hardness";
 const string ItemProperties::ITEM_PROPERTIES_DIG_BREAKAGE_PCT_CHANCE = "dig_breakage_pct_chance";
+const string ItemProperties::ITEM_PROPERTIES_SEED_ITEM_ID = "seed_item_id";
