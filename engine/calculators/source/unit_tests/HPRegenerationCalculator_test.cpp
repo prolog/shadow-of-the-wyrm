f2c110c49ebd1e48830c5182dd693782d20e7e2c
Julian Day <jcd748@mail.usask.ca>
2015-11-11 19:33:30 -0600

    A creature's health score now has a large impact on HP regeneration, and there is now a minimum of 4 minutes required to regenerate 1 HP.

--- engine/calculators/source/unit_tests/HPRegenerationCalculator_test.cpp
+++ engine/calculators/source/unit_tests/HPRegenerationCalculator_test.cpp
@@ -5,6 +5,9 @@
 TEST(SL_Engine_Calculators_HPRegenerationCalculator, minutes_per_hp)
 {
   CreaturePtr creature = std::make_shared<Creature>();
+  Statistic health(0);
+  creature->set_health(health);
+
   TilePtr tile = std::make_shared<BarrowTile>();
   FeaturePtr bed = std::make_shared<Bed>();
 
@@ -12,5 +15,17 @@ TEST(SL_Engine_Calculators_HPRegenerationCalculator, minutes_per_hp)
 
   HPRegenerationCalculator hprc;
   EXPECT_EQ(static_cast<uint>(30 * bed->get_hp_regeneration_multiplier()), hprc.calculate_minutes_per_hit_point(creature, tile));
+
+  health.set_base(25);
+  health.set_current(25);
+  creature->set_health(health);
+
+  EXPECT_EQ(static_cast<uint>(30 * bed->get_hp_regeneration_multiplier() * 0.75), hprc.calculate_minutes_per_hit_point(creature, tile));
+
+  health.set_base(100);
+  health.set_current(100);
+  creature->set_health(health);
+
+  EXPECT_EQ(4, hprc.calculate_minutes_per_hit_point(creature, tile));
 }
 
