b00d73437fe11b2023b6dab6278f7d7dbcd1b4bc
Julian Day <jcd748@mail.usask.ca>
2015-11-12 21:04:32 -0600

    AP regeneration now also includes a creature's willpower score in the calculation.

--- engine/calculators/source/APRegenerationCalculator.cpp
+++ engine/calculators/source/APRegenerationCalculator.cpp
@@ -1,6 +1,7 @@
 #include "APRegenerationCalculator.hpp"
 
 const uint APRegenerationCalculator::BASE_MINUTES_PER_ARCANA_POINT = 40;
+const uint APRegenerationCalculator::MIN_MINUTES_PER_ARCANA_POINT = 4;
 
 uint APRegenerationCalculator::calculate_minutes_per_arcana_point(CreaturePtr creature, TilePtr tile)
 {
@@ -12,6 +13,7 @@ uint APRegenerationCalculator::calculate_minutes_per_arcana_point(CreaturePtr cr
     if (tile != nullptr)
     {
       multiplier *= tile->get_ap_regeneration_multiplier();
+      multiplier *= get_willpower_multiplier(creature);
 
       FeaturePtr feature = tile->get_feature();
 
@@ -22,8 +24,21 @@ uint APRegenerationCalculator::calculate_minutes_per_arcana_point(CreaturePtr cr
     }
   }
 
-  minutes_per_arcana_point = static_cast<uint>(minutes_per_arcana_point * multiplier);
+  minutes_per_arcana_point = std::max(static_cast<uint>(minutes_per_arcana_point * multiplier), MIN_MINUTES_PER_ARCANA_POINT);
   return minutes_per_arcana_point;
+}
+
+// A creature's base willpower multiplier is 1.0, with -0.01 per pt of will.
+float APRegenerationCalculator::get_willpower_multiplier(CreaturePtr creature)
+{
+  float mult = 1.0f;
+
+  if (creature != nullptr)
+  {
+    mult -= (0.01f * creature->get_willpower().get_current());
+  }
+
+  return mult;
 }
 
 #ifdef UNIT_TESTS
--- engine/calculators/source/unit_tests/APRegenerationCalculator_test.cpp
+++ engine/calculators/source/unit_tests/APRegenerationCalculator_test.cpp
@@ -8,9 +8,24 @@ TEST(SL_Engine_Calculators_APRegenerationCalculator, minutes_per_ap)
   TilePtr tile = std::make_shared<AirTile>();
   FeaturePtr bed = std::make_shared<Bed>();
 
+  Statistic will(0);
+  creature->set_willpower(will);
+
   tile->set_feature(bed);
 
   APRegenerationCalculator aprc;
   EXPECT_EQ(static_cast<uint>(40 * bed->get_ap_regeneration_multiplier()), aprc.calculate_minutes_per_arcana_point(creature, tile));
+
+  will.set_base(20);
+  will.set_current(20);
+  creature->set_willpower(will);
+
+  EXPECT_EQ(static_cast<uint>(40 * bed->get_ap_regeneration_multiplier() * 0.80), aprc.calculate_minutes_per_arcana_point(creature, tile));
+
+  will.set_base(99);
+  will.set_current(99);
+  creature->set_willpower(will);
+
+  EXPECT_EQ(4, aprc.calculate_minutes_per_arcana_point(creature, tile));
 }
 
