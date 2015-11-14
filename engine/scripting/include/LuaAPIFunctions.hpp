33deb339463fbb9211f79cbd91c0dbe77815b61b
Julian Day <jcd748@mail.usask.ca>
2015-11-14 07:37:23 -0600

    Fixed a bug with the caravan master's quest not being finishable.

--- engine/scripting/include/LuaAPIFunctions.hpp
+++ engine/scripting/include/LuaAPIFunctions.hpp
@@ -93,6 +93,7 @@ int calendar_add_minutes(lua_State* ls);
 int calendar_add_hours(lua_State* ls);
 int calendar_add_days(lua_State* ls);
 int calendar_add_years(lua_State* ls);
+int add_kill_to_creature_mortuary(lua_State* ls);
 
 // helper functions for the Lua API functions.
 std::string read_sid_and_replace_values(lua_State* ls, int offset = 0);
