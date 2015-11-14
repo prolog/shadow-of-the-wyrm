68e94faff7188868930004a09e686c52e6686327
Julian Day <jcd748@mail.usask.ca>
2015-11-14 09:51:55 -0600

    Show the race/class on the naming screen, in preparation for adding randomized character options.

--- engine/screens/source/NamingScreen.cpp
+++ engine/screens/source/NamingScreen.cpp
@@ -1,10 +1,11 @@
+#include <sstream>
 #include "NamingScreen.hpp"
 #include "PromptTextKeys.hpp"
 
 using namespace std;
 
-NamingScreen::NamingScreen(DisplayPtr new_display, const string& warning) 
-: Screen(new_display), warning_message(warning)
+NamingScreen::NamingScreen(DisplayPtr new_display, const string& synop, const string& warning) 
+: Screen(new_display), creature_synopsis(synop), warning_message(warning)
 {
   initialize();
 }
@@ -13,6 +14,7 @@ void NamingScreen::initialize()
 {
   string optional_message;
   string empty_str;
+  ostringstream synop;
 
   vector<ScreenComponentPtr> naming_screen;
 
@@ -21,11 +23,17 @@ void NamingScreen::initialize()
     optional_message = warning_message;
   }
 
-  TextComponentPtr optional_warning_message = std::make_shared<TextComponent>(optional_message);
+  synop << "[" << creature_synopsis << "]";
+
   TextComponentPtr blank_space = std::make_shared<TextComponent>(empty_str);
 
-  naming_screen.push_back(optional_warning_message);
-  naming_screen.push_back(blank_space);
+  naming_screen.push_back(make_shared<TextComponent>(synop.str()));
+
+  if (!optional_message.empty())
+  {
+    naming_screen.push_back(make_shared<TextComponent>(optional_message));
+    naming_screen.push_back(make_shared<TextComponent>(empty_str));
+  }
 
   user_prompt = std::make_shared<TextPrompt>();
   user_prompt->set_text_sid(PromptTextKeys::PROMPT_ENTER_YOUR_NAME);
