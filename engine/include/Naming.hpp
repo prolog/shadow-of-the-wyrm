c337d744b34751d2db8c43b6aac93b8488cc633e
Julian Day <jcd748@mail.usask.ca>
2015-12-06 11:15:34 -0600

    Random name generation.  Also updated the Lua naming function so that it followed the same logic as the main naming screen.

--- engine/include/Naming.hpp
+++ engine/include/Naming.hpp
@@ -1,14 +1,28 @@
 #pragma once
 #include <string>
+#include <map>
+#include <vector>
 #include "common.hpp"
+#include "CreatureFeatures.hpp"
+
+using SyllableMap = std::map<CreatureSex, std::map<int, std::map<int, std::vector<std::string>>>>;
 
 class Naming
 {
   public:
+    static std::string generate_name(const CreatureSex sex);
     static std::string clean_name(const std::string& name);
+    static std::string clean_name_or_use_default(const std::string& name, const CreatureSex sex);
     static uint get_max_name_size();
+
   protected:
     Naming();
 
+    static std::string create_name(const int num_syllables, const std::map<int, std::map<int, std::vector<std::string>>>& syl_map_entry);
     static uint MAXIMUM_NAME_SIZE;
+    static const int MIN_NAME_SYL;
+    static const int MAX_NAME_SYL;
+
+    // Map from number of syllables to potential syllables for each place.
+    static const SyllableMap syllables;
 };
