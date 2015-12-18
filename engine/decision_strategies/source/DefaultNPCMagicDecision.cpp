337decd31bd7943a6aeecc6368d214c2cfe4da28
Julian Day <jcd748@mail.usask.ca>
2015-12-17 20:27:30 -0600

    Default decision logic for NPC spellcasting.

--- engine/decision_strategies/source/DefaultNPCMagicDecision.cpp
+++ engine/decision_strategies/source/DefaultNPCMagicDecision.cpp
@@ -1,6 +1,17 @@
 #include "DefaultNPCMagicDecision.hpp"
 
+// Optimistic: if the creature *can* cast it, cast it!
 bool DefaultNPCMagicDecision::decide(CreaturePtr caster, MapPtr view_map, const Spell& spell) const
 {
-  return false;
+  bool decision = false;
+
+  if (caster != nullptr)
+  {
+    if (spell.get_ap_cost() <= static_cast<uint>(caster->get_arcana_points().get_current()))
+    {
+      decision = true;
+    }
+  }
+
+  return decision;
 }
