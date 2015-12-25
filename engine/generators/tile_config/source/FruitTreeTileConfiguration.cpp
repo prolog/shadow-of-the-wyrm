bd34b4f7fe6db9b5ddc2cf2f685920b1f1891268
Julian Day <jcd748@mail.usask.ca>
2015-12-25 15:54:08 -0600

    Olive trees.

--- engine/generators/tile_config/source/FruitTreeTileConfiguration.cpp
+++ engine/generators/tile_config/source/FruitTreeTileConfiguration.cpp
@@ -20,17 +20,19 @@ void FruitTreeTileConfiguration::initialize_fruit_ids()
   fruit_ids.insert(make_pair(make_pair(TreeSpeciesID::TREE_SPECIES_PEACH, Season::SEASON_SUMMER), "_peach"));
   fruit_ids.insert(make_pair(make_pair(TreeSpeciesID::TREE_SPECIES_PLUM, Season::SEASON_SUMMER), "_plum"));
   fruit_ids.insert(make_pair(make_pair(TreeSpeciesID::TREE_SPECIES_PEAR, Season::SEASON_SUMMER), "_pear"));
+  fruit_ids.insert(make_pair(make_pair(TreeSpeciesID::TREE_SPECIES_OLIVE, Season::SEASON_SUMMER), "_olive"));
 }
 
 void FruitTreeTileConfiguration::initialize_tree_species_details()
 {
-  tree_species_ids = { TreeSpeciesID::TREE_SPECIES_APPLE, TreeSpeciesID::TREE_SPECIES_CHERRY, TreeSpeciesID::TREE_SPECIES_PEACH, TreeSpeciesID::TREE_SPECIES_PLUM, TreeSpeciesID::TREE_SPECIES_PEAR };
+  tree_species_ids = { TreeSpeciesID::TREE_SPECIES_APPLE, TreeSpeciesID::TREE_SPECIES_CHERRY, TreeSpeciesID::TREE_SPECIES_PEACH, TreeSpeciesID::TREE_SPECIES_PLUM, TreeSpeciesID::TREE_SPECIES_PEAR, TreeSpeciesID::TREE_SPECIES_OLIVE };
 
   tree_species_description_sids.insert(make_pair(TreeSpeciesID::TREE_SPECIES_APPLE, TileDescriptionKeys::FLORA_TILE_DESC_APPLE));
   tree_species_description_sids.insert(make_pair(TreeSpeciesID::TREE_SPECIES_CHERRY, TileDescriptionKeys::FLORA_TILE_DESC_CHERRY));
   tree_species_description_sids.insert(make_pair(TreeSpeciesID::TREE_SPECIES_PEACH, TileDescriptionKeys::FLORA_TILE_DESC_PEACH));
   tree_species_description_sids.insert(make_pair(TreeSpeciesID::TREE_SPECIES_PLUM, TileDescriptionKeys::FLORA_TILE_DESC_PLUM));
   tree_species_description_sids.insert(make_pair(TreeSpeciesID::TREE_SPECIES_PEAR, TileDescriptionKeys::FLORA_TILE_DESC_PEAR));
+  tree_species_description_sids.insert(make_pair(TreeSpeciesID::TREE_SPECIES_OLIVE, TileDescriptionKeys::FLORA_TILE_DESC_OLIVE));
 }
 
 void FruitTreeTileConfiguration::configure_additional_features(TilePtr tile, const Season season, const TreeSpeciesID tree_species_id) const
