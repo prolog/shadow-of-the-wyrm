40ed90c7a3ab60169fbaf283ce6c529cbddeb5df
Julian Day <jcd748@mail.usask.ca>
2016-01-24 12:56:25 -0600

    Add unit testing file.

---
+++ world/source/unit_tests/CalendarDay_test.cpp
@@ -0,0 +1,25 @@
+#include "gtest/gtest.h"
+
+TEST(SW_World_CalendarDay, serialization_id)
+{
+  CalendarDay cd;
+
+  EXPECT_EQ(ClassIdentifier::CLASS_ID_CALENDAR_DAY, cd.get_class_identifier());
+}
+
+TEST(SW_World_Calendarday, saveload)
+{
+  CalendarDay cd("asdf", 123);
+  CalendarDay cd2;
+
+  ostringstream ss;
+
+  cd.serialize(ss);
+
+  istringstream iss(ss.str());
+
+  cd2.deserialize(iss);
+
+  EXPECT_TRUE(cd == cd2);
+}
+
