bc04e600c3d588ead5157b054e51d96d1a78ed0d
Julian Day <jcd748@mail.usask.ca>
2016-02-05 18:14:10 -0600

    Usernames are now included in score file entries.

--- engine/include/ScoreFileEntry.hpp
+++ engine/include/ScoreFileEntry.hpp
@@ -8,7 +8,7 @@ class ScoreFileEntry : public ISerializable
 {
   public:
     ScoreFileEntry();
-    ScoreFileEntry(const long long new_score, const std::string& name, const CreatureSex new_sex, const bool is_current_char, const int lvl, const std::string& race_class_abrv);
+    ScoreFileEntry(const long long new_score, const std::string& name, const std::string& username, const CreatureSex new_sex, const bool is_current_char, const int lvl, const std::string& race_class_abrv);
     bool operator<(const ScoreFileEntry& sfe) const;
 
     bool serialize(std::ostream& stream) const override;
@@ -16,6 +16,7 @@ class ScoreFileEntry : public ISerializable
 
     long long get_score() const;
     std::string get_name() const;
+    std::string get_username() const;
     CreatureSex get_sex() const;
     bool get_is_current_char() const;
     int get_level() const;
@@ -26,6 +27,7 @@ class ScoreFileEntry : public ISerializable
   protected:
     long long score;
     std::string name;
+    std::string username;
     CreatureSex sex;
     bool is_current_char;
     int level;
