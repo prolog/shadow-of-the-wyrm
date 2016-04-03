eb47a9e5245684937e325b2cad6507c5718cb8c5
Julian Day <jcd748@mail.usask.ca>
2016-04-03 07:18:35 -0500

    Updated breeding to respect when the creature regeneration rate for the current map is 0.

--- engine/actions/source/BreedAction.cpp
+++ engine/actions/source/BreedAction.cpp
@@ -1,8 +1,11 @@
+#include "global_prototypes.hpp"
 #include "BreedAction.hpp"
+#include "Conversion.hpp"
 #include "CoordUtils.hpp"
 #include "CreatureFactory.hpp"
 #include "Game.hpp"
 #include "GameUtils.hpp"
+#include "MapProperties.hpp"
 #include "MapUtils.hpp"
 #include "RNG.hpp"
 
@@ -16,30 +19,35 @@ ActionCostValue BreedAction::breed(CreaturePtr creature, MapPtr map) const
 {
   Game& game = Game::instance();
 
-  if (creature != nullptr)
+  if (creature != nullptr && map != nullptr)
   {
-    Coordinate creature_coords = map->get_location(creature->get_id());
-    vector<Coordinate> coords = CoordUtils::get_adjacent_map_coordinates(map->size(), creature_coords.first, creature_coords.second);
-    std::shuffle(coords.begin(), coords.end(), RNG::get_engine());
+    string regen_rate_s = map->get_property(MapProperties::MAP_PROPERTIES_CREATURE_GENERATION_RATE);
 
-    if (creature != nullptr)
+    if (regen_rate_s.empty() || !fequal(String::to_float(regen_rate_s), 0.0f))
     {
-      for (const Coordinate& c : coords)
+      Coordinate creature_coords = map->get_location(creature->get_id());
+      vector<Coordinate> coords = CoordUtils::get_adjacent_map_coordinates(map->size(), creature_coords.first, creature_coords.second);
+      std::shuffle(coords.begin(), coords.end(), RNG::get_engine());
+
+      if (creature != nullptr)
       {
-        TilePtr tile = map->at(c);
-        
-        if (tile && tile->has_creature() == false)
+        for (const Coordinate& c : coords)
         {
-          // Breed into the tile.
-          string original_id = creature->get_original_id();
-          CreatureFactory cf;
+          TilePtr tile = map->at(c);
 
-          CreaturePtr spawn = cf.create_by_creature_id(game.get_action_manager_ref(), original_id);
-          GameUtils::add_new_creature_to_map(game, spawn, map, c);
+          if (tile && tile->has_creature() == false)
+          {
+            // Breed into the tile.
+            string original_id = creature->get_original_id();
+            CreatureFactory cf;
 
-          // Breeding is silent - the player gets no notification, so the message
-          // manager is not needed.
-          break;
+            CreaturePtr spawn = cf.create_by_creature_id(game.get_action_manager_ref(), original_id);
+            GameUtils::add_new_creature_to_map(game, spawn, map, c);
+
+            // Breeding is silent - the player gets no notification, so the message
+            // manager is not needed.
+            break;
+          }
         }
       }
     }
