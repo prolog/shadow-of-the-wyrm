b01f534419d9c52b893265f0dccfc5aaf12a8082
Julian Day <jcd748@mail.usask.ca>
2016-04-04 20:11:45 -0500

    Added the Caldera to the dwarf lands - just an empty map at the moment.

--- scripts/areas/dwarf_lands.lua
+++ scripts/areas/dwarf_lands.lua
@@ -4,10 +4,15 @@ local wyrmeswraec = Area:new(6, 9)
 wyrmeswraec:set_custom_map_id("wyrmeswraec")
 wyrmeswraec:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_WYRMESWRAEC")
 
+local caldera = Area:new(6, 18)
+caldera:set_custom_map_id("caldera")
+caldera:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_CALDERA")
+
 local stoneheim = Area:new(9, 19)
 stoneheim:set_custom_map_id("stoneheim")
 stoneheim:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_STONEHEIM")
 
 wyrmeswraec:insert()
+caldera:insert()
 stoneheim:insert()
 
