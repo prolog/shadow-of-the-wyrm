a065232064ff7f3326fbfa51a125d491af430226
Julian Day <jcd748@mail.usask.ca>
2016-04-13 21:10:46 -0500

    Skills can now be used/explained by selecting the skill from the skills screen.

--- engine/actions/skills/include/SkillProcessors.hpp
+++ engine/actions/skills/include/SkillProcessors.hpp
@@ -4,8 +4,22 @@
 class DefaultSkillProcessor : public ISkillProcessor
 {
   public:
-    DefaultSkillProcessor();
+    DefaultSkillProcessor(const std::string& new_message_sid);
 
     virtual ActionCostValue process(CreaturePtr creature, MapPtr map);
+
+  protected:
+    std::string message_sid;
 };
 
+class WeaponSkillProcessor : public DefaultSkillProcessor
+{
+  public:
+    WeaponSkillProcessor();
+};
+
+class MagicSkillProcessor : public DefaultSkillProcessor
+{
+  public:
+    MagicSkillProcessor();
+};
