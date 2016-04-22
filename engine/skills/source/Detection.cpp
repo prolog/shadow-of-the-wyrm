680fbea6e5f15d401ce25d6713418bed9e0bc8b7
Julian Day <jcd748@mail.usask.ca>
2016-04-21 19:51:34 -0500

    Merged action/skill into skill.

--- engine/skills/source/Detection.cpp
+++ engine/skills/source/Detection.cpp
@@ -6,25 +6,24 @@
 
 using std::string;
 
-void Detection::detect_creatures_if_necessary(CreaturePtr creature, MapPtr map, const string& original_map_id, const string& current_map_id)
+ActionCostValue Detection::process(CreaturePtr creature, MapPtr map)
 {
   IMessageManager& manager = MessageManagerFactory::instance(creature, creature && creature->get_is_player());
   
-  if (original_map_id != current_map_id) // We've just loaded a new map
+  if (map && map->get_map_type() != MapType::MAP_TYPE_WORLD) // Player's the only creature on the map in this case, so no msgs.
   {
-    if (map && map->get_map_type() != MapType::MAP_TYPE_WORLD) // Player's the only creature on the map in this case, so no msgs.
+    if (MapUtils::hostile_creature_exists(creature->get_id(), map))
     {
-      if (MapUtils::hostile_creature_exists(creature->get_id(), map))
-      {
-        SkillManager sm;
+      SkillManager sm;
         
-        if (sm.check_skill(creature, SkillType::SKILL_GENERAL_DETECTION))
-        {
-          string detected_creatures = StringTable::get(ActionTextKeys::ACTION_DETECTED_HOSTILE_CREATURES);
-          manager.add_new_message(detected_creatures);
-          manager.send();
-        }
+      if (sm.check_skill(creature, SkillType::SKILL_GENERAL_DETECTION))
+      {
+        string detected_creatures = StringTable::get(ActionTextKeys::ACTION_DETECTED_HOSTILE_CREATURES);
+        manager.add_new_message(detected_creatures);
+        manager.send();
       }
     }
   }
+
+  return 1; // JCD FIXME
 }
