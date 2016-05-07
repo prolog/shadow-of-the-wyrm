39de367112c4a15584546aabfcf9d8104e90afb6
Julian Day <jcd748@mail.usask.ca>
2016-05-07 15:56:13 -0500

    Goblins trappers now can set traps as a decision action.

--- scripts/decision/goblin_trapper.lua
+++ scripts/decision/goblin_trapper.lua
@@ -1,22 +1,28 @@
 require('decide')
 
+goblin_traps = {"arrow_trap", "rock_trap", "crossbow_trap", 
+                "flash_trap", "poison_dart_trap", "blackwater_trap"}
+
 local function set_goblin_trap(creature_id)
   local creature_ids = {get_nearby_hostile_creatures(creature_id)}
-  local trap_hidden = true
+  local trap_triggered = false
 
   -- For each nearby creature, see if it passes a Detection check.
   -- If any creatures pass their detection check, the trap should be
   -- generated as "seen"; otherwise, it will be hidden.
   for k,v in pairs(creature_ids) do
     if check_skill(v, 12) == true then
-      trap_hidden = false
+      trap_triggered = true
       break
     end
   end
 
   -- Now we've determined whether the trap is properly hidden or not.
   -- Set the trap appropriately on the trapper's square.
-  -- ...
+  local y, x = get_creature_yx(creature_id)
+  local trap_id = goblin_traps[RNG_range(1, table.getn(goblin_traps))]
+
+  set_trap(y, x, trap_triggered, trap_id)
 
   local action_cost = 10
   return action_cost
