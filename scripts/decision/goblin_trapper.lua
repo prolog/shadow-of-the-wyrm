8d779a6ed81deba46625679a214588b93bcaafd7
Julian Day <jcd748@mail.usask.ca>
2016-05-08 08:52:24 -0500

    Goblin trappers now now set at most 3 traps.

--- scripts/decision/goblin_trapper.lua
+++ scripts/decision/goblin_trapper.lua
@@ -3,7 +3,36 @@ require('decide')
 goblin_traps = {"arrow_trap", "rock_trap", "crossbow_trap", 
                 "flash_trap", "poison_dart_trap", "blackwater_trap"}
 
+local function has_remaining_traps(creature_id)
+  local has_traps = false
+  local traps_property = "CREATURE_PROPERTIES_NUM_TRAPS"
+  local traps_prop_value = get_creature_additional_property(creature_id, traps_property)
+  local num_traps = tonumber(traps_prop_value)
+
+  if (num_traps ~= nil and num_traps > 0) then
+    has_traps = true
+  end
+
+  return has_traps
+end
+
+local function decrement_goblin_traps(creature_id)
+  local traps_property = "CREATURE_PROPERTIES_NUM_TRAPS"
+  local num_traps = tonumber(get_creature_additional_property(creature_id, traps_property))
+
+  if (num_traps ~= nil and num_traps > 0) then
+     num_traps = num_traps - 1
+     set_creature_additional_property(creature_id, traps_property, num_traps)
+  end
+end
+
 local function set_goblin_trap(creature_id)
+  -- If the goblin doesn't have any more traps, then exit and do a
+  -- different action.
+  if has_remaining_traps(creature_id) == false then
+    return 0
+  end
+
   local creature_ids = {get_nearby_hostile_creatures(creature_id)}
   local trap_triggered = false
 
@@ -23,6 +52,7 @@ local function set_goblin_trap(creature_id)
   local trap_id = goblin_traps[RNG_range(1, table.getn(goblin_traps))]
 
   set_trap(y, x, trap_triggered, trap_id)
+  decrement_goblin_traps(creature_id)
 
   local action_cost = 10
   return action_cost
