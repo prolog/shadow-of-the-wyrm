63125ba5ba3eeff14a6894384201cd19fd3a9723
Julian Day <jcd748@mail.usask.ca>
2016-05-12 22:42:09 -0500

    Moved the beam details creation into its own function, so it can be easily and repeatedly called for beams with radii greater than 1.

--- engine/magic/include/BeamShapeProcessor.hpp
+++ engine/magic/include/BeamShapeProcessor.hpp
@@ -12,6 +12,7 @@ protected:
   virtual std::pair<std::vector<TilePtr>, Animation> get_affected_tiles_and_animation_for_spell(MapPtr map, const Coordinate& caster_coord, const Direction d, const Spell& spell) override;
     virtual bool should_beam_reflect() const;
 
+    virtual std::pair<std::vector<TilePtr>, std::vector<std::pair<DisplayTile, std::vector<Coordinate>>>> create_beam(MapPtr map, const Spell& spell, const Coordinate& cur_coord, const Coordinate& caster_coord, const Direction d);
     virtual Direction get_new_beam_direction_after_impact(const Direction old_direction, const Coordinate& current_coord, MapPtr map);
 
     Direction get_ne_reflection(const Coordinate& current_coord, MapPtr map);
