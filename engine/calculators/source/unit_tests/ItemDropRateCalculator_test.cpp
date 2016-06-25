8b92481dbd78866fca4bb6214a896879724fb3dd
Julian Day <jcd748@mail.usask.ca>
2016-06-24 23:43:52 -0500

    Lowered the base currency drop rate.  Adjusted unit tests.

--- engine/calculators/source/unit_tests/ItemDropRateCalculator_test.cpp
+++ engine/calculators/source/unit_tests/ItemDropRateCalculator_test.cpp
@@ -5,23 +5,6 @@ TEST(SW_Engine_Calculators_ItemDropRateCalculator, calculate_pct_chance_item_dro
   CreaturePtr creature;
   ItemDropRateCalculator idrc;
 
-  EXPECT_EQ(15, idrc.calculate_pct_chance_currency_drop(creature));
-
-  creature = std::make_shared<Creature>();
-  creature->get_skills().set_value(SkillType::SKILL_GENERAL_DUNGEONEERING, 42);
-
-  EXPECT_EQ(21, idrc.calculate_pct_chance_currency_drop(creature));
-
-  creature->get_skills().set_value(SkillType::SKILL_GENERAL_DUNGEONEERING, 90);
-
-  EXPECT_EQ(28, idrc.calculate_pct_chance_currency_drop(creature));
-}
-
-TEST(SW_Engine_Calculators_ItemDropRateCalculator, calculate_pct_chance_currency_drop)
-{
-  CreaturePtr creature;
-  ItemDropRateCalculator idrc;
-
   EXPECT_EQ(5, idrc.calculate_pct_chance_item_drop(creature));
 
   creature = std::make_shared<Creature>();
@@ -32,4 +15,21 @@ TEST(SW_Engine_Calculators_ItemDropRateCalculator, calculate_pct_chance_currency
   creature->get_skills().set_value(SkillType::SKILL_GENERAL_DUNGEONEERING, 99);
 
   EXPECT_EQ(9, idrc.calculate_pct_chance_item_drop(creature));
+}
+
+TEST(SW_Engine_Calculators_ItemDropRateCalculator, calculate_pct_chance_currency_drop)
+{
+  CreaturePtr creature;
+  ItemDropRateCalculator idrc;
+
+  EXPECT_EQ(10, idrc.calculate_pct_chance_currency_drop(creature));
+
+  creature = std::make_shared<Creature>();
+  creature->get_skills().set_value(SkillType::SKILL_GENERAL_DUNGEONEERING, 42);
+
+  EXPECT_EQ(14, idrc.calculate_pct_chance_currency_drop(creature));
+
+  creature->get_skills().set_value(SkillType::SKILL_GENERAL_DUNGEONEERING, 90);
+
+  EXPECT_EQ(19, idrc.calculate_pct_chance_currency_drop(creature));
 }
--- engine/creatures/source/CreatureGenerationConstants.cpp
+++ engine/creatures/source/CreatureGenerationConstants.cpp
@@ -4,7 +4,7 @@ const float CreatureGenerationConstants::BASE_EXPERIENCE_LOWER_MULTIPLIER = 0.8f
 const float CreatureGenerationConstants::BASE_EXPERIENCE_UPPER_MULTIPLIER = 1.2f;
 const uint CreatureGenerationConstants::MIN_CREATURES_FOR_MAP_UPDATE = 5;
 const int CreatureGenerationConstants::CREATURE_DROP_RATE = 5;
-const int CreatureGenerationConstants::HUMANOID_CURRENCY_RATE = 15;
+const int CreatureGenerationConstants::HUMANOID_CURRENCY_RATE = 10;
 const int CreatureGenerationConstants::CREATURE_GENERATION_UNLIMITED = -1;
 
 CreatureGenerationConstants::CreatureGenerationConstants()
