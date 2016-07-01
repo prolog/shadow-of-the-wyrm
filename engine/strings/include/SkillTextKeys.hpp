75ffc16542f58d0ae8e9e727150aa0bc0b64db9c
Julian Day <jcd748@mail.usask.ca>
2016-07-01 15:34:23 -0500

    Items for particular traps.

--- engine/strings/include/SkillTextKeys.hpp
+++ engine/strings/include/SkillTextKeys.hpp
@@ -190,6 +190,7 @@ class SkillTextKeys
     static const std::string SKILL_USAGE_DUNGEONEERING;
     static const std::string SKILL_USAGE_ESCAPE;
     static const std::string SKILL_USAGE_FORAGING;
+    static const std::string SKILL_USAGE_HERBALISM;
     static const std::string SKILL_USAGE_HUNTING;
     static const std::string SKILL_USAGE_JEWELER;
     static const std::string SKILL_USAGE_GENERAL_MAGIC;
