a3e57b531f8b765e193e3b2f9f270903f91ef59a
Julian Day <jcd748@mail.usask.ca>
2016-07-01 08:39:25 -0500

    Stanza.

---
+++ engine/calculators/include/DisarmTrapsCalculator.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "Creature.hpp"
+
+enum struct DisarmTrapsOutcome
+{
+  DISARM_TRAPS_TRIGGER = 0,
+  DISARM_TRAPS_FAIL = 1,
+  DISARM_TRAPS_DISARM = 2,
+  DISARM_TRAPS_DISMANTLE = 3
+};
+
+class DisarmTrapsCalculator
+{
+  public:
+    DisarmTrapsOutcome calculate_disarm_traps_outcome(CreaturePtr creature);
+
+  protected:
+    static const float DISARM_TRAPS_SKILL_MULTIPLIER;
+    static const float DISMANTLE_TRAPS_SKILL_MULTIPLIER;
+};
+
