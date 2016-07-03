752963f36ae35effeab796eba10fe13065c6eafb
Julian Day <jcd748@mail.usask.ca>
2016-07-03 10:40:24 -0500

    Hooked up extra descriptions for the three shrines.

---
+++ scripts/areas/nether_shrine.lua
@@ -0,0 +1,7 @@
+require('areas')
+
+local nether_shrine = Area:new(97, 2)
+nether_shrine:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_NETHER_SHRINE")
+
+nether_shrine:insert()
+
---
+++ scripts/areas/sky_shrine.lua
@@ -0,0 +1,7 @@
+require('areas')
+
+local sky_shrine = Area:new(3, 89)
+sky_shrine:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_SKY_SHRINE")
+
+sky_shrine:insert()
+
