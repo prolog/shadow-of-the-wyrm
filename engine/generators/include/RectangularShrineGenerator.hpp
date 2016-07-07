72eb1b1936a018a72339b571264aeea84a9f836f
Julian Day <jcd748@mail.usask.ca>
2016-07-06 19:52:39 -0500

    Feature generation in rectangular shrines.

---
+++ engine/generators/include/RectangularShrineGenerator.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "Generator.hpp"
+#include "TileGenerator.hpp"
+
+class RectangularShrineGenerator : public SOTW::Generator
+{
+  public:
+    RectangularShrineGenerator(MapPtr base_map);
+
+    virtual MapPtr generate(const Dimensions& dim) override;
+    virtual MapPtr generate();
+
+  protected:
+    void create_entrances(MapPtr map, const int start_row, const int start_col, const int height, const int width, const int mid_row, const int mid_col);
+    void place_pillars_and_statues(MapPtr map, const int start_row, const int start_col, const int height, const int width, const int mid_row, const int mid_col);
+    void generate_pillar_or_statue(MapPtr map, const std::vector<Coordinate>& coordinates, const bool generate_pillar);
+
+    MapPtr base_map;
+    TileGenerator tg;
+};
+
