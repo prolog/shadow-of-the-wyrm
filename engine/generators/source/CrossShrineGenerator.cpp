f7e72b7f090c0520e7d88a05f05473e4c5be6df1
Julian Day <jcd748@mail.usask.ca>
2016-07-07 20:15:44 -0500

    Refactored some code.

--- engine/generators/source/CrossShrineGenerator.cpp
+++ engine/generators/source/CrossShrineGenerator.cpp
@@ -22,7 +22,7 @@ MapPtr CrossShrineGenerator::generate()
 
   int num_rows = dim.get_y();
   int mid_row = num_rows / 2;
-  int start_row = RNG::range(mid_row * 0.2, mid_row * 0.4);  
+  int start_row = RNG::range(mid_row * 0.2, mid_row * 0.4);
   int shrine_height = 2 * std::abs(mid_row - start_row);
 
   int num_cols = dim.get_x();
@@ -33,9 +33,19 @@ MapPtr CrossShrineGenerator::generate()
 
   int start_col = mid_col - (shrine_width / 2);
 
+  generate_building(map, start_row, start_col, mid_row, shrine_height, shrine_width);
+  generate_entrances(map, start_row, shrine_height, mid_col);
+
+  // Relic
+
+  return map;
+}
+
+void CrossShrineGenerator::generate_building(MapPtr map, const int start_row, const int start_col, const int mid_row, const int shrine_height, const int shrine_width)
+{
   // Generate the tall bit
   GeneratorUtils::generate_building(map, start_row, start_col, shrine_height, shrine_width);
-  
+
   // Generate the wide bit
   int wide_start_row = mid_row - (shrine_width / 4);
   int wide_height = shrine_height / 2;
@@ -43,7 +53,7 @@ MapPtr CrossShrineGenerator::generate()
   GeneratorUtils::generate_building(map, wide_start_row, start_col - (shrine_width / 2), wide_height, wide_width);
 
   // Get rid of the walls in the middle
-  vector<int> rows = {wide_start_row, wide_start_row + wide_height - 1};
+  vector<int> rows = { wide_start_row, wide_start_row + wide_height - 1 };
   for (int row : rows)
   {
     for (int col = start_col + 1; col < (start_col + shrine_width - 1); col++)
@@ -52,16 +62,18 @@ MapPtr CrossShrineGenerator::generate()
       map->insert(row, col, dungeon_tile);
     }
   }
+}
 
-  // Entrances
+void CrossShrineGenerator::generate_entrances(MapPtr map, const int start_row, const int shrine_height, const int mid_col)
+{
   int end_row = start_row + shrine_height - 1;
   vector<Coordinate> entrances;
 
   for (int row : {start_row, end_row})
   {
-    for (int col : {mid_col-1, mid_col, mid_col+1})
+    for (int col : {mid_col - 1, mid_col, mid_col + 1})
     {
-      entrances.push_back({row,col});
+      entrances.push_back({ row,col });
     }
   }
 
@@ -70,10 +82,4 @@ MapPtr CrossShrineGenerator::generate()
     TilePtr dungeon_tile = tg.generate(TileType::TILE_TYPE_DUNGEON);
     map->insert(entr, dungeon_tile);
   }
-  // ...
-
-  // Relic
-
-  return map;
 }
-
