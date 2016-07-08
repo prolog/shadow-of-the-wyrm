b4b49b38df24ffdd260b4301d048b802a1f1fb06
Julian Day <jcd748@mail.usask.ca>
2016-07-07 20:32:03 -0500

    Per-shrine relic IDs.

--- scripts/areas/sky_shrine.lua
+++ scripts/areas/sky_shrine.lua
@@ -2,6 +2,7 @@ require('areas')
 
 local sky_shrine = Area:new(3, 89)
 sky_shrine:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_SKY_SHRINE")
+sky_shrine:set_additional_properties({["MAP_PROPERTIES_RELIC_ID"] = "heart_heaven"})
 
 sky_shrine:insert()
 
