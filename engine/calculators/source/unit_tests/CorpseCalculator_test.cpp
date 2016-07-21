ca40fd6ab3afc4ef661b243d866a4de6826b92a6
Julian Day <jcd748@mail.usask.ca>
2016-07-21 08:54:00 -0500

    Fix failing unit test.

--- engine/calculators/source/unit_tests/CorpseCalculator_test.cpp
+++ engine/calculators/source/unit_tests/CorpseCalculator_test.cpp
@@ -33,22 +33,22 @@ TEST(SW_Engine_Calculators_CorpseCaclulator, calculate_chance_resistance)
 {
   CorpseCalculator cc;
 
-  EXPECT_EQ(40, cc.calculate_chance_resistance(nullptr));
+  EXPECT_EQ(25, cc.calculate_chance_resistance(nullptr));
 
   CreaturePtr creature = std::make_shared<Creature>();
 
-  EXPECT_EQ(40, cc.calculate_chance_resistance(creature));
+  EXPECT_EQ(25, cc.calculate_chance_resistance(creature));
 
   Skills skills;
   skills.set_value(SkillType::SKILL_GENERAL_SKINNING, 40);
   creature->set_skills(skills);
 
-  EXPECT_EQ(60, cc.calculate_chance_resistance(creature));
+  EXPECT_EQ(38, cc.calculate_chance_resistance(creature));
 
   skills.set_value(SkillType::SKILL_GENERAL_SKINNING, 100);
   creature->set_skills(skills);
 
-  EXPECT_EQ(90, cc.calculate_chance_resistance(creature));
+  EXPECT_EQ(58, cc.calculate_chance_resistance(creature));
   
 }
 
