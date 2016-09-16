b6ea87bf874648cc9509999c5b331d282009d218
Julian Day <jcd748@mail.usask.ca>
2016-09-15 20:36:28 -0500

    Prompt when stealing from a friendly creature, and then if the action is confirmed, notify the stealing creature's deity.  Set hostility when stealing.  Clear the screen after selecting the thievery skill, so any messages displayed can be properly shown in the message area.  Update the guidebook to remove the unimplemented flag for Thievery.

--- engine/source/MessageManagerFactory.cpp
+++ engine/source/MessageManagerFactory.cpp
@@ -9,6 +9,24 @@ IMessageManager& MessageManagerFactory::instance()
   return mm_instance();
 }
 
+// If the creature passed is the player, return the message manager;
+// otherwise, return the null message manager.
+//
+// This is the message manager factory function that should be used for
+// messages that "only the creature itself cares about" - messages that
+// would only be known to it, and have no effect on the outside world.
+IMessageManager& MessageManagerFactory::instance(CreaturePtr creature)
+{
+  if (creature && creature->get_is_player())
+  {
+    return mm_instance();
+  }
+  else
+  {
+    return nmm_instance();
+  }
+}
+
 // Select the correct message manager based on whether the creature is
 // the player, or at least present within the player's view map.
 IMessageManager& MessageManagerFactory::instance(CreaturePtr creature, bool player_is_affected)
