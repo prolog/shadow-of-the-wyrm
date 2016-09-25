85954b95e421f00af59b9e4cc627ae40fe2b317b
Julian Day <jcd748@mail.usask.ca>
2016-09-25 14:01:06 -0500

    Added the draining flag to the damage description.

--- engine/strings/source/EquipmentTextKeys.cpp
+++ engine/strings/source/EquipmentTextKeys.cpp
@@ -124,6 +124,11 @@ string EquipmentTextKeys::get_weapon_difficulty_speed_and_damage_synopsis(const 
     dmg_flags.push_back(TextKeys::DAMAGE_VORPAL);
   }
 
+  if (damage.get_draining())
+  {
+    dmg_flags.push_back(TextKeys::DAMAGE_DRAINING);
+  }
+
   if (damage.get_piercing())
   {
     dmg_flags.push_back(TextKeys::DAMAGE_PIERCING);
