e39950635aa621e4b06219bc264337e7ca553ebb
Julian Day <jcd748@mail.usask.ca>
2016-10-18 21:58:13 -0500

    Converted the currency drop code to use racial item drop details specified in the game XML, rather than hard-coding item IDs/races in the engine.

--- engine/include/RaceManager.hpp
+++ engine/include/RaceManager.hpp
@@ -11,5 +11,8 @@ class RaceManager
     // Returns true if race_id = race_to_match, or if the race identified
     // by race_id is a descendent in the race hierarchy of the race represented
     // by race_to_match.
-    bool is_race_or_descendent(const std::string race_id, const std::string& race_to_match);
+    bool is_race_or_descendent(const std::string& race_id, const std::string& race_to_match);
+
+    // Aggregates all the drop chances from the current race up to the parent.
+    std::map<std::string, DropParameters> get_all_drops(const std::string& race_id);
 };
