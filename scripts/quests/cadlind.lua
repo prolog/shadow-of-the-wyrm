11a2ce41418154ec78f582612dacd2686f7f247f
Julian Day <jcd748@mail.usask.ca>
2016-10-19 20:05:33 -0500

    Chat text for Kew/Cadlind on dragons/constructs dropping magici shards.

--- scripts/quests/cadlind.lua
+++ scripts/quests/cadlind.lua
@@ -1,3 +1,6 @@
-add_message_with_pause("CADLIND_SPEECH_TEXT_SID")
-clear_and_add_message("CADLIND_SPEECH2_TEXT_SID")
+local chat_topics = {"CADLIND_SPEECH_TEXT_SID",
+                     "CADLIND_SPEECH_TEXT2_SID"}
+
+clear_and_add_message(chat_topics[RNG_range(1, table.getn(chat_topics))])
+
 
