1528d089c494ff79975d172a643e273214c7ca66
Julian Day <jcd748@mail.usask.ca>
2016-11-06 17:24:30 -0600

    Added a message when trying to chat while timewalking.  Fixed an issue with timewalking where it would sometimes wear off immediately.

--- engine/strings/include/ActionTextKeys.hpp
+++ engine/strings/include/ActionTextKeys.hpp
@@ -56,6 +56,7 @@ class ActionTextKeys
     static const std::string ACTION_FIRE;
     static const std::string ACTION_NOTHING_HAPPENS;
     static const std::string ACTION_CHAT_NOBODY_NEARBY;
+    static const std::string ACTION_CHAT_TIMEWALK;
     static const std::string ACTION_CHAT_NO_RESPONSE;
     static const std::string ACTION_GET_DIRECTION;
     static const std::string ACTION_GET_CARDINAL_DIRECTION;
--- engine/strings/source/ActionTextKeys.cpp
+++ engine/strings/source/ActionTextKeys.cpp
@@ -365,6 +365,7 @@ const string ActionTextKeys::ACTION_LOOK_TILE_OUT_OF_RANGE     = "ACTION_LOOK_TI
 const string ActionTextKeys::ACTION_FIRE                       = "ACTION_FIRE";
 const string ActionTextKeys::ACTION_NOTHING_HAPPENS            = "ACTION_NOTHING_HAPPENS";
 const string ActionTextKeys::ACTION_CHAT_NOBODY_NEARBY         = "ACTION_CHAT_NOBODY_NEARBY";
+const string ActionTextKeys::ACTION_CHAT_TIMEWALK              = "ACTION_CHAT_TIMEWALK";
 const string ActionTextKeys::ACTION_CHAT_NO_RESPONSE           = "ACTION_CHAT_NO_RESPONSE";
 const string ActionTextKeys::ACTION_GET_DIRECTION              = "ACTION_GET_DIRECTION";
 const string ActionTextKeys::ACTION_GET_CARDINAL_DIRECTION     = "ACTION_GET_CARDINAL_DIRECTION";
