52cd03105925e48ff26412c2744e78c27b7768d6
Julian Day <jcd748@mail.usask.ca>
2016-11-22 20:29:14 -0600

    Fix broken unit test.

--- world/source/unit_tests/Date_test.cpp
+++ world/source/unit_tests/Date_test.cpp
@@ -12,9 +12,9 @@ TEST(SW_World_Date, get_time_of_day)
 
   Date day_date1(0, 0, 7, 1, 1, 1, 1, 1);
 
-  EXPECT_EQ(TimeOfDayType::TIME_OF_DAY_DAY, day_date1.get_time_of_day());
+  EXPECT_EQ(TimeOfDayType::TIME_OF_DAY_DAWN, day_date1.get_time_of_day());
 
-  Date day_date2(0, 15, 16, 1, 1, 1, 1, 1);
+  Date day_date2(0, 15, 19, 1, 1, 1, 1, 1);
 
-  EXPECT_EQ(TimeOfDayType::TIME_OF_DAY_DAY, day_date2.get_time_of_day());
+  EXPECT_EQ(TimeOfDayType::TIME_OF_DAY_DUSK, day_date2.get_time_of_day());
 }
