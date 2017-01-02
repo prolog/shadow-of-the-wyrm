c9805f5e8445f719c1e58bf7666405a785d9f616
Julian Day <jcd748@mail.usask.ca>
2017-01-02 15:27:54 -0600

    ItemEnchantmentCalculator support for calculating the percent chance of a brand, given the enchantment multiplier, and the item's brandable and artifact flags.

--- engine/calculators/include/ItemEnchantmentCalculator.hpp
+++ engine/calculators/include/ItemEnchantmentCalculator.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "ItemTypes.hpp"
 
 // Calculation class used to determine how many enchantments an item
 // should get, based on the current danger level.  This is not a fixed
@@ -7,5 +8,9 @@ class ItemEnchantmentCalculator
 {
   public:
     int calculate_enchantments(const int danger_level);
+    int calculate_pct_chance_brand(const float pct_chance_multiplier, const bool brandable, const bool artifact);
+
+  protected:
+    static const int BASE_CHANCE_BRAND;
 };
 
