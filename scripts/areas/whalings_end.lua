52fb87dcd37d5d860b77e6bb71df930651f58c76
Julian Day <jcd748@mail.usask.ca>
2017-01-22 17:08:59 -0600

    Support for Whaling's End.

---
+++ scripts/areas/whalings_end.lua
@@ -0,0 +1,8 @@
+require('areas')
+
+local last_rock = Area:new(99, 59)
+last_rock:set_custom_map_id("whalings_end")
+last_rock:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_WHALINGS_END")
+
+last_rock:insert()
+
