29f2b401a84b814a7014c5b1bcd03914e8e2286f
Julian Day <jcd748@mail.usask.ca>
2017-01-29 09:01:57 -0600

    When the player has unpaid amounts owing, show it in the '$' command.  Restrict a shopkeeper's random movement when within a shop, to ensure that the shopkeeper stays in the shop.

--- engine/actions/source/CurrencyAction.cpp
+++ engine/actions/source/CurrencyAction.cpp
@@ -8,24 +8,25 @@ ActionCostValue CurrencyAction::count_currency(CreaturePtr creature)
 {
   if (creature)
   {
+    IMessageManager& manager = MM::instance(MessageTransmit::SELF, creature, creature && creature->get_is_player());
+
     uint currency_quantity = creature->get_inventory()->count_currency();
-    add_currency_message_if_necessary(creature, currency_quantity);    
+    string currency_message = TextMessages::get_currency_amount_message(currency_quantity);
+    manager.add_new_message(currency_message);
+
+    uint unpaid_amount = creature->get_unpaid_amount();
+    if (unpaid_amount > 0)
+    {
+      manager.add_new_message(TextMessages::get_unpaid_amount_message(unpaid_amount));
+    }
+
+    manager.send();
   }
 
   return get_action_cost_value(creature);
 }
 
-void CurrencyAction::add_currency_message_if_necessary(CreaturePtr creature, const uint currency_quantity)
-{
-  IMessageManager& manager = MM::instance(MessageTransmit::SELF, creature, creature && creature->get_is_player());
-    
-  string currency_message = TextMessages::get_currency_amount_message(currency_quantity);
-      
-  manager.add_new_message(currency_message);
-  manager.send();
-}
-
 ActionCostValue CurrencyAction::get_action_cost_value(CreaturePtr creature) const
 {
-  return 0; // Always costs nothing to double-check currency.
+  return 0; // Always costs nothing to double-check balances.
 }
