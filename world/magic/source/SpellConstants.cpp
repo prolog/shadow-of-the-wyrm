a5112c835d7f0f8219c38fad950ad45380254336
Julian Day <jcd748@mail.usask.ca>
2017-02-11 08:33:31 -0600

    Spellbooks now provide fewer castings when read.  Adventurers start with an identified wand, either lightning or teleport.

--- world/magic/source/SpellConstants.cpp
+++ world/magic/source/SpellConstants.cpp
@@ -7,8 +7,8 @@ const unsigned char SpellConstants::BEAM_BURST_SYMBOL = '*';
 const int SpellConstants::SPELL_FAILURE_BAD = -100;
 const int SpellConstants::SPELL_FAILURE_VERY_BAD = - 200;
 const int SpellConstants::MIN_CASTINGS = 10;
-const int SpellConstants::BASE_MIN_CASTINGS = 50;
-const int SpellConstants::BASE_MAX_CASTINGS = 100;
+const int SpellConstants::BASE_MIN_CASTINGS = 20;
+const int SpellConstants::BASE_MAX_CASTINGS = 50;
 const int SpellConstants::NO_CATEGORY_SKILL_SPELL_LEARNING_PENALTY = -100;
 
 SpellConstants::SpellConstants()
