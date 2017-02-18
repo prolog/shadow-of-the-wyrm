4661962e47a3b2764b91e66bd0fa5908c465ad43
Julian Day <jcd748@mail.usask.ca>
2017-02-18 11:36:15 -0600

    Deities can now remove the breeder flags from all creatures on the current level via prayer.

--- engine/decision_strategies/include/DecisionStrategy.hpp
+++ engine/decision_strategies/include/DecisionStrategy.hpp
@@ -38,6 +38,8 @@ class DecisionStrategy : public ISerializable
 
     void set_property(const std::string& prop, const std::string& val);
     std::string get_property(const std::string& prop) const;
+    bool remove_property(const std::string& prop);
+
     void set_properties(const std::map<std::string, std::string>& new_props);
     std::map<std::string, std::string>& get_properties_ref();
     std::map<std::string, std::string> get_properties() const;
