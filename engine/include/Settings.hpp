83aef44a7f64d84a6a8303ce944876ec8fdcaf6e
Julian Day <jcd748@mail.usask.ca>
2017-02-22 23:07:40 -0600

    Stanza.  Prep for reloading keybindings.  Ensure that creatures can't drop any items (either eq or random) when petrified.

--- engine/include/Settings.hpp
+++ engine/include/Settings.hpp
@@ -14,12 +14,16 @@ class Settings : public ISerializable
     std::string get_setting(const std::string& key) const;
     bool get_setting_as_bool(const std::string& key, const bool default_val = false) const;
 
+    // Get all the keybindings (settings whose key starts with "key_")
+    std::map<std::string, std::string> get_keybindings() const;
+
     bool serialize(std::ostream& stream) const override;
     bool deserialize(std::istream& stream) override;
 
   protected:
     boost::property_tree::ptree settings_tree;
     static const std::string SETTINGS_FILENAME;
+    static const std::string KEYBINDING_PREFIX;
 
   private:
     ClassIdentifier internal_class_identifier() const override;
