7637f5409bcf02757bebe2618da726cad4af973f
Julian Day <jcd748@mail.usask.ca>
2017-03-11 15:19:17 -0600

    Handle swimming for submerged tiles.

--- engine/time/source/SwimmingMovementAccumulationChecker.cpp
+++ engine/time/source/SwimmingMovementAccumulationChecker.cpp
@@ -1,4 +1,6 @@
 #include "CombatManager.hpp"
+#include "Game.hpp"
+#include "MapUtils.hpp"
 #include "SwimmingCalculator.hpp"
 #include "SwimmingMovementAccumulationChecker.hpp"
 #include "RNG.hpp"
@@ -9,8 +11,16 @@ void SwimmingMovementAccumulationChecker::check(CreaturePtr creature)
   if (creature)
   {
     SwimmingCalculator sc;
-    
-    ulonglong max_swimming_time = static_cast<ulonglong>(sc.calculate_maximum_swimming_time(sm.get_skill_value(creature, SkillType::SKILL_GENERAL_SWIMMING), creature->has_status(StatusIdentifiers::STATUS_ID_INCORPOREAL), creature->get_breathes()));
+    MapPtr current_map = Game::instance().get_current_map();
+    bool submerged = false;
+
+    if (current_map != nullptr)
+    {
+      TilePtr tile = MapUtils::get_tile_for_creature(current_map, creature);
+      submerged = tile && tile->get_submerged();
+    }
+
+    ulonglong max_swimming_time = static_cast<ulonglong>(sc.calculate_maximum_swimming_time(submerged, creature, creature->get_breathes()));
     MovementAccumulation& movement_accumulation = creature->get_movement_accumulation_ref();
     ulonglong time_in_water = movement_accumulation.get_minutes_on_super_type_given_movement();
 
