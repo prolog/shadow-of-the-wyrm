b70fd030fea79e2108a4c364dd7b6a14ccf3eec0
Julian Day <jcd748@mail.usask.ca>
2017-03-24 07:59:32 -0500

    The Worldsinger.

---
+++ scripts/quests/the_worldsinger.lua
@@ -0,0 +1,5 @@
+add_message_with_pause("THE_WORLDSINGER_SPEECH_TEXT_SID")
+add_message_with_pause("THE_WORLDSINGER_SPEECH_TEXT2_SID")
+add_message_with_pause("THE_WORLDSINGER_SPEECH_TEXT3_SID")
+clear_and_add_message("THE_WORLDSINGER_SPEECH_TEXT4_SID")
+
