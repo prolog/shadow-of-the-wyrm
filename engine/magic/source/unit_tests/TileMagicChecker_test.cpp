2f32326e4d884fcadc5e9aad1d163494a54c77d7
Julian Day <jcd748@mail.usask.ca>
2017-04-19 20:04:33 -0500

    TileMagicChecker no longer blocks when damage is incorporeal or the effect type is digging.

---
+++ engine/magic/source/unit_tests/TileMagicChecker_test.cpp
@@ -0,0 +1,38 @@
+#include "gtest/gtest.h"
+#include "FieldTile.hpp"
+#include "RockTile.hpp"
+#include "FirePillar.hpp"
+
+TEST(SW_Engine_Magic_TileMagicChecker, does_tile_block_spell)
+{
+  TileMagicChecker tmc;
+  Spell spell;
+  Spell incorp_spell;
+  Spell dig_spell;
+
+  Damage damage;
+  damage.set_incorporeal(true);
+  incorp_spell.set_damage(damage);
+
+  dig_spell.set_effect(EffectType::EFFECT_TYPE_DIGGING);
+
+  TilePtr rock = std::make_shared<RockTile>();
+  TilePtr field = std::make_shared<FieldTile>();
+  FeaturePtr fp = std::make_shared<FirePillar>();
+
+  // Solid tiles should block
+  EXPECT_TRUE(tmc.does_tile_block_spell(rock, spell));
+
+  // Regular tiles should not block
+  EXPECT_FALSE(tmc.does_tile_block_spell(field, spell));
+
+  // Blocking features should block
+  field->set_feature(fp);
+
+  EXPECT_TRUE(tmc.does_tile_block_spell(field, spell));
+
+  // Incorporeal damage and digging effects should prevent blockage.
+  EXPECT_FALSE(tmc.does_tile_block_spell(field, incorp_spell));
+  EXPECT_FALSE(tmc.does_tile_block_spell(field, dig_spell));
+}
+
