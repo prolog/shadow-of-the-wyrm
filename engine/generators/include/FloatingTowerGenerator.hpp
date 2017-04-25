25f96b8de5ed63f32d6d2008057f14cbefe86c9a
Julian Day <jcd748@mail.usask.ca>
2017-04-24 22:33:20 -0500

    Some initial empty functions in the floating tower generator.

--- engine/generators/include/FloatingTowerGenerator.hpp
+++ engine/generators/include/FloatingTowerGenerator.hpp
@@ -7,4 +7,8 @@ class FloatingTowerGenerator : public SOTW::Generator
     FloatingTowerGenerator(const std::string& map_exit_id);
     
     virtual MapPtr generate(const Dimensions& dim) override;
+
+  protected:
+    std::pair<Coordinate, Coordinate> generate_tower(MapPtr map);
+    void place_staircases(MapPtr map, const std::pair<Coordinate, Coordinate>& tower_boundaries);
 };
--- engine/generators/source/FloatingTowerGenerator.cpp
+++ engine/generators/source/FloatingTowerGenerator.cpp
@@ -1,4 +1,7 @@
 #include "FloatingTowerGenerator.hpp"
+#include "RNG.hpp"
+
+using namespace std;
 
 FloatingTowerGenerator::FloatingTowerGenerator(const std::string& new_map_exit_id)
 : Generator(new_map_exit_id, TileType::TILE_TYPE_AIR)
@@ -7,10 +10,40 @@ FloatingTowerGenerator::FloatingTowerGenerator(const std::string& new_map_exit_i
 
 MapPtr FloatingTowerGenerator::generate(const Dimensions& dimensions)
 {
-  MapPtr result_map = std::make_shared<Map>(dimensions);
+  MapPtr map = std::make_shared<Map>(dimensions);
  
-  fill(result_map, TileType::TILE_TYPE_AIR);
+  fill(map, TileType::TILE_TYPE_AIR);
 
-  return result_map;
+  pair<Coordinate, Coordinate> tower_boundaries = generate_tower(map);
+  place_staircases(map, tower_boundaries);
+
+  return map;
+}
+
+pair<Coordinate, Coordinate> FloatingTowerGenerator::generate_tower(MapPtr map)
+{
+  pair<Coordinate, Coordinate> boundaries({0,0}, {0,0});
+
+  if (map != nullptr)
+  {
+    Dimensions dim = map->size();
+    int rows = dim.get_y();
+    int cols = dim.get_x();
+
+    int width = RNG::range(cols * 0.6, cols * 0.8);
+    int height = RNG::range(cols * 0.6, cols * 0.8);
+
+    int start_row = (rows / 2) - (height / 2);
+    int start_col = (cols / 2) - (width / 2);
+    int end_row = start_row + height;
+    int end_col = start_col + width;
+
+    boundaries = {{start_row, start_col}, {end_row, end_col}};
+  }
+
+  return boundaries;
+}
+void FloatingTowerGenerator::place_staircases(MapPtr map, const pair<Coordinate, Coordinate>& tower_boundaries)
+{
 }
 
