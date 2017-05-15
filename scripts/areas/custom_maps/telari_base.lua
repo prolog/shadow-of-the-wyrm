aecd57ca334dbda3c8b2c7ed1c8e54faf3c8deec
Julian Day <jcd748@mail.usask.ca>
2017-05-15 18:59:12 -0500

    Stanza.

--- scripts/areas/custom_maps/telari_base.lua
+++ scripts/areas/custom_maps/telari_base.lua
@@ -5,8 +5,10 @@ require('custom_map')
 function init_telari_base(map_id)
   local height, width = map_get_dimensions(map_id)
   local consumables = {"gain_attributes_potion", "restorative_potion", 
-                       "teleport_wand", "dragon_breath_wand", 
-                       "vorpal_bolt_wand", "enchanting_scroll"}
+                       "amut_potion", "speed_potion", "ether_potion",
+                       "healing_potion", "teleport_wand", 
+                       "dragon_breath_wand", "vorpal_bolt_wand", 
+                       "enchanting_scroll"}
 
   log(CLOG_ERROR, "Height, width: " .. tostring(height) .. "," .. tostring(width))
   for row = 0, height-1 do
@@ -19,7 +21,7 @@ function init_telari_base(map_id)
           log(CLOG_ERROR, "Tile nil at " .. tostring(row) .. "," .. tostring(col))
         end
 
-        if tile_details ~= nil and tile_details["tile_type"] == CTILE_TYPE_DUNGEON then
+        if tile_details ~= nil and tile_type == CTILE_TYPE_DUNGEON then
           local item_id = consumables[RNG_range(1, #consumables)]
           add_object_to_map(item_id, map_id, row, col)
         end
