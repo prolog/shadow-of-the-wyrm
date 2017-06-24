32ed1536a8ac76c1778379a568cdb7d3088c9d12
Julian Day <jcd748@mail.usask.ca>
2017-06-23 20:42:44 -0500

    Recursive properties.  These are used to ensure all the randomly generated floating tower levels disallow digging and praying.

--- engine/generators/source/unit_tests/CrossPillarGenerator_test.cpp
+++ engine/generators/source/unit_tests/CrossPillarGenerator_test.cpp
@@ -1,6 +1,29 @@
 #include "gtest/gtest.h"
 #include "FieldGenerator.hpp"
 
+// This needs to be in one of the generator unit tests - here is as good
+// as anywhere!
+TEST(SW_World_Generators_CrossPillarGenerator, recursive_properties)
+{
+  FieldGenerator fg("hi!");
+  map<string, string> properties = {{"AAA", "123"},
+                                    {"BBB", "test"},
+                                    {"CCC", "this should come through"},
+                                    {"CCC_RECURSIVE", std::to_string(true)}};
+
+  fg.set_additional_properties(properties);
+
+  map<string, string> rec_properties = fg.get_recursive_properties();
+
+  EXPECT_EQ(2, rec_properties.size());
+
+  auto r_it = rec_properties.find("CCC");
+  EXPECT_TRUE(r_it != rec_properties.end());
+
+  r_it = rec_properties.find("CCC_RECURSIVE");
+  EXPECT_TRUE(r_it != rec_properties.end());
+}
+
 TEST(SW_World_Generators_CrossPillarGenerator, expected_shape)
 {
   FieldGenerator fg("foo");
