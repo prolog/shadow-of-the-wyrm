0a22c636389595a92bf7e38ce08b28552733c53e
Julian Day <jcd748@mail.usask.ca>
2017-08-17 10:16:07 -0500

    Additional features.  Fixed up some wonky code in the MapTester.  Some skeletal code for VoidGenerator to use the new features.

--- engine/generators/include/VoidGenerator.hpp
+++ engine/generators/include/VoidGenerator.hpp
@@ -7,4 +7,20 @@ class VoidGenerator : public SOTW::Generator
     VoidGenerator(const std::string& map_exit_id);
     
     virtual MapPtr generate(const Dimensions& dim) override;
+
+  protected:
+    void add_cosmic_dust(MapPtr result_map);
+    void add_celestial_objects(MapPtr result_map);
+    void add_spiral_arm(MapPtr result_map);
+
+    static const std::string FEATURE_ID_COSMIC_DUST;
+    static const std::string FEATURE_ID_DWARF_STAR;
+    static const std::string FEATURE_ID_RED_GIANT_STAR;
+    static const std::string FEATURE_ID_SUPERNOVA;
+    static const std::string FEATURE_ID_BLACK_HOLE;
+    static const std::string FEATURE_ID_PULSAR;
+
+    static const int PCT_CHANCE_SPACE_DUST;
+    static const int PCT_CHANCE_CELESTIAL_OBJECTS;
+    static const int PCT_CHANCE_SPIRAL_ARM;
 };
