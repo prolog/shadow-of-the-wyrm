ddf3909bf0380e17d99e0a7f850b49974858d1d2
Julian Day <jcd748@mail.usask.ca>
2017-08-22 19:55:14 -0500

    Celestial objects and spiral arms in void maps.

--- engine/generators/include/VoidGenerator.hpp
+++ engine/generators/include/VoidGenerator.hpp
@@ -13,21 +13,30 @@ class VoidGenerator : public SOTW::Generator
     void add_celestial_objects(MapPtr result_map);
     void add_spiral_arm(MapPtr result_map);
 
+    void add_random_features(MapPtr result_map, const std::vector<std::string>& feature_ids, const int num_features);
+
     static const std::string FEATURE_ID_COSMIC_DUST;
     static const std::string FEATURE_ID_DWARF_STAR;
     static const std::string FEATURE_ID_RED_GIANT_STAR;
     static const std::string FEATURE_ID_SUPERNOVA;
     static const std::string FEATURE_ID_BLACK_HOLE;
     static const std::string FEATURE_ID_PULSAR;
+    static const std::string FEATURE_ID_SPIRAL_ARM;
 
     static const int PCT_CHANCE_COSMIC_DUST;
     
-    // When there's cosmic dust, the rows and cols are multiplied together
-    // (to get the total number of tiles), and then divided by a divisor
-    // to determine how much dust is present.
+    // When there's cosmic dust/features, the rows and cols are multiplied 
+    // together (to get the total number of tiles), and then divided by a 
+    // divisor to determine how much dust is present.
     static const int COSMIC_DUST_DIV_MIN;
     static const int COSMIC_DUST_DIV_MAX;
+
+    static const int CELESTIAL_OBJECTS_DIV_MIN;
+    static const int CELESTIAL_OBJECTS_DIV_MAX;
     
     static const int PCT_CHANCE_CELESTIAL_OBJECTS;
+
     static const int PCT_CHANCE_SPIRAL_ARM;
+    static const int SPIRAL_ARM_DENSITY_PCT_MIN;
+    static const int SPIRAL_ARM_DENSITY_PCT_MAX;
 };
