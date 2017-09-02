7856909aa60c7875d32fdd98ea8ec31a68aae48e
Julian Day <jcd748@mail.usask.ca>
2017-09-02 12:32:53 -0500

    Harmonized custom map and map exit event scripts - these now use the same process (EventScript elements), and use CreateScript instead of LoadScript.

--- world/include/MapProperties.hpp
+++ world/include/MapProperties.hpp
@@ -71,9 +71,6 @@ class MapProperties
     static const std::string MAP_PROPERTIES_MIN_DEPTH;
     static const std::string MAP_PROPERTIES_MAX_DEPTH;
 
-    // Some custom maps have a script that's run after the map is loaded.
-    static const std::string MAP_PROPERTIES_LOAD_SCRIPT;
-
     // Shimmer colours exist on certain maps.  They are a CSV of
     // numbers representing colours: "x,y,z"
     //
