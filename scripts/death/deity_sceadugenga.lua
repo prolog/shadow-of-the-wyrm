4dc2949c4bbc5f112f7668717981e98b50ad94a6
Julian Day <jcd748@mail.usask.ca>
2017-09-25 15:14:08 -0500

    Tone down the status effect bonuses in Telari/cosmos.  Remove The Namer and the associated quest: mechanics are too fiddly, and it's more easily done by just not having the creature.  Create the down staircase from the base of Telari to the ocean after killing Sceadugenga.  With the removal of The Namer, added two artifact amulets to the bottom of the tower.  One is good against physical attacks (and has some soak), the other against magical (and has some evade).

--- scripts/death/deity_sceadugenga.lua
+++ scripts/death/deity_sceadugenga.lua
@@ -27,10 +27,19 @@ local function generate_telari_staircase(creature_id, attacking_creature_id, map
     -- Create a staircase back to the top of the Cloudspire.
     local cur_map_id = get_current_map_id()
     local telari_top_map_id = "telari_top"
+    local telari_base_map_id = "telari_base"
+    local telari_map_id = "telari"
     local pl_y, pl_x = get_creature_yx(PLAYER_ID)
 
+    -- Link from the cosmos back to the top of Telari
     map_transform_tile(cur_map_id, pl_y, pl_x, CTILE_TYPE_DOWN_STAIRCASE)
     map_add_tile_exit(cur_map_id, pl_y, pl_x, CDIRECTION_DOWN, telari_top_map_id)
+
+    -- Create a staircase back from the base of Telari to the undersea
+    -- world.
+    map_transform_tile(telari_base_map_id, 14, 40, CTILE_TYPE_DOWN_STAIRCASE)
+    map_add_tile_exit(telari_base_map_id, 14, 40, CDIRECTION_DOWN, telari_map_id)
+
     -- Remove the staircase at the top of the Cloudspire.  The shrine is now
     -- inert.
     map_transform_tile(telari_top_map_id, 9, 39, CTILE_TYPE_DAIS)
--- scripts/quests/the_worldsinger.lua
+++ scripts/quests/the_worldsinger.lua
@@ -1,5 +1,4 @@
 add_message_with_pause("THE_WORLDSINGER_SPEECH_TEXT_SID")
 add_message_with_pause("THE_WORLDSINGER_SPEECH_TEXT2_SID")
-add_message_with_pause("THE_WORLDSINGER_SPEECH_TEXT3_SID")
-clear_and_add_message("THE_WORLDSINGER_SPEECH_TEXT4_SID")
+clear_and_add_message("THE_WORLDSINGER_SPEECH_TEXT3_SID")
 
