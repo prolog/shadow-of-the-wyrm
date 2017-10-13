5454ab39f76eeb3049b693d0b2cbf146642e8b18
Julian Day <jcd748@mail.usask.ca>
2017-10-12 20:49:57 -0500

    If there are no more deities, the piety command should do nothing, and should also wipe out the creature's piety values for the gods that are no more.

--- engine/actions/include/PietyAction.hpp
+++ engine/actions/include/PietyAction.hpp
@@ -7,6 +7,8 @@ class PietyAction : public IActionManager
 {
   public:
     ActionCostValue piety(CreaturePtr creature, MapPtr map, ActionManager * const am) const;
+    void check_piety(CreaturePtr creature, IMessageManager& manager) const;
+    void remove_all_piety(CreaturePtr creature, IMessageManager& manager) const;
 
     ActionCostValue get_action_cost_value(CreaturePtr creature) const override;
 
