b421310f3ace3648f5b5272c74f91f6caa71a85c
Julian Day <jcd748@mail.usask.ca>
2017-10-19 20:47:52 -0500

    Remove the restriction on the Lua console being debug mode only.

--- engine/commands/source/KeyboardCommandMap.cpp
+++ engine/commands/source/KeyboardCommandMap.cpp
@@ -177,12 +177,7 @@ void KeyboardCommandMap::initialize_command_mapping(const Settings& settings)
   parse_keybindings(settings, remappable_commands);
 
   // Non-remappable commands
-  //
-  // JCD FIXME: When adding Unix support, update this to include whatever
-  // debug flag is needed.
-  #ifdef _DEBUG
   command_mapping.insert(make_pair(std::to_string(KEY_F(11)), CommandKeys::RUN_SCRIPT));
-  #endif
   command_mapping.insert(make_pair(std::to_string(KEY_F(12)), CommandKeys::RELOAD_SCRIPTS_AND_SIDS));
 }
 
