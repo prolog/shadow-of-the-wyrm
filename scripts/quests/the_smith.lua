f852cce71aa8fda7aff8af30957a3ce3e0d624bc
Julian Day <jcd748@mail.usask.ca>
2017-11-07 20:18:33 -0600

    Stanza. Lua API function to check number of deities, and alternative speech text for The Smith for players who return to the Cloudspire after slaying the Nine.

--- scripts/quests/the_smith.lua
+++ scripts/quests/the_smith.lua
@@ -1,63 +1,68 @@
 require('constants')
 require('fn')
 
-local divine_race_id = "_divine"
-local smith_prop = "the_smith_gift_generated"
-local given_gift = get_creature_additional_property(PLAYER_ID, smith_prop)
-
-if string.len(given_gift) == 0 then
-  local num_dice, num_sides, modifier = get_creature_base_damage(PLAYER_ID)
-  local level = get_creature_level(PLAYER_ID)
-  local class_id = get_class_id(PLAYER_ID)
-
-  if class_id == "pugilist" then
-    add_message_with_pause("THE_SMITH_SPEECH_UNARMED_SID")
-
-    -- If the player is skilled at unarmed combat, then:
-    --
-    -- 1. Give a large damage increase
-    modifier = modifier + 400
-    --
-    -- 2. If the player doesn't have slay divine, grant it.  If the player
-    --    has that slay, grant an additional stack of damage.
-    local slays = {get_unarmed_slays(PLAYER_ID)}
-
-    if fn.contains(slays, divine_race_id) then
-      modifier = modifier + 250
-      clear_and_add_message("THE_SMITH_SPEECH_UNARMED_SLAY_SID")
-    else
-      add_unarmed_slay(PLAYER_ID, divine_race_id)
-      clear_and_add_message("THE_SMITH_SPEECH_UNARMED_NO_SLAY_SID")
-    end
-
-    set_creature_base_damage(PLAYER_ID, num_dice, num_sides, modifier)
-  else
-    -- If the creature has Dual Wield skill, there's a <skill>% chance of
-    -- being given the Shard of Starlight.
-    local dual_wield = get_skill_value(PLAYER_ID, CSKILL_GENERAL_DUAL_WIELD)
-    if RNG_percent_chance(dual_wield) then
-      add_message_with_pause("THE_SMITH_SPEECH_SHARD_SID")
-      add_message_with_pause("THE_SMITH_SPEECH_SHARD2_SID")
-      add_message_with_pause("THE_SMITH_SPEECH_SHARD3_SID")
-      clear_and_add_message("THE_SMITH_SPEECH_SHARD4_SID")
-
-      add_object_to_player_tile("shard_starlight")
-    else
-      -- If the creature doesn't meet the unarmed/dual wield requirements,
-      -- grant the God-Blade.
-      add_message_with_pause("THE_SMITH_SPEECH_GODBLADE_SID")
-      add_message_with_pause("THE_SMITH_SPEECH_GODBLADE2_SID")
-      add_message_with_pause("THE_SMITH_SPEECH_GODBLADE3_SID")
-      add_message_with_pause("THE_SMITH_SPEECH_GODBLADE4_SID")
-      clear_and_add_message("THE_SMITH_SPEECH_GODBLADE5_SID")
-
-      add_object_to_player_tile("godblade")
-    end
-  end
-
-  -- Set the gift property so that this'll be skipped next time.
-  set_creature_additional_property(PLAYER_ID, smith_prop, "1")
+if get_num_deities() == 0 then
+  clear_and_add_message("THE_SMITH_GODSLAYER_SPEECH_TEXT_SID")
 else
-  clear_and_add_message("THE_SMITH_SPEECH_TEXT_SID")
-end
+  local divine_race_id = "_divine"
+  local smith_prop = "the_smith_gift_generated"
+  local given_gift = get_creature_additional_property(PLAYER_ID, smith_prop)
 
+  if string.len(given_gift) == 0 then
+    local num_dice, num_sides, modifier = get_creature_base_damage(PLAYER_ID)
+    local level = get_creature_level(PLAYER_ID)
+    local class_id = get_class_id(PLAYER_ID)
+
+    if class_id == "pugilist" then
+      add_message_with_pause("THE_SMITH_SPEECH_UNARMED_SID")
+
+      -- If the player is skilled at unarmed combat, then:
+      --
+      -- 1. Give a large damage increase
+      
+      modifier = modifier + 400
+      
+      --
+      -- 2. If the player doesn't have slay divine, grant it.  If the player
+      --    has that slay, grant an additional stack of damage.
+      local slays = {get_unarmed_slays(PLAYER_ID)}
+
+      if fn.contains(slays, divine_race_id) then
+        modifier = modifier + 250
+        clear_and_add_message("THE_SMITH_SPEECH_UNARMED_SLAY_SID")
+      else
+        add_unarmed_slay(PLAYER_ID, divine_race_id)
+        clear_and_add_message("THE_SMITH_SPEECH_UNARMED_NO_SLAY_SID")
+      end
+
+      set_creature_base_damage(PLAYER_ID, num_dice, num_sides, modifier)
+    else
+      -- If the creature has Dual Wield skill, there's a <skill>% chance of
+      -- being given the Shard of Starlight.
+      local dual_wield = get_skill_value(PLAYER_ID, CSKILL_GENERAL_DUAL_WIELD)
+      if RNG_percent_chance(dual_wield) then
+        add_message_with_pause("THE_SMITH_SPEECH_SHARD_SID")
+        add_message_with_pause("THE_SMITH_SPEECH_SHARD2_SID")
+        add_message_with_pause("THE_SMITH_SPEECH_SHARD3_SID")
+        clear_and_add_message("THE_SMITH_SPEECH_SHARD4_SID")
+
+        add_object_to_player_tile("shard_starlight")
+      else
+        -- If the creature doesn't meet the unarmed/dual wield requirements,
+        -- grant the God-Blade.
+        add_message_with_pause("THE_SMITH_SPEECH_GODBLADE_SID")
+        add_message_with_pause("THE_SMITH_SPEECH_GODBLADE2_SID")
+        add_message_with_pause("THE_SMITH_SPEECH_GODBLADE3_SID")
+        add_message_with_pause("THE_SMITH_SPEECH_GODBLADE4_SID")
+        clear_and_add_message("THE_SMITH_SPEECH_GODBLADE5_SID")
+
+        add_object_to_player_tile("godblade")
+      end
+    end
+
+    -- Set the gift property so that this'll be skipped next time.
+    set_creature_additional_property(PLAYER_ID, smith_prop, "1")
+  else
+    clear_and_add_message("THE_SMITH_SPEECH_TEXT_SID")
+  end
+end
