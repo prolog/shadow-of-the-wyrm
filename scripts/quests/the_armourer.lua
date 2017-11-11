346200845a6dee4208e94d79d1d4a4076ef97206
Julian Day <jcd748@mail.usask.ca>
2017-11-11 09:32:37 -0600

    Post-godslayer text for Ead, The Conjurer, The Armourer.

--- scripts/quests/the_armourer.lua
+++ scripts/quests/the_armourer.lua
@@ -1,29 +1,33 @@
 require('constants')
 
-local armourer_gift_key = "armourer_gift_generated"
-local gift_generated = get_creature_additional_property(PLAYER_ID, armourer_gift_key)
-
-if string.len(gift_generated) == 0 then
-  -- Generate one of the three possible gifts.
-  -- The shield has 75/25 divine/shadow, and is best against the eight
-  -- "regular" deities.
-  -- The helm has 75/25 shadow/divine, and provides the best protection
-  -- against Sceadugenga.
-  -- The mail is 50/50.
-  if RNG_percent_chance(33) then
-    add_object_to_player_tile("telarian_helm")
-  elseif RNG_percent_chance(33) then
-    add_object_to_player_tile("telarian_shield")
-  else
-    add_object_to_player_tile("telarian_mail")
-  end
-
-  add_message_with_pause("THE_ARMOURER_SPEECH_ARMOUR_SID")
-  add_message_with_pause("THE_ARMOURER_SPEECH_ARMOUR2_SID")
-  clear_and_add_message("THE_ARMOURER_SPEECH_ARMOUR3_SID")
-
-  set_creature_additional_property(PLAYER_ID, armourer_gift_key, "1")
+if get_num_deities() == 0 then
+  add_message_with_pause("THE_ARMOURER_GODSLAYER_SPEECH_TEXT_SID")
+  clear_and_add_message("THE_ARMOURER_GODSLAYER_SPEECH_TEXT2_SID")
 else
-  clear_and_add_message("THE_ARMOURER_SPEECH_TEXT_SID")
-end
+  local armourer_gift_key = "armourer_gift_generated"
+  local gift_generated = get_creature_additional_property(PLAYER_ID, armourer_gift_key)
 
+  if string.len(gift_generated) == 0 then
+    -- Generate one of the three possible gifts.
+    -- The shield has 75/25 divine/shadow, and is best against the eight
+    -- "regular" deities.
+    -- The helm has 75/25 shadow/divine, and provides the best protection
+    -- against Sceadugenga.
+    -- The mail is 50/50.
+    if RNG_percent_chance(33) then
+      add_object_to_player_tile("telarian_helm")
+    elseif RNG_percent_chance(33) then
+      add_object_to_player_tile("telarian_shield")
+    else
+      add_object_to_player_tile("telarian_mail")
+    end
+
+    add_message_with_pause("THE_ARMOURER_SPEECH_ARMOUR_SID")
+    add_message_with_pause("THE_ARMOURER_SPEECH_ARMOUR2_SID")
+    clear_and_add_message("THE_ARMOURER_SPEECH_ARMOUR3_SID")
+
+    set_creature_additional_property(PLAYER_ID, armourer_gift_key, "1")
+  else
+    clear_and_add_message("THE_ARMOURER_SPEECH_TEXT_SID")
+  end
+end
--- scripts/quests/the_conjurer.lua
+++ scripts/quests/the_conjurer.lua
@@ -56,47 +56,52 @@ local function create_spellbook_reward(spellbooks, highest_magic)
   end
 end
 
--- Determine which gift to give the player, and then set the gift
--- property as generated.
-local conjurer_gift_key = "conjurer_gift_generated"
-local gift_generated = get_creature_additional_property(PLAYER_ID, conjurer_gift_key)
-
-if gift_generated ~= "1" then
-  local spellbooks = get_spellbooks()
-
-  local magic_skills = get_magic_skills(PLAYER_ID)
-  table.sort(magic_skills)
-
-  -- Halve the value of Cantrips.  If it's very high and the other skills
-  -- are very low, it should still be considered for the Conjurer's reward.
-  -- But for example, if the player has maxed Cantrips and Primordial,
-  -- the player should always get the witchling reward.
-  if magic_skills[CSKILL_MAGIC_CANTRIPS] ~= nil then
-    magic_skills[CSKILL_MAGIC_CANTRIPS] = (magic_skills[C_SKILL_MAGIC_CANTRIPS] / 2)
-  end
-
-  local highest_magic, max_val = get_highest_magic_skill(magic_skills)
-
-  -- If the player has no magic skills, he or she gets a wand of inferno.
-  if max_val == 0 or highest_magic == -1 then
-    create_no_magic_reward()
-  -- Primordial casters get access to antimatter.
-  elseif highest_magic == tostring(CSKILL_MAGIC_PRIMORDIAL) then
-    create_primordial_reward()
-  -- Cantrips users get a spellbook of Inferno.
-  elseif highest_magic == tostring(CSKILL_MAGIC_CANTRIPS) then
-    local spellbook_id = spellbooks[highest_magic]
-    add_object_to_player_tile(spellbook_id)
-
-    add_message_with_pause("THE_CONJURER_CANTRIPS_TEXT_SID")
-    clear_and_add_message("THE_CONJURER_CANTRIPS_TEXT2_SID")
-  -- Spellcasters get a copy of a mega-spellbook.
-  else
-    create_spellbook_reward(spellbooks, highest_magic)
-  end
-
-  set_creature_additional_property(PLAYER_ID, conjurer_gift_key, "1")
+if get_num_deities() == 0 then
+  add_message_with_pause("THE_CONJURER_GODSLAYER_SPEECH_TEXT_SID")
+  add_message_with_pause("THE_CONJURER_GODSLAYER_SPEECH_TEXT2_SID")
+  clear_and_add_message("THE_CONJURER_GODSLAYER_SPEECH_TEXT3_SID")
 else
-  clear_and_add_message("THE_CONJURER_SPEECH_TEXT_SID")
-end
+  -- Determine which gift to give the player, and then set the gift
+  -- property as generated.
+  local conjurer_gift_key = "conjurer_gift_generated"
+  local gift_generated = get_creature_additional_property(PLAYER_ID, conjurer_gift_key)
 
+  if gift_generated ~= "1" then
+    local spellbooks = get_spellbooks()
+
+    local magic_skills = get_magic_skills(PLAYER_ID)
+    table.sort(magic_skills)
+
+    -- Halve the value of Cantrips.  If it's very high and the other skills
+    -- are very low, it should still be considered for the Conjurer's reward.
+    -- But for example, if the player has maxed Cantrips and Primordial,
+    -- the player should always get the witchling reward.
+    if magic_skills[CSKILL_MAGIC_CANTRIPS] ~= nil then
+      magic_skills[CSKILL_MAGIC_CANTRIPS] = (magic_skills[C_SKILL_MAGIC_CANTRIPS] / 2)
+    end
+
+    local highest_magic, max_val = get_highest_magic_skill(magic_skills)
+
+    -- If the player has no magic skills, he or she gets a wand of inferno.
+    if max_val == 0 or highest_magic == -1 then
+      create_no_magic_reward()
+    -- Primordial casters get access to antimatter.
+    elseif highest_magic == tostring(CSKILL_MAGIC_PRIMORDIAL) then
+      create_primordial_reward()
+    -- Cantrips users get a spellbook of Inferno.
+    elseif highest_magic == tostring(CSKILL_MAGIC_CANTRIPS) then
+      local spellbook_id = spellbooks[highest_magic]
+      add_object_to_player_tile(spellbook_id)
+
+      add_message_with_pause("THE_CONJURER_CANTRIPS_TEXT_SID")
+      clear_and_add_message("THE_CONJURER_CANTRIPS_TEXT2_SID")
+    -- Spellcasters get a copy of a mega-spellbook.
+    else
+      create_spellbook_reward(spellbooks, highest_magic)
+    end
+
+    set_creature_additional_property(PLAYER_ID, conjurer_gift_key, "1")
+  else
+    clear_and_add_message("THE_CONJURER_SPEECH_TEXT_SID")
+  end
+end
