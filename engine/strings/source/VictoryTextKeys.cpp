c0d6c16477d4423a137b3bffdfe47801b818c7d3
Julian Day <jcd748@mail.usask.ca>
2017-11-23 19:35:55 -0600

    Add victory information to character dumps when appropriate.

---
+++ engine/strings/source/VictoryTextKeys.cpp
@@ -0,0 +1,17 @@
+#include "VictoryTextKeys.hpp"
+
+using namespace std;
+
+VictoryTextKeys::VictoryTextKeys()
+{
+}
+
+VictoryTextKeys::~VictoryTextKeys()
+{
+}
+
+const string VictoryTextKeys::VICTORY = "VICTORY";
+const string VictoryTextKeys::VICTORY_1 = "VICTORY_1";
+const string VictoryTextKeys::VICTORY_2 = "VICTORY_2";
+const string VictoryTextKeys::VICTORY_3 = "VICTORY_3";
+
