37ace53a2d3b4ad11cd57e3ad7599d55c8c2b97c
Julian Day <jcd748@mail.usask.ca>
2017-12-14 18:37:15 -0600

    Allow an instrument's status to affect the chance of pacification.

--- engine/calculators/source/PacificationCalculator.cpp
+++ engine/calculators/source/PacificationCalculator.cpp
@@ -1,6 +1,6 @@
 #include "PacificationCalculator.hpp"
 
-const int PacificationCalculator::MAX_PCT_CHANCE_PACIFY_MUSIC = 95;
+const int PacificationCalculator::MAX_PCT_CHANCE_PACIFY_MUSIC = 90;
 const int PacificationCalculator::CHARMS_BONUS = 25;
 
 // The chance to pacify musically is:
@@ -11,7 +11,8 @@ const int PacificationCalculator::CHARMS_BONUS = 25;
 // - half the opponent's level
 // - half the opponent's willpower
 // + charms bonus, if specified
-int PacificationCalculator::calculate_pct_chance_pacify_music(CreaturePtr music_creature, CreaturePtr fov_creature, bool charms_creature)
+// + applicable item status bonus or penalty
+int PacificationCalculator::calculate_pct_chance_pacify_music(CreaturePtr music_creature, CreaturePtr fov_creature, bool charms_creature, const ItemStatus item_status) const
 {
   int pct_chance_pacify = 0;
 
@@ -23,16 +24,40 @@ int PacificationCalculator::calculate_pct_chance_pacify_music(CreaturePtr music_
     int opp_lvl   = fov_creature->get_level().get_current() / 2;
     int opp_will  = fov_creature->get_willpower().get_current() / 2;
     int charms    = charms_creature ? CHARMS_BONUS : 0;
+    int status    = get_item_status_bonus(item_status);
 
     pct_chance_pacify = music_val
                       + music_lvl
                       + music_cha
                       - opp_lvl
                       - opp_will
-                      + charms;
+                      + charms
+                      + status;
   }
 
+  pct_chance_pacify = std::max<int>(pct_chance_pacify, 0);
   return std::min<int>(pct_chance_pacify, MAX_PCT_CHANCE_PACIFY_MUSIC);
+}
+
+int PacificationCalculator::get_item_status_bonus(const ItemStatus status) const
+{
+  int bonus = 0;
+
+  switch (status)
+  {
+    case ItemStatus::ITEM_STATUS_CURSED:
+      bonus = -20;
+      break;
+    case ItemStatus::ITEM_STATUS_BLESSED:
+      bonus = 20;
+      break;
+    case ItemStatus::ITEM_STATUS_UNCURSED:
+    default:
+      bonus = 0;
+      break;
+  }
+
+  return bonus;
 }
 
 #ifdef UNIT_TESTS
