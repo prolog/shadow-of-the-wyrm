fc943c8495d4c457d686f689753d460d911d1e12
Julian Day <jcd748@mail.usask.ca>
2018-01-28 10:53:17 -0600

    Set up Carcassia details on the Isle.  Glich.

--- scripts/areas/central_lands.lua
+++ scripts/areas/central_lands.lua
@@ -2,9 +2,10 @@ require('constants')
 require('areas')
 
 -- Set up the seat of the empire.
-local carcassia = Area:new(39, 50)
+local carcassia = Area:new(47, 53)
 
 -- Set up the city tile appropriately.
+carcassia:set_custom_map_id("carcassia_a1")
 carcassia:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_CARCASSIA")
 carcassia:insert()
 
