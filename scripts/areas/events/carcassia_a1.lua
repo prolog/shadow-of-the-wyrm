361a5de60d58ed8f99a7db56326b94eb57a7d0cf
Julian Day <jcd748@mail.usask.ca>
2018-02-14 19:47:08 -0600

    Parklands around the teleport room.

--- scripts/areas/events/carcassia_a1.lua
+++ scripts/areas/events/carcassia_a1.lua
@@ -3,6 +3,28 @@ require('map_events')
 
 local map_id = "carcassia_a1"
 
+-- JCD FIXME: Consider making this a generic Lua API function in the
+-- engine.
+function setup_parklands_around_treasure_room(map_id)
+  local start_y, start_x = 2,3
+  local end_y, end_x = 9,11
+  local mnum = RNG_range(2, 3) -- Modulo used to determine spacing
+
+  for y = start_y, end_y do
+    for x = start_x, end_x do
+      local tile_details = map_get_tile(map_id, y, x)
+
+      if tile_details ~= nil then
+        local ttype = tile_details["tile_type"]
+
+        if ttype == CTILE_TYPE_FIELD and ((y + x) % mnum == 0) then
+          map_transform_tile(map_id, y, x, CTILE_TYPE_TREE)
+        end
+      end
+    end
+  end
+end
+
 function setup_nodig_treasure_room_and_traps(map_id)
   -- Ensure the teleport room is no-dig, while keeping the rest of
   -- the map diggable.
@@ -41,10 +63,11 @@ function setup_nodig_treasure_room_and_traps(map_id)
 
   -- Set the teleporter from the closed room to just outside the gate.
   set_trap(4, 8, false, teleport_trap_id, map_id)
-  set_feature_additional_property(map_id, 4, 8, teleport_loc_prop, "10,1") 
+  set_feature_additional_property(map_id, 4, 8, teleport_loc_prop, "10,1")
 end
 
 function init_carcassia_a1(map_id)
+  setup_parklands_around_treasure_room(map_id)
   setup_nodig_treasure_room_and_traps(map_id)
 end
 
