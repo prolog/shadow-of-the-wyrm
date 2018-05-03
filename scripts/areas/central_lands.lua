71ec6e8a2540c7f7c1f60e8bf255c53a4c9596cd
Julian Day <jcd748@mail.usask.ca>
2018-05-03 09:59:45 -0400

    Add a dungeon to the NE tip of Carcassia.

--- scripts/areas/central_lands.lua
+++ scripts/areas/central_lands.lua
@@ -1,6 +1,11 @@
 require('constants')
 require('areas')
 
+-- Details for Kazmar's dungeon.
+local dungeon = Area:new(43,52)
+dungeon:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_CARCASSIA_DUNGEON")
+dungeon:set_additional_property("MAP_PROPERTIES_MAX_DEPTH", 10)
+
 -- Set up the seat of the empire.
 local carcassia = Area:new(47, 53)
 
@@ -9,3 +14,11 @@ carcassia:set_custom_map_id("carcassia_a1")
 carcassia:set_extra_description_sid("TILE_EXTRA_DESCRIPTION_CARCASSIA")
 carcassia:insert()
 
+local carcassia_island = {}
+
+table.insert(carcassia_island, dungeon)
+table.insert(carcassia_island, carcassia)
+
+for k,v in pairs(carcassia_island) do
+  v:insert()
+end
