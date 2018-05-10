f7d28b3991fbffa9fdfb49355509cf10e04b586f
Julian Day <jcd748@mail.usask.ca>
2018-05-09 22:00:53 -0500

    Update digging so that digging through rock has a chance to break the utensil, but other tiles won't (earth is soft and won't damage the item).  Ensure that the code to check for breakage is also called when digging within a tile.

--- engine/calculators/source/unit_tests/ItemBreakageCalculator_test.cpp
+++ engine/calculators/source/unit_tests/ItemBreakageCalculator_test.cpp
@@ -1,21 +1,31 @@
 #include "gtest/gtest.h"
 #include "Amulet.hpp"
+#include "RockTile.hpp"
+#include "EarthTile.hpp"
 
 TEST(SW_Engine_Calculator_ItemBreakageCalculator, calculate_pct_chance_digging_breakage)
 {
   ItemBreakageCalculator ibc;
+  TilePtr rock_tile = std::make_shared<RockTile>();
+  TilePtr earth_tile = std::make_shared<EarthTile>();
 
-  EXPECT_EQ(50, ibc.calculate_pct_chance_digging_breakage(nullptr, nullptr));
+  EXPECT_EQ(50, ibc.calculate_pct_chance_digging_breakage(nullptr, nullptr, nullptr));
+  EXPECT_EQ(0, ibc.calculate_pct_chance_digging_breakage(nullptr, earth_tile, nullptr));
+  EXPECT_EQ(50, ibc.calculate_pct_chance_digging_breakage(nullptr, rock_tile, nullptr));
 
   ItemPtr amulet = std::make_shared<Amulet>();
   CreaturePtr creature = std::make_shared<Creature>();
   amulet->set_additional_property(ItemProperties::ITEM_PROPERTIES_DIG_BREAKAGE_PCT_CHANCE, "18");
 
-  EXPECT_EQ(18, ibc.calculate_pct_chance_digging_breakage(creature, amulet));
+  EXPECT_EQ(18, ibc.calculate_pct_chance_digging_breakage(creature, nullptr, amulet));
+  EXPECT_EQ(0, ibc.calculate_pct_chance_digging_breakage(creature, earth_tile, amulet));
+  EXPECT_EQ(18, ibc.calculate_pct_chance_digging_breakage(creature, rock_tile, amulet));
 
   creature->get_skills().set_value(SkillType::SKILL_GENERAL_DUNGEONEERING, 50);
 
-  EXPECT_EQ(9, ibc.calculate_pct_chance_digging_breakage(creature, amulet));
+  EXPECT_EQ(9, ibc.calculate_pct_chance_digging_breakage(creature, nullptr, amulet));
+  EXPECT_EQ(0, ibc.calculate_pct_chance_digging_breakage(creature, earth_tile, amulet));
+  EXPECT_EQ(9, ibc.calculate_pct_chance_digging_breakage(creature, rock_tile, amulet));
 }
 
 TEST(SW_Engine_Calculator_ItemBreakageCalculator, calculate_skill_breakage_multiplier)
