2d38468634991e86b775d4ad64e1059ece3278e0
Julian Day <jcd748@mail.usask.ca>
2018-06-20 21:00:15 -0500

    Common buy-in code for casino dealers.

--- scripts/quests/carcassia/pov_dealer.lua
+++ scripts/quests/carcassia/pov_dealer.lua
@@ -1,2 +1,6 @@
-clear_and_add_message("POV_DEALER_SPEECH_TEXT_SID")
+require('dealer')
+
+if dealer.buy_in("POV_DEALER_SPEECH_TEXT_SID", "POV_DEALER_NSF_MESSAGE_SID", "POV_DEALER_DECLINE_MESSAGE_SID", 2) then
+  -- ...
+end
 
