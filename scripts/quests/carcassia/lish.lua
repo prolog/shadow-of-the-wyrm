6a495f59aac28b084b4844615f8bb8c379d40262
Julian Day <jcd748@mail.usask.ca>
2018-07-25 20:52:03 -0500

    New artifact whip, Calara's flaming whip.  Another quest for Lish in Carcassia.

--- scripts/quests/carcassia/lish.lua
+++ scripts/quests/carcassia/lish.lua
@@ -22,19 +22,56 @@ local function lish_silverweed_completion_fn()
   return true  
 end
 
-lish_quest = Quest:new("lish_silverweed",
-                       "LISH_SILVERWEED_QUEST_TITLE_SID",
-                       "LISH_SHORT_DESCRIPTION_SID",
-                       "LISH_SILVERWEED_QUEST_DESCRIPTION_SID",
-                       "LISH_SILVERWEED_QUEST_COMPLETE_SID",
-                       "LISH_SILVERWEED_QUEST_REMINDER_SID",
-                       truefn,
-                       lish_silverweed_start_fn,
-                       lish_silverweed_completion_condition_fn,
-                       lish_silverweed_completion_fn)
+lish_silverweed_quest = Quest:new("lish_silverweed",
+                                  "LISH_SILVERWEED_QUEST_TITLE_SID",
+                                  "LISH_SHORT_DESCRIPTION_SID",
+                                  "LISH_SILVERWEED_QUEST_DESCRIPTION_SID",
+                                  "LISH_SILVERWEED_QUEST_COMPLETE_SID",
+                                  "LISH_SILVERWEED_QUEST_REMINDER_SID",
+                                  truefn,
+                                  lish_silverweed_start_fn,
+                                  lish_silverweed_completion_condition_fn,
+                                  lish_silverweed_completion_fn)
 
-if lish_quest:execute() == false then
-  add_message("LISH_SPEECH_TEXT_SID")
+-- Lish's second quest is to find out what happened to her lover.
+local function lish_lover_start_fn()
+  add_message_with_pause("LISH_LOVER_QUEST_START_SID")
+  add_message_with_pause("LISH_LOVER_QUEST_START2_SID")
+  clear_and_add_message("LISH_LOVER_QUEST_START3_SID")
 end
 
-                       
+local function lish_lover_completion_condition_fn()
+  return player_has_item("calara_cloak")
+end
+
+local function lish_lover_completion_fn()
+  add_message_with_pause("LISH_LOVER_QUEST_COMPLETE_SID")
+  add_message_with_pause("LISH_LOVER_QUEST_COMPLETE2_SID")
+  add_message_with_pause("LISH_LOVER_QUEST_COMPLETE3_SID")
+  add_message_with_pause("LISH_LOVER_QUEST_COMPLETE4_SID")
+  add_message_with_pause("LISH_LOVER_QUEST_COMPLETE5_SID")
+  clear_and_add_message("LISH_LOVER_QUEST_COMPLETE6_SID")
+
+  remove_object_from_player("calara_cloak")
+  add_object_to_player_tile("calara_whip")
+
+  return true
+end
+
+lish_lover_quest = Quest:new("lish_lover",
+                             "LISH_LOVER_QUEST_TITLE_SID",
+                             "LISH_SHORT_DESCRIPTION_SID",
+                             "LISH_LOVER_QUEST_DESCRIPTION_SID",
+                             "LISH_LOVER_QUEST_COMPLETE_SID",
+                             "LISH_LOVER_QUEST_REMINDER_SID",
+                             truefn,
+                             lish_lover_start_fn,
+                             lish_lover_completion_condition_fn,
+                             lish_lover_completion_fn)
+
+if lish_silverweed_quest:execute() == false then
+  if lish_lover_quest:execute() == false then
+    add_message("LISH_SPEECH_TEXT_SID")
+  end
+end
+                  
