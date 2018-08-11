c5671795bc8f709d733debd7110f722691a94e7e
Julian Day <jcd748@mail.usask.ca>
2018-08-10 20:39:14 -0500

    Flowers in front of the palace at Carcassia.

---
+++ engine/generators/settlements/sector/include/ResidentialSectorFeatureGenerator.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "SectorFeatureGenerator.hpp"
+
+enum struct ResidentialSectorFeatureType
+{
+  RESIDENTIAL_SECTOR_FIRST = 0,
+  RESIDENTIAL_SECTOR_FEATURE_GARDEN = 0,
+  RESIDENTIAL_SECTOR_FEATURE_FLOWER_GARDEN = 1,
+  RESIDENTIAL_SECTOR_LAST = 2
+};
+
+class ResidentialSectorFeatureGenerator : public SectorFeatureGenerator
+{
+  public:
+    ResidentialSectorFeatureGenerator();
+
+  protected:
+    bool create_feature(MapPtr map, const Coordinate& start_coord, const Coordinate& end_coord, const int feat_idx, const int f_type);
+    
+    ResidentialSectorFeatureType get_random_feature(const int feat_idx) const;
+    std::map<int, ResidentialSectorFeatureType> features;
+};
+
