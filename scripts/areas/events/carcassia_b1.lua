499c9450c945c46e015cf13aac6bfe0e9a4d6b37
Julian Day <jcd748@mail.usask.ca>
2018-09-08 10:20:29 -0500

    Additional guard at the entrance to Carcassia to match the east gate. Link the roads and ensure a road runs to both entrances to the fortress. More features in the residential area, though these are currently just gardens and the generator will be updated later. Stanza for "Tel and Floridel".

--- scripts/areas/events/carcassia_b1.lua
+++ scripts/areas/events/carcassia_b1.lua
@@ -16,6 +16,20 @@ local function setup_dynamic_content(m_id)
   -- Either rocks or wildflowers in front of the palace.
   generate_city_feature(m_id, 13, 37, 18, 56, CCITY_SECTOR_RESIDENTIAL, gt)
   generate_city_feature(m_id, 13, 60, 18, 79, CCITY_SECTOR_RESIDENTIAL, gt)
+
+  -- JCD FIXME: For each of the two below, there should be a chance to
+  -- divide the columns in half and create two features instead of one.
+
+  -- Set up the various other features.
+  -- Set features along the northern jut of the fortress.
+  generate_city_feature(m_id, 1, 5, 5, 36, CCITY_SECTOR_RESIDENTIAL)
+  generate_city_feature(m_id, 7, 5, 11, 36, CCITY_SECTOR_RESIDENTIAL)
+
+  -- Features along the road to the fortress.
+  generate_city_feature(m_id, 1, 45, 4, 57, CCITY_SECTOR_RESIDENTIAL)
+  generate_city_feature(m_id, 1, 59, 4, 78, CCITY_SECTOR_RESIDENTIAL)
+  generate_city_feature(m_id, 7, 39, 11, 56, CCITY_SECTOR_RESIDENTIAL)
+  generate_city_feature(m_id, 7, 60, 11, 78, CCITY_SECTOR_RESIDENTIAL)
 end
  
 function init_carcassia_b1(map_id)
