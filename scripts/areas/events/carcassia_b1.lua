97a858705d6c8b5fa8475c9685fb040ce7c3ee4a
Julian Day <jcd748@mail.usask.ca>
2018-09-16 18:03:16 -0500

    Fix the enums in the sector generators to ensure that flower/rock gardens generate across the front of the fortress. Some initial work towards the praetor's residence in New Town. Stanza for "Tel and Floridel".

--- scripts/areas/events/carcassia_b1.lua
+++ scripts/areas/events/carcassia_b1.lua
@@ -31,23 +31,26 @@ local function setup_dynamic_content(m_id)
   generate_city_feature(m_id, 13, 37, 18, 56, CCITY_SECTOR_RESIDENTIAL, gt)
   generate_city_feature(m_id, 13, 60, 18, 79, CCITY_SECTOR_RESIDENTIAL, gt)
 
-  -- JCD FIXME: For each of the two below, there should be a chance to
-  -- divide the columns in half and create two features instead of one.
-
-  -- Set up the various other features.
-  -- Set features along the northern jut of the fortress.
-  generate_city_feature(m_id, 1, 5, 5, 36, CCITY_SECTOR_RESIDENTIAL)
-  generate_city_feature(m_id, 7, 5, 11, 36, CCITY_SECTOR_RESIDENTIAL)
-
   -- Features along the road to the fortress.
   generate_city_feature(m_id, 1, 45, 4, 57, CCITY_SECTOR_RESIDENTIAL)
   generate_city_feature(m_id, 1, 59, 4, 78, CCITY_SECTOR_RESIDENTIAL)
   generate_city_feature(m_id, 7, 39, 11, 56, CCITY_SECTOR_RESIDENTIAL)
   generate_city_feature(m_id, 7, 60, 11, 78, CCITY_SECTOR_RESIDENTIAL)
 end
+
+local function setup_praetor_house(map_id)
+  -- Need to do the quests in order to see the praetor - no cheating by
+  -- digging!
+  for row = 2,7 do
+    for col = 10,34 do
+      map_set_tile_property(map_id, row, col, "TILE_PROPERTY_CANNOT_DIG", "1")
+    end
+  end
+end
  
 function init_carcassia_b1(map_id)
   setup_dynamic_content(map_id)
+  setup_praetor_house(map_id)
 end
 
 map_events.set_map_fn(map_id, init_carcassia_b1)
