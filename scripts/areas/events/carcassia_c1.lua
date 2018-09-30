bbf062325d0fe7c9045fd4f80c4e1a77a7d7777d
Julian Day <jcd748@mail.usask.ca>
2018-09-30 16:38:06 -0500

    Emperor Titus.  Emperor's Guard.  API function to allow setting the generation count of an item for debugging purposes.  Pillars for the throne room in the Carcassian fortress.

--- scripts/areas/events/carcassia_c1.lua
+++ scripts/areas/events/carcassia_c1.lua
@@ -3,7 +3,23 @@ require('map_events')
 
 local map_id = "carcassia_c1"
 
+local function init_throne_room()
+  local rows = {4,14}
+  local start_col = 6
+
+  for i,row in ipairs(rows) do
+    for col = start_col, 78, 5 do
+      add_feature_to_map(CCLASS_ID_FIRE_PILLAR, row, col, map_id)
+    end
+  end
+
+  for row = 4, 14, 2 do
+    add_feature_to_map(CCLASS_ID_FIRE_PILLAR, row, start_col, map_id)
+  end
+end
+
 function init_carcassia_c1(map_id)
+  init_throne_room(map_id)
 end
 
 map_events.set_map_fn(map_id, init_carcassia_c1)
