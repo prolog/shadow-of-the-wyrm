1fbdc6a2caea8cc0119c98bbd187a374fd9002fd
Julian Day <jcd748@mail.usask.ca>
2018-10-20 19:37:00 -0500

    Lycaon. Fire pillars now change colour between red and yellow. Stanza for "Tel and Floridel".

--- engine/include/ShimmerColours.hpp
+++ engine/include/ShimmerColours.hpp
@@ -1,10 +1,14 @@
 #pragma once
 #include <vector>
 #include "Colours.hpp"
-class ShimmerColours
+#include "ISerializable.hpp"
+
+class ShimmerColours : public ISerializable
 {
   public:
-    ShimmerColours(const std::vector<Colour>& sh_colours);
+    ShimmerColours();
+    ShimmerColours(const std::vector<Colour>& sh_colours, const int pct_chance_shimmer = BASE_PCT_CHANCE_SHIMMER);
+    bool operator==(const ShimmerColours& sc) const;
 
     Colour get_passable_colour() const;
     Colour get_impassable_colour() const;
@@ -12,17 +16,26 @@ class ShimmerColours
     Colour get_shimmer_colour() const;
     int get_pct_chance_shimmer() const;
 
+    std::vector<Colour> get_raw_colours() const;
+
+    bool serialize(std::ostream& stream) const;
+    bool deserialize(std::istream& stream);
+
   protected:
     std::vector<Colour> colours;
+    int pct_chance_shimmer;
 
     bool is_valid() const;
     Colour get_colour_or_undefined(const size_t idx) const;
 
-    static const int PCT_CHANCE_SHIMMER;
+    static const int BASE_PCT_CHANCE_SHIMMER;
     static const size_t PASSABLE_IDX;
     static const size_t IMPASSABLE_IDX;
     static const size_t FEATURE_IDX;
     static const size_t SHIMMER_IDX;
     static const size_t VALID_SIZE;
+
+  private:
+    ClassIdentifier internal_class_identifier() const;
 };
 
