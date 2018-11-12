f09887e8ef74ade62df8534084fc309ccaabb271
Julian Day <jcd748@mail.usask.ca>
2018-11-11 19:13:26 -0600

    Some work towards Pitmistress Ghara. Stanza for the long poem.

--- scripts/quests/carcassia/pitmistress_ghara.lua
+++ scripts/quests/carcassia/pitmistress_ghara.lua
@@ -2,7 +2,11 @@ require('constants')
 require('fn')
 require('quest')
 
+local pg_on_quest = "pitmistress_ghara_quest"
+
 local function ghara_quest_start_fn()
+  -- Set the creature's ID
+  -- set_creature_id("carcassia_c2", some_y, some_x, "ghara_quest_crid")
 end
 
 local function ghara_quest_completion_condition_fn()
@@ -15,4 +19,12 @@ local function ghara_quest_completion_fn()
   return true
 end
 
-clear_and_add_message("PITMISTRESS_GHARA_SPEECH_TEXT_SID")
+if get_creature_additional_property(PLAYER_ID, pg_on_quest) == "1" then  
+else
+  if add_confirmation_message("PITMISTRESS_GHARA_FIGHT_QUERY_SID") then
+    clear_and_add_message("PITMISTRESS_GHARA_FIGHT_ACCEPT_SID")
+    -- set_creature_additional_property(PLAYER_ID, pg_on_quest, "1")
+  else
+    clear_and_add_message("PITMISTRESS_GHARA_FIGHT_DECLINE_SID")
+  end
+end
