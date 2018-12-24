64f48a418e85719ea3ce80c4e9144af1da335ad1
Julian Day <jcd748@mail.usask.ca>
2018-12-24 09:01:26 -0600

    Calabus

---
+++ scripts/quests/carcassia/calabus.lua
@@ -0,0 +1,7 @@
+add_message_with_pause("CALABUS_SPEECH_TEXT_SID")
+add_message_with_pause("CALABUS_SPEECH_TEXT2_SID")
+add_message_with_pause("CALABUS_SPEECH_TEXT3_SID")
+add_message_with_pause("CALABUS_SPEECH_TEXT4_SID")
+add_message_with_pause("CALABUS_SPEECH_TEXT5_SID")
+clear_and_add_message("CALABUS_SPEECH_TEXT6_SID")
+
