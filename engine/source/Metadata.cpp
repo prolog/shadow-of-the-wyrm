1a55c8397025520412d815425f4120d11ef4c84b
Julian Day <jcd748@mail.usask.ca>
2019-06-11 19:36:12 -0500

    Start v1.1.1

--- engine/source/Metadata.cpp
+++ engine/source/Metadata.cpp
@@ -55,7 +55,7 @@ string Metadata::get_version() const
 
 string Metadata::get_code_name() const
 { 
-  return "\"Robert Dowland\"";
+  return "\"Campion\"";
 }
 
 string Metadata::get_game_version_synopsis() const
