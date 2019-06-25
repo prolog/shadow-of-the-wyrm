8cccfa080d0404d2fd9ddaaa8339e63980673e7f
Julian Day <jcd748@mail.usask.ca>
2019-06-24 19:54:26 -0500

    The player wasn't being teleported out of the arena when the opponent killed itself.

--- scripts/death/fighting_pits.lua
+++ scripts/death/fighting_pits.lua
@@ -3,9 +3,12 @@ require('constants')
 
 local pg_cur_q_complete = "pitmistress_ghara_current_quest_complete"
 
+-- Always teleport the player out, regardless of whether or not they
+-- killed the creature, as creatures can kill themselves via stunning
+-- attacks, etc.
 local function exit_pits(creature_id, attacking_creature_id, map_id)
   add_message_with_pause("EXIT_FIGHTING_PITS_SID", false)
-  teleport(attacking_creature_id, 9, 47)
+  teleport(PLAYER_ID, 9, 47)
   set_creature_additional_property(PLAYER_ID, pg_cur_q_complete, "1")
 
   clear_messages()
