68f195f7f24224d4533434a80e30dc411c469328
Julian Day <jcd748@mail.usask.ca>
2019-06-25 19:51:24 -0500

    Intimidation now has a level-based check, and automatic hits are a bit more likely.

--- engine/calculators/source/IntimidationCalculator.cpp
+++ engine/calculators/source/IntimidationCalculator.cpp
@@ -3,6 +3,7 @@
 const int IntimidationCalculator::MAX_PCT_CHANCE_INTIMIDATION = 85;
 const int IntimidationCalculator::INTIMIDATION_DIVISOR = 3;
 const int IntimidationCalculator::CHARISMA_DIVISOR = 4;
+const int IntimidationCalculator::LEVEL_DIVISOR = 2;
 
 int IntimidationCalculator::calculate_pct_chance_intimidated(CreaturePtr attacking_creature, CreaturePtr attacked_creature)
 {
@@ -12,14 +13,16 @@ int IntimidationCalculator::calculate_pct_chance_intimidated(CreaturePtr attacki
   {
     int attack_intim = attacking_creature->get_skills().get_value(SkillType::SKILL_GENERAL_INTIMIDATION);
     int attack_cha = attacking_creature->get_charisma().get_current();
+    int attack_level = attacking_creature->get_level().get_current();
 
     int attacked_intim = attacked_creature->get_skills().get_value(SkillType::SKILL_GENERAL_INTIMIDATION);
     int attacked_cha = attacked_creature->get_charisma().get_current();
+    int attacked_level = attacked_creature->get_level().get_current();
 
     if (attacked_intim > 0)
     {
-      int attack = (attack_intim / INTIMIDATION_DIVISOR) + (attack_cha / CHARISMA_DIVISOR);
-      int attacked = (attacked_intim / INTIMIDATION_DIVISOR) + (attacked_cha / CHARISMA_DIVISOR);
+      int attack = (attack_intim / INTIMIDATION_DIVISOR) + (attack_cha / CHARISMA_DIVISOR) + (attack_level / LEVEL_DIVISOR);
+      int attacked = (attacked_intim / INTIMIDATION_DIVISOR) + (attacked_cha / CHARISMA_DIVISOR) + (attacked_level / LEVEL_DIVISOR);
 
       pct_chance = attacked - attack;
       pct_chance = std::max(0, pct_chance);
