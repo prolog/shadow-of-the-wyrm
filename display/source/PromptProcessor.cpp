0e25a59951ec6ca893851bd3d6acb61442032c9d
Julian Day <jcd748@mail.usask.ca>
2019-09-09 19:51:09 -0500

    Fix prompt on naming screen for SDL.

--- display/source/PromptProcessor.cpp
+++ display/source/PromptProcessor.cpp
@@ -4,8 +4,6 @@ using namespace std;
 
 Coordinate PromptProcessor::get_prompt_coords(const PromptLocation pl, const string& prompt_text, const int row, const int col, const int max_rows, const int max_cols)
 {
-  Coordinate c = make_pair(0, 0);
-
   int prompt_row = row;
   int prompt_col = col;
 
@@ -14,9 +12,7 @@ Coordinate PromptProcessor::get_prompt_coords(const PromptLocation pl, const str
     int prompt_text_length = prompt_text.size();
     prompt_col = max_cols - prompt_text_length - 1;
     prompt_row = max_rows - 1;
-
-    c = {prompt_row, prompt_col};
   }
 
-  return c;
+  return {prompt_row, prompt_col};
 }
