a93b9696424e7cd1d143ab90985682e531fd2a01
Julian Day <jcd748@mail.usask.ca>
2020-01-05 12:16:09 -0600

    gcc fixes

--- engine/XML/custom_maps/features/source/XMLConfigurableFeatureCMReader.cpp
+++ engine/XML/custom_maps/features/source/XMLConfigurableFeatureCMReader.cpp
@@ -11,18 +11,6 @@ FeaturePtr XMLConfigurableFeatureCMReader::create(const XMLNode& cf_node) const
   {
     string id = XMLUtils::get_attribute_value(cf_node, "id");
     
-    MaterialType material = static_cast<MaterialType>(XMLUtils::get_child_node_int_value(cf_node, "Material", static_cast<int>(MaterialType::MATERIAL_TYPE_WOOD)));
-    string symbol_s = XMLUtils::get_child_node_value(cf_node, "Symbol");
-    uchar symbol = '?';
-
-    if (!symbol_s.empty())
-    {
-      symbol = symbol_s.at(0);
-    }
-
-    Colour colour = static_cast<Colour>(XMLUtils::get_child_node_int_value(cf_node, "Colour"));
-    string desc_sid = XMLUtils::get_child_node_value(cf_node, "DescriptionSID");
-
     if (!id.empty())
     {
       cf = FeatureGenerator::generate_configurable_feature(id);
