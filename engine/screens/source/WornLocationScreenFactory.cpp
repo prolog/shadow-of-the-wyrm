06f7e248c6b797bdd733ae8c88111403b02df3eb
Julian Day <jcd748@mail.usask.ca>
2020-04-12 10:18:56 -0500

    WornLocationScreenPtr to unique_ptr. More creature tiles.

--- engine/screens/source/WornLocationScreenFactory.cpp
+++ engine/screens/source/WornLocationScreenFactory.cpp
@@ -6,7 +6,7 @@ using namespace std;
 WornLocationSelectionScreenPtr WornLocationScreenFactory::create_skin_screen(DisplayPtr display)
 {
   vector<EquipmentWornLocation> skin_locs = { EquipmentWornLocation::EQUIPMENT_WORN_HEAD, EquipmentWornLocation::EQUIPMENT_WORN_BODY, EquipmentWornLocation::EQUIPMENT_WORN_AROUND_BODY, EquipmentWornLocation::EQUIPMENT_WORN_FEET, EquipmentWornLocation::EQUIPMENT_WORN_RANGED_WEAPON };
-  WornLocationSelectionScreenPtr scr = make_shared<WornLocationSelectionScreen>(display, skin_locs, TextKeys::SKIN_PROMPT);
+  WornLocationSelectionScreenPtr scr = make_unique<WornLocationSelectionScreen>(display, skin_locs, TextKeys::SKIN_PROMPT);
 
   return scr;
 }
@@ -14,7 +14,7 @@ WornLocationSelectionScreenPtr WornLocationScreenFactory::create_skin_screen(Dis
 WornLocationSelectionScreenPtr WornLocationScreenFactory::create_weaving_screen(DisplayPtr display)
 {
   vector<EquipmentWornLocation> weave_locs = { EquipmentWornLocation::EQUIPMENT_WORN_BODY, EquipmentWornLocation::EQUIPMENT_WORN_AROUND_BODY };
-  WornLocationSelectionScreenPtr scr = make_shared<WornLocationSelectionScreen>(display, weave_locs, TextKeys::WEAVE_PROMPT);
+  WornLocationSelectionScreenPtr scr = make_unique<WornLocationSelectionScreen>(display, weave_locs, TextKeys::WEAVE_PROMPT);
 
   return scr;
 }
