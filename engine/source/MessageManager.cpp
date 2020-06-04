1df38ab22c61938e2455267c6bc8fef3d3e24c60
Julian Day <jcd748@mail.usask.ca>
2020-06-04 07:26:55 -0500

    A few improved sprites.

--- engine/source/MessageManager.cpp
+++ engine/source/MessageManager.cpp
@@ -58,6 +58,8 @@ void MessageManager::send(const MessageSpacing ms, const bool halt_after, const 
 
       buffer_has_messages = true;
     }
+
+    user_display->refresh_current_window();
   }
   
   if (halt_after)
