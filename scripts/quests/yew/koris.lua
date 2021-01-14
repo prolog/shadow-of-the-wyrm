0e2e760fe6d13ada5504671795dcf4e5462d965b
Julian Day <jcd748@mail.usask.ca>
2021-01-14 08:34:40 -0600

    Koris basics.

---
+++ scripts/quests/yew/koris.lua
@@ -0,0 +1,8 @@
+add_message_with_pause("KORIS_SPEECH_TEXT_SID")
+add_message_with_pause("KORIS_SPEECH_TEXT2_SID")
+add_message_with_pause("KORIS_SPEECH_TEXT3_SID")
+add_message_with_pause("KORIS_SPEECH_TEXT4_SID")
+add_message_with_pause("KORIS_SPEECH_TEXT5_SID")
+clear_and_add_message("KORIS_SPEECH_TEXT6_SID")
+
+-- ...
