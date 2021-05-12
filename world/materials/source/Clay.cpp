30d0835fb2b9734b94f956dca28e971fc5230652
Julian Day <jcd748@mail.usask.ca>
2021-05-12 18:19:39 -0500

    Stanza. Clay. Fix a typo in the codex.

---
+++ world/materials/source/Clay.cpp
@@ -0,0 +1,53 @@
+#include "Clay.hpp"
+#include "MaterialTextKeys.hpp"
+
+using namespace std;
+
+MaterialType Clay::get_type() const
+{
+  return MaterialType::MATERIAL_TYPE_CLAY;
+}
+
+string Clay::get_name_sid() const
+{
+  return MaterialTextKeys::MATERIAL_CLAY;
+}
+
+bool Clay::get_is_flammable() const
+{
+  return false;
+}
+
+bool Clay::get_is_explosive() const
+{
+  return false;
+}
+
+bool Clay::get_is_corrodible() const
+{
+  return false;
+}
+
+bool Clay::get_crumbles() const
+{
+  return true;
+}
+
+Colour Clay::get_colour() const
+{
+  return Colour::COLOUR_YELLOW;
+}
+
+Material* Clay::clone()
+{
+  return new Clay(*this);
+}
+
+bool Clay::get_material_floats() const
+{
+  return false;
+}
+
+#ifdef UNIT_TESTS
+#include "unit_tests/Clay_test.cpp"
+#endif
---
+++ world/materials/source/unit_tests/Clay_test.cpp
@@ -0,0 +1,31 @@
+#include "gtest/gtest.h"
+
+TEST(SW_World_Material_Clay, type)
+{
+  Clay c;
+  EXPECT_EQ(MaterialType::MATERIAL_TYPE_CLAY, c.get_type());
+}
+
+TEST(SW_World_Material_Clay, get_colour)
+{
+  Clay c;
+
+  EXPECT_EQ(Colour::COLOUR_YELLOW, c.get_colour());
+}
+
+TEST(SW_World_Material_Clay, flags)
+{
+  Clay c;
+
+  EXPECT_FALSE(c.get_is_flammable());
+  EXPECT_FALSE(c.get_is_explosive());
+  EXPECT_FALSE(c.get_is_corrodible());
+  EXPECT_TRUE(c.get_crumbles());
+}
+
+TEST(SW_World_Material_Clay, get_material_floats)
+{
+  Clay c;
+
+  EXPECT_FALSE(c.get_material_floats());
+}
