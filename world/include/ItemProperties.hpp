15002b7cb7864daca4f52e00cb221e320a02e25e
Julian Day <jcd748@mail.usask.ca>
2021-08-11 12:57:19 -0500

    WIP towards piety for burying the dead.

--- world/include/ItemProperties.hpp
+++ world/include/ItemProperties.hpp
@@ -25,6 +25,7 @@ class ItemProperties
     static const std::string ITEM_PROPERTIES_FLOATS;
     static const std::string ITEM_PROPERTIES_METABOLIZES_ALCOHOL;
     static const std::string ITEM_PROPERTIES_SUPPRESS_ITEM_GENERATION_ON_DESTRUCTION;
+    static const std::string ITEM_PROPERTIES_REMAINS;
 
   protected:
     ItemProperties();
