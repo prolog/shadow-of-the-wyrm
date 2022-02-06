7f16afd3351e2678078fe6421a55e25edde38937
Julian Day <jcd748@mail.usask.ca>
2022-02-06 13:59:22 -0600

    Quest for commoners.

---
+++ scripts/quests/commoner.lua
@@ -0,0 +1,43 @@
+require('constants')
+require('quest')
+
+local cr_id = args[SPEAKING_CREATURE_ID]
+
+-- Commoner quest details
+local function commoner_start_fn()
+  add_message_with_pause("COMMONER_QUEST_START_SID")
+  clear_and_add_message("COMMONER_QUEST_START2_SID")
+end
+
+local function commoner_completion_condition_fn()
+  return (player_has_item(BREAD_ID))
+end
+
+local function commoner_completion_fn()
+  remove_object_from_player(BREAD_ID)
+  add_piety(PLAYER_ID, 1000)
+  
+  add_message("COMMONER_QUEST_COMPLETE_SID")
+  return true
+end
+
+commoner_quest = Quest:new("commoner_" .. cr_id, 
+                           "COMMONER_QUEST_TITLE_SID", 
+                           "COMMONER_SHORT_DESCRIPTION_SID", 
+                           "COMMONER_QUEST_DESCRIPTION_SID", 
+                           "COMMONER_QUEST_COMPLETE_SID", 
+                           "COMMONER_QUEST_REMINDER_SID", 
+                           truefn,
+                           commoner_start_fn, 
+                           commoner_completion_condition_fn, 
+                           commoner_completion_fn)
+
+local do_quest = Quest:check_probabilistic_quest(cr_id, 20)
+
+if do_quest == true then
+  do_quest = commoner_quest:execute()
+end
+
+if do_quest == false then
+  clear_and_add_message("COMMONER_SPEECH_TEXT_SID")
+end
