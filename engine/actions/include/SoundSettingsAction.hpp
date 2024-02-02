b55865911ad50cb474d709aa31a031bc57bcf689
Julian Day <jcd748@mail.usask.ca>
2024-02-01 18:44:40 -0600

    Refactor SoundSettingsAction into a common class to be shared with AutomaticActions (& others)

--- engine/actions/include/SoundSettingsAction.hpp
+++ engine/actions/include/SoundSettingsAction.hpp
@@ -1,18 +1,13 @@
 #pragma once
-#include <string>
-#include <utility>
-#include <vector>
-#include "IActionManager.hpp"
+#include "ToggleSettingsAction.hpp"
 
-class SoundSettingsAction : public IActionManager
+class SoundSettingsAction : public ToggleSettingsAction
 {
   public:
-    std::vector<std::pair<std::string, std::string>> get_sound_settings(CreaturePtr creature) const;
-    ActionCostValue sound_actions(CreaturePtr creature) const;
+    ActionCostValue sound_actions(CreaturePtr creature);
     ActionCostValue get_action_cost_value(CreaturePtr creature) const override;
 
   protected:
-    std::vector<std::pair<std::string, std::string>> get_sound_settings() const;
-    std::string get_autopickup_type_string(CreaturePtr creature) const;
+    std::vector<std::pair<std::string, std::string>> get_settings_to_display() const override;
 };
 
