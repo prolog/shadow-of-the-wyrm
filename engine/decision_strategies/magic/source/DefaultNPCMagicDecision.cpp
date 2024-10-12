ac5ec33ae507d4521a3b4cb39e945311b2ef628c
Julian Day <jcd748@mail.usask.ca>
2024-10-12 10:06:01 -0500

    Some prep for NPCs using utility spells.

--- engine/decision_strategies/magic/source/DefaultNPCMagicDecision.cpp
+++ engine/decision_strategies/magic/source/DefaultNPCMagicDecision.cpp
@@ -7,7 +7,7 @@ pair<bool, Direction> DefaultNPCMagicDecision::decide(CreaturePtr caster, MapPtr
   bool decision = false;
 
   // JCD FIXME revisit this later and create per-effect checkers, etc
-/*  if (caster != nullptr)
+  if (caster != nullptr)
   {
     // Make sure the creature is not already affected. Also, ensure that the
     // creature doesn't actually cast anything unless there are hostiles
@@ -18,9 +18,9 @@ pair<bool, Direction> DefaultNPCMagicDecision::decide(CreaturePtr caster, MapPtr
     if (!caster->is_affected_by_modifier_spell(spell.get_spell_id()) &&
         !caster->get_decision_strategy()->get_threats_ref().get_true_threats_without_level().empty())
     {
-      decision = true;
+      // decision = true;
     }
-  } */
+  }
 
   return make_pair(decision, Direction::DIRECTION_NULL);
 }
