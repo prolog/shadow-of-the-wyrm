9d850d4aa9f65bdb6eef6e2dbd0c60942be1137f
Julian Day <jcd748@mail.usask.ca>
2024-10-22 08:30:28 -0500

    Create TileMovementConfirmationDetails class + unit tests in preparation for replacing the currently-used tuple.

---
+++ engine/actions/source/TileMovementConfirmationDetails.cpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "TileMovementConfirmationDetails.hpp"
+
+using std::string;
+
+TileMovementConfirmationDetails::TileMovementConfirmationDetails()
+: require_prompt(false)
+{
+}
+
+TileMovementConfirmationDetails::TileMovementConfirmationDetails(const bool new_require_prompt, const string& new_confirmation_message_sid, const string& new_sound_effect_id, const string& new_post_movement_message_sid)
+: require_prompt(new_require_prompt), confirmation_message_sid(new_confirmation_message_sid), sound_effect_id(new_sound_effect_id), post_movement_message_sid(new_post_movement_message_sid)
+{
+}
+
+bool TileMovementConfirmationDetails::get_require_prompt() const
+{
+	return require_prompt;
+}
+
+string TileMovementConfirmationDetails::get_confirmation_message_sid() const
+{
+	return confirmation_message_sid;
+}
+
+string TileMovementConfirmationDetails::get_sound_effect_id() const
+{
+	return sound_effect_id;
+}
+
+string TileMovementConfirmationDetails::get_post_movement_message_sid() const
+{
+	return post_movement_message_sid;
+}
+
+#ifdef UNIT_TESTS
+#include "unit_tests/TileMovementConfirmationDetails_test.cpp"
+#endif
