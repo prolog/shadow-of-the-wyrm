537cdd6c664a7ab4f514d6f476992df15089872c
Julian Day <jcd748@mail.usask.ca>
2024-12-13 08:21:59 -0600

    gcc fixes

--- engine/maps/tiles/source/NullTileDamageProcessor.cpp
+++ engine/maps/tiles/source/NullTileDamageProcessor.cpp
@@ -1,6 +1,6 @@
 #include "NullTileDamageProcessor.hpp"
 
-bool NullTileDamageProcessor::affects_item(ItemPtr item)
+bool NullTileDamageProcessor::affects_item(ItemPtr /*item*/)
 {
   return false;
 }
