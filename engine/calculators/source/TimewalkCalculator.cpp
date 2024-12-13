0913b0085c7375b60b0964e6c3fca1b5cbb24e2c
Julian Day <jcd748@mail.usask.ca>
2024-12-12 19:26:00 -0600

    gcc fixes

--- engine/calculators/source/TimewalkCalculator.cpp
+++ engine/calculators/source/TimewalkCalculator.cpp
@@ -4,19 +4,19 @@
 const int TimewalkCalculator::BASE_TIMEWALK_PCT_CHANCE = 100;
 const int TimewalkCalculator::BASE_TIMEWALK_WEAR_OFF_CHANCE_PER_TURN = 2;
 
-int TimewalkCalculator::calculate_pct_chance_effect(CreaturePtr creature) const
+int TimewalkCalculator::calculate_pct_chance_effect(CreaturePtr /*creature*/) const
 {
   return BASE_TIMEWALK_PCT_CHANCE;
 }
 
 // This function is just here for completeness - timewalk by definition is done
 // whenever time moves, so when the creature attacks, drops something, etc.
-int TimewalkCalculator::calculate_duration_in_minutes(CreaturePtr creature) const
+int TimewalkCalculator::calculate_duration_in_minutes(CreaturePtr /*creature*/) const
 {
   return 1;
 }
 
-int TimewalkCalculator::calculate_pct_chance_wear_off_per_turn(CreaturePtr creature) const
+int TimewalkCalculator::calculate_pct_chance_wear_off_per_turn(CreaturePtr /*creature*/) const
 {
   return BASE_TIMEWALK_WEAR_OFF_CHANCE_PER_TURN;
 }
--- engine/calculators/source/WaterBreathingCalculator.cpp
+++ engine/calculators/source/WaterBreathingCalculator.cpp
@@ -4,14 +4,14 @@
 const int WaterBreathingCalculator::WB_DURATION = 45;
 const int WaterBreathingCalculator::BASE_WB_PCT_CHANCE = 100;
 
-int WaterBreathingCalculator::calculate_pct_chance_effect(CreaturePtr creature) const
+int WaterBreathingCalculator::calculate_pct_chance_effect(CreaturePtr /*creature*/) const
 {
   return BASE_WB_PCT_CHANCE;
 }
 
 // Water Breathing has a standard predictable wear-off time, since it's so
 // dangerous once gone.
-int WaterBreathingCalculator::calculate_duration_in_minutes(CreaturePtr creature) const
+int WaterBreathingCalculator::calculate_duration_in_minutes(CreaturePtr /*creature*/) const
 {
   return WB_DURATION;
 }
