f3ae49a995f1118046ea48f93edd976f74954b04
Julian Day <jcd748@mail.usask.ca>
2025-04-09 08:19:58 -0500

    Deathly tile movement message.

--- engine/strings/include/TileDangerConfirmationKeys.hpp
+++ engine/strings/include/TileDangerConfirmationKeys.hpp
@@ -6,6 +6,7 @@ class TileDangerConfirmationKeys
   public:
     static const std::string TILE_DANGER_MOUNTAINS;
     static const std::string TILE_DANGER_AIR;
+    static const std::string TILE_DANGER_DEATHLY;
     
   protected:
     TileDangerConfirmationKeys();
