1c97ad8dc5d7ece96e2517d4d77cb8ad70dd0ccc
Julian Day <jcd748@mail.usask.ca>
2025-05-10 09:14:27 -0500

    Add charm info to codex.

--- engine/strings/source/TextKeys.cpp
+++ engine/strings/source/TextKeys.cpp
@@ -214,4 +214,5 @@ const string TextKeys::THE_WORLD_MAP = "THE_WORLD_MAP";
 const string TextKeys::NO_CREATURES_GENERATED = "NO_CREATURES_GENERATED";
 const string TextKeys::DEFAULT_CLASS_NAME_SID = "DEFAULT_CLASS_NAME_SID";
 const string TextKeys::RESPAWN_MESSAGE = "RESPAWN_MESSAGE";
+const string TextKeys::CHARM_RACES = "CHARM_RACES";
 
