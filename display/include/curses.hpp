47e682f0cc9577d881f61df28acbc96668b6750c
Julian Day <jcd748@mail.usask.ca>
2025-08-30 17:44:32 -0500

    Define CTL_LEFT and CTL_RIGHT for the cases where it's not defined (eg Linux w/ncurses)

--- display/include/curses.hpp
+++ display/include/curses.hpp
@@ -7,3 +7,14 @@
   #endif
 #include <ncurses.h>
 #endif
+
+// These aren't going to make things magically work in Linux/ncurses,
+// but it will at least define the same constants so the SDL code
+// can use the common constants.
+#ifndef CTL_LEFT
+#define CTL_LEFT 0x1bb
+#endif
+
+#ifndef CTL_RIGHT
+#define CTL_RIGHT 0x1bc
+#endif
