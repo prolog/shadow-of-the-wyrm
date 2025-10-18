d70a441f8ed2428a7a4020d6e6b8b581a2e8f5a8
Julian Day <jcd748@mail.usask.ca>
2025-10-18 18:41:16 -0500

    fix

--- engine/decision_strategies/source/NPCDecisionStrategy.cpp
+++ engine/decision_strategies/source/NPCDecisionStrategy.cpp
@@ -639,7 +639,7 @@ CommandPtr NPCDecisionStrategy::get_ranged_attack_decision(const string& this_cr
                 // with this, but animals, etc, should not rouse a hive to
                 // attack a foe.
                 if (this_cr->get_intelligence().get_current() >= IntelligenceConstants::MIN_INTELLIGENCE_UNDERSTAND_INDIRECT_ACTIONS &&
-                    CoordUtils::chebyshev_distance(threat_c, hive_c) <= CoordUtils::chebyshev_distance(c_this, hive_c) &&
+                    CoordUtils::chebyshev_distance(threat_c, tdf_c.first) <= CoordUtils::chebyshev_distance(c_this, tdf_c.first) &&
                     RangedCombatUtils::is_coordinate_obstacle_free(this_cr, c_this, hive_c, view_map))
                 {
                   rc_selected_id.clear();
