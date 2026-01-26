775c36c031f540929369970cbc954bc59219cac0
Julian Day <julianclday@gmail.com>
2026-01-25 19:38:30 -0600

    Initial 2.0.0 commit.

--- engine/source/Metadata.cpp
+++ engine/source/Metadata.cpp
@@ -56,7 +56,7 @@ string Metadata::get_version() const
 
 string Metadata::get_code_name() const
 { 
-  return "\"Sergio Assad\"";
+  return "\"de Cabezon\"";
 }
 
 string Metadata::get_game_version_synopsis() const
