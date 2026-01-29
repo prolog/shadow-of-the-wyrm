c268a037dfe3dab7544a8cb0fa815508eeab7f65
Julian Day <julianclday@gmail.com>
2026-01-28 20:14:40 -0600

    Ensure the crash dump message uses the new email.

--- source/UnhandledExceptions.cpp
+++ source/UnhandledExceptions.cpp
@@ -14,7 +14,7 @@ LONG sotw_fault_handler(struct _EXCEPTION_POINTERS *  e)
   write_minidump(e);
 
   cout << "Shadow of the Wyrm crashed unexpectedly (sorry!)." << endl << endl;
-  cout << "Please email the .dmp file created to jcd748@mail.usask.ca." << endl << endl;
+  cout << "Please email the .dmp file created to julianclday@gmail.com." << endl << endl;
   return EXCEPTION_EXECUTE_HANDLER;
 }
 
