069fe2e2cefbda227814ad40c993c10d353e09b1
Julian Day <julianclday@gmail.com>
2026-05-07 08:33:10 -0500

    Thieves pay a little bit more in shops.

--- scripts/quests/shopkeeper.lua
+++ scripts/quests/shopkeeper.lua
@@ -28,11 +28,17 @@ end
 local function get_purchase_amount(unpaid_amount)
   local purchase_amount = unpaid_amount
   local bargain, amount_pct = bargain_discount(PLAYER_ID)
+  local class_id = get_class_id(PLAYER_ID)
 
   if bargain == true then
     purchase_amount = math.floor(purchase_amount * (1 - (amount_pct / 100)))
   end
 
+  -- Thieves pay an extra 10% everywhere.
+  if class_id == CLASS_ID_THIEF then
+    purchase_amount = math.floor(purchase_amount * 1.1)
+  end
+  
   return purchase_amount
 end
 
@@ -44,12 +50,17 @@ local shopkeep_id = args[SPEAKING_CREATURE_ID]
 if num_unpaid_items > 0 then
   -- Does the player have enough ivory to cover it?
   local currency_amount = count_currency(PLAYER_ID)
+  local purchase_amount = get_purchase_amount(unpaid_amount)
 
-  if (currency_amount >= unpaid_amount) then
-    local purchase_amount = get_purchase_amount(unpaid_amount)
-
+  if (currency_amount >= purchase_amount) then
     -- Confirm the purchase
-    if add_confirmation_message("SHOPKEEPER_CONFIRM_PURCHASE_SID", {tostring(unpaid_amount)}) then
+    purchase_sid = "SHOPKEEPER_CONFIRM_PURCHASE_SID"
+
+    if purchase_amount > unpaid_amount then
+      purchase_sid = "SHOPKEEPER_CONFIRM_PURCHASE_DISLIKE_SID"
+    end
+
+    if add_confirmation_message(purchase_sid, {tostring(purchase_amount)}) then
       complete_purchase(purchase_amount, unpaid_amount, shopkeep_id)
     else
       clear_and_add_message("SHOPKEEPER_WARNING_SID")
